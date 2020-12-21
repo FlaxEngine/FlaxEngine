965352c3f4f0fb0c9a809961dab5578a71e30741
Jean-Baptiste Perrier <jeanbapt.perrier@gmail.com>
2020-12-21 18:14:52 +0100

    Revert "Fix wrong engine path with RegisterEng. as Admin."
    This reverts commit 4a3092d0e82732c457a1820ab5d0257a687cb00f.

--- RegisterEngineLocation.bat
+++ RegisterEngineLocation.bat
@@ -3,7 +3,7 @@
 rem Copyright (c) 2012-2020 Wojciech Figat. All rights reserved.
 
 setlocal
-pushd %~dp0
+pushd
 echo Registering Flax Engine project files...
 
 rem Check the current versions config
