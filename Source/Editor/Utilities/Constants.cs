6c7529f6b3650b2e9c34fbc4eeb25701c61e2cfc
Wojtek Figat <wojtek@figat.pl>
2021-01-03 22:14:04 +0100

    Update link to Youtube channel

--- Source/Editor/Utilities/Constants.cs
+++ Source/Editor/Utilities/Constants.cs
@@ -12,7 +12,7 @@ namespace FlaxEditor.Utilities
         public static readonly string BugTrackerUrl = "https://github.com/FlaxEngine/FlaxEngine/issues";
         public static readonly string WebsiteUrl = "https://flaxengine.com";
         public static readonly string FacebookUrl = "https://facebook.com/FlaxEngine";
-        public static readonly string YoutubeUrl = "https://www.youtube.com/channel/UChdER2A3n19rJWIMOZJClhw";
+        public static readonly string YoutubeUrl = "https://youtube.com/c/FlaxEngine";
         public static readonly string TwitterUrl = "https://twitter.com/FlaxEngine";
     }
 }
