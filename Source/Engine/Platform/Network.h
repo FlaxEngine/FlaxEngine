c53fe681f573c1f30e0abd903f8af9dbbfbeb802
Jean-Baptiste Perrier <jeanbapt.perrier@gmail.com>
2021-01-23 15:36:02 +0100

    Add network header.

---
+++ Source/Engine/Platform/Network.h
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2012-2021 Wojciech Figat. All rights reserved.
+
+#pragma once
+
+#if PLATFORM_WINDOWS
+#include "Win32/Win32Network.h"
+#elif PLATFORM_UWP
+#include "Win32/Win32Network.h"
+#elif PLATFORM_LINUX
+
+#elif PLATFORM_PS4
+
+#elif PLATFORM_XBOX_SCARLETT
+#include "Win32/Win32Network.h" // it's probably isn't working
+#elif PLATFORM_ANDROID
+
+#else
+#error Missing Network implementation!
+#endif
+
+#include "Types.h"
