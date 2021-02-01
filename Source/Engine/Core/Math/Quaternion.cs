e2cefc7407cb1f74c49748c4c3726eab3fb9765a
Wojtek Figat <wojtek@figat.pl>
2021-02-01 16:58:01 +0100

    Fix Quaternion comparison epsilon to reduce error rate

--- Source/Engine/Core/Math/Quaternion.cs
+++ Source/Engine/Core/Math/Quaternion.cs
@@ -635,7 +635,7 @@ namespace FlaxEngine
         public static float AngleBetween(Quaternion a, Quaternion b)
         {
             float num = Dot(a, b);
-            return num > 0.99999999f ? 0 : Mathf.Acos(Mathf.Min(Mathf.Abs(num), 1f)) * 2f * 57.29578f;
+            return num > 0.9999999f ? 0 : Mathf.Acos(Mathf.Min(Mathf.Abs(num), 1f)) * 2f * 57.29578f;
         }
 
         /// <summary>
@@ -1478,7 +1478,7 @@ namespace FlaxEngine
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator ==(Quaternion left, Quaternion right)
         {
-            return Dot(ref left, ref right) > 0.99999999f;
+            return Dot(ref left, ref right) > 0.9999999f;
         }
 
         /// <summary>
@@ -1493,7 +1493,7 @@ namespace FlaxEngine
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator !=(Quaternion left, Quaternion right)
         {
-            return Dot(ref left, ref right) <= 0.99999999f;
+            return Dot(ref left, ref right) <= 0.9999999f;
         }
 
         /// <summary>
@@ -1605,7 +1605,7 @@ namespace FlaxEngine
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool Equals(ref Quaternion other)
         {
-            //return Dot(ref this, ref other) > 0.99999999f;
+            //return Dot(ref this, ref other) > 0.9999999f;
             return Mathf.NearEqual(other.X, X) && Mathf.NearEqual(other.Y, Y) && Mathf.NearEqual(other.Z, Z) && Mathf.NearEqual(other.W, W);
         }
 
