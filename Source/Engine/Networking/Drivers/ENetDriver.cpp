ea7cdd00bd31697b7b7ef359a7c2d4c43f9ffafe
Damian Korczowski <erdroy2@gmail.com>
2021-03-11 18:47:15 +0100

    Add ENet stubs

---
+++ Source/Engine/Networking/Drivers/ENetDriver.cpp
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2012-2021 Wojciech Figat. All rights reserved.
+
+#include "ENetDriver.h"
+
+#include "Engine/Core/Collections/Array.h"
+
+void ENetDriver::Initialize(const NetworkConfig& config)
+{
+}
+
+void ENetDriver::Dispose()
+{
+}
+
+bool ENetDriver::Listen()
+{
+    return false;
+}
+
+void ENetDriver::Connect()
+{
+}
+
+void ENetDriver::Disconnect()
+{
+}
+
+void ENetDriver::Disconnect(const NetworkConnection& connection)
+{
+}
+
+bool ENetDriver::PopEvent(NetworkEvent* eventPtr)
+{
+    return true; // No events
+}
+
+void ENetDriver::SendMessage(NetworkChannelType channelType, const NetworkMessage& message, Array<NetworkConnection, HeapAllocation> targets)
+{
+}
---
+++ Source/Engine/Networking/Drivers/ENetDriver.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2012-2021 Wojciech Figat. All rights reserved.
+
+#pragma once
+
+#include "Engine/Networking/Types.h"
+#include "Engine/Networking/INetworkDriver.h"
+
+#include "Engine/Networking/NetworkConnection.h"
+#include "Engine/Scripting/ScriptingType.h"
+
+API_CLASS(Namespace="FlaxEngine.Networking", Sealed) class FLAXENGINE_API ENetDriver : public INetworkDriver
+{
+DECLARE_SCRIPTING_TYPE_MINIMAL(ENetDriver);
+public:
+    void Initialize(const NetworkConfig& config) override;
+    void Dispose() override;
+    
+    bool Listen() override;
+    void Connect() override;
+    void Disconnect() override;
+    void Disconnect(const NetworkConnection& connection) override;
+
+    bool PopEvent(NetworkEvent* eventPtr) override;
+    
+    void SendMessage(NetworkChannelType channelType, const NetworkMessage& message, Array<NetworkConnection, HeapAllocation> targets) override;
+
+};
+
