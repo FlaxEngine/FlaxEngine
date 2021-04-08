8661c2af18830eec37988e4d0b4c09438f227486
Jean-Baptiste Perrier <jeanbapt.perrier@gmail.com>
2021-04-08 18:46:28 +0200

    Convert Int4 to generated type.

--- Source/Engine/Core/Math/Int4.h
+++ Source/Engine/Core/Math/Int4.h
@@ -13,25 +13,34 @@ struct Vector4;
 /// <summary>
 /// Four-components vector (32 bit integer type).
 /// </summary>
-API_STRUCT(InBuild) struct FLAXENGINE_API Int4
+API_STRUCT() struct FLAXENGINE_API Int4
 {
+DECLARE_SCRIPTING_TYPE_MINIMAL(Int4);
 public:
 
     union
     {
         struct
         {
-            // X component
-            int32 X;
+            /// <summary>
+            /// The X component.
+            /// </summary>
+            API_FIELD() int32 X;
 
-            // Y component
-            int32 Y;
+            /// <summary>
+            /// The Y component.
+            /// </summary>
+            API_FIELD() int32 Y;
 
-            // Z component
-            int32 Z;
+            /// <summary>
+            /// The Z component.
+            /// </summary>
+            API_FIELD() int32 Z;
 
-            // W component
-            int32 W;
+            /// <summary>
+            /// The W component.
+            /// </summary>
+            API_FIELD() int32 W;
         };
 
         // Raw values
