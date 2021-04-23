d3c974a8385955ae20588cd305719f59e200e6c0
Crawcik <crawcik@gmail.com>
2021-04-23 08:31:00 +0200

    Access modifier detection fix

--- Source/Engine/Scripting/ManagedCLR/MMethod.Mono.cpp
+++ Source/Engine/Scripting/ManagedCLR/MMethod.Mono.cpp
@@ -33,14 +33,14 @@ MMethod::MMethod(MonoMethod* monoMethod, const char* name, MClass* parentClass)
     case MONO_METHOD_ATTR_PRIVATE:
         _visibility = MVisibility::Private;
         break;
-    case MONO_METHOD_ATTR_FAM_AND_ASSEM:
-        _visibility = MVisibility::ProtectedInternal;
-        break;
     case MONO_METHOD_ATTR_ASSEM:
         _visibility = MVisibility::Internal;
         break;
     case MONO_METHOD_ATTR_FAMILY:
         _visibility = MVisibility::Protected;
+        break;
+    case MONO_METHOD_ATTR_FAM_OR_ASSEM:
+        _visibility = MVisibility::ProtectedInternal;
         break;
     case MONO_METHOD_ATTR_PUBLIC:
         _visibility = MVisibility::Public;
