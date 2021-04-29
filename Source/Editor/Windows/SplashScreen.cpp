346d0e31111fb1ee8f9f248a3bded6ce8c4c55a4
stefnotch <stefnotch@users.noreply.github.com>
2021-04-29 21:28:36 +0200

    Improve quotes?

--- Source/Editor/Windows/SplashScreen.cpp
+++ Source/Editor/Windows/SplashScreen.cpp
@@ -115,22 +115,21 @@ const Char* SplashScreenQuotes[] =
     TEXT("Cyberpunk of game engines"),
     TEXT("That's what she said"),
     TEXT("Compiling Shaders (93,788)"),
-    TEXT("Hi There"),
+    TEXT("Hello There"),
     TEXT("BAGUETTE"),
     TEXT("All we had to do was follow the damn train, CJ"),
     TEXT("28 stab wounds"),
     TEXT("Here we go again"),
     TEXT("@everyone"),
-    TEXT("uwu some spiders on the ceiling"),
-    TEXT("There you are you little shit"),
-    TEXT("potato"),
-    TEXT("python is a programming snek"),
+    TEXT("Potato"),
+    TEXT("Python is a programming snek"),
     TEXT("Flax will start when pigs will fly"),
     TEXT("I'm the android sent by CyberLife"),
-    TEXT("fancy-ass ray tracing rtx on lighting"),
+    TEXT("Fancy-ass ray tracing, rtx on, lighting"),
     TEXT("ZOINKS"),
     TEXT("Scooby dooby doo"),
-    TEXT("You shall not load"),
+    TEXT("You shall not load!"),
+    TEXT("The roof, the roof, the roof is on fire!")
 };
 
 SplashScreen::~SplashScreen()
