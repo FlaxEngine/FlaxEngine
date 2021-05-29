e6074149e52303d26b1a988dcdfbcdfa28969b44
Wojtek Figat <wojtek@figat.pl>
2021-05-29 13:48:38 +0200

    Fix Margin loading

--- Source/Engine/Serialization/JsonConverters.cs
+++ Source/Engine/Serialization/JsonConverters.cs
@@ -184,21 +184,30 @@ namespace FlaxEngine.Json
                     case JsonToken.PropertyName:
                     {
                         var propertyName = (string)reader.Value;
-                        var propertyValue = (float)reader.ReadAsDouble();
-                        switch (propertyName)
+                        reader.Read();
+                        switch (reader.TokenType)
                         {
-                        case "Left":
-                            value.Left = propertyValue;
+                        case JsonToken.Integer:
+                        case JsonToken.Float:
+                        {
+                            var propertyValue = Convert.ToSingle(reader.Value);
+                            switch (propertyName)
+                            {
+                            case "Left":
+                                value.Left = propertyValue;
+                                break;
+                            case "Right":
+                                value.Right = propertyValue;
+                                break;
+                            case "Top":
+                                value.Top = propertyValue;
+                                break;
+                            case "Bottom":
+                                value.Bottom = propertyValue;
+                                break;
+                            }
                             break;
-                        case "Right":
-                            value.Right = propertyValue;
-                            break;
-                        case "Top":
-                            value.Top = propertyValue;
-                            break;
-                        case "Bottom":
-                            value.Bottom = propertyValue;
-                            break;
+                        }
                         }
                         break;
                     }
