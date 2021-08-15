def293f88b6c8df64e754ab6fbfe3d1a47418f5b
Zbigniew Skowron <zbychs@gmail.com>
2021-08-15 14:35:52 +0200

    Added Catch2 library for native unit tests.

---
+++ Source/ThirdParty/catch2/catch2.Build.cs
@@ -0,0 +1,31 @@
+// Copyright (c) 2012-2021 Wojciech Figat. All rights reserved.
+
+using System.Collections.Generic;
+using System.IO;
+using Flax.Build;
+
+/// <summary>
+/// https://github.com/catchorg/Catch2
+/// </summary>
+public class catch2 : HeaderOnlyModule
+{
+    /// <inheritdoc />
+    public override void Init()
+    {
+        base.Init();
+
+        LicenseType = LicenseTypes.BoostSoftwareLicense;
+        LicenseFilePath = "LICENSE.txt";
+
+        // Merge into test binary
+        BinaryModuleName = "Engine.Tests";
+    }
+
+    /// <inheritdoc />
+    public override void GetFilesToDeploy(List<string> files)
+    {
+        base.GetFilesToDeploy(files);
+
+        files.AddRange(Directory.GetFiles(FolderPath, "*.hpp", SearchOption.AllDirectories));
+    }
+}
