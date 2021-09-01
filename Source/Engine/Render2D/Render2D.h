ad0c39e4ac2beadfa71431dee8d745fe35d453e6
Jean-Baptiste Perrier <jimi.vacarians@gmail.com>
2021-09-01 08:58:15 +0200

    Typo

--- Source/Engine/Render2D/Render2D.h
+++ Source/Engine/Render2D/Render2D.h
@@ -408,7 +408,7 @@ public:
     /// <param name="t">The texture.</param>
     /// <param name="vertices">The vertices array.</param>
     /// <param name="uvs">The uvs array.</param>
-    /// /// <param name="color">The color.</param>
+    /// <param name="color">The color.</param>
     API_FUNCTION() static void DrawTexturedTriangles(GPUTexture* t, const Span<Vector2>& vertices, const Span<Vector2>& uvs, const Color& color);
     
     /// <summary>
@@ -417,7 +417,7 @@ public:
     /// <param name="t">The texture.</param>
     /// <param name="vertices">The vertices array.</param>
     /// <param name="uvs">The uvs array.</param>
-    /// /// <param name="colors">The colors array.</param>
+    /// <param name="colors">The colors array.</param>
     API_FUNCTION() static void DrawTexturedTriangles(GPUTexture* t, const Span<Vector2>& vertices, const Span<Vector2>& uvs, const Span<Color>& colors);
 
     /// <summary>
