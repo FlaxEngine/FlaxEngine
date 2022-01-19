200ac18335e0a01f767d416cfd5921c8bbc412fa
Wojtek Figat <wojtek@figat.pl>
2022-01-19 10:25:26 +0100

    Fix mac app icons

--- Source/Editor/Cooker/Platform/Mac/MacPlatformTools.cpp
+++ Source/Editor/Cooker/Platform/Mac/MacPlatformTools.cpp
@@ -144,11 +144,15 @@ bool MacPlatformTools::OnPostProcess(CookingData& data)
             return true;
         }
         bool failed = Platform::RunProcess(TEXT("sips -z 16 16 icon_1024x1024.png --out icon_16x16.png"), tmpFolderPath);
+        failed |= Platform::RunProcess(TEXT("sips -z 32 32 icon_1024x1024.png --out icon_16x16@2x.png"), tmpFolderPath);
         failed |= Platform::RunProcess(TEXT("sips -z 32 32 icon_1024x1024.png --out icon_32x32.png"), tmpFolderPath);
-        failed |= Platform::RunProcess(TEXT("sips -z 64 64 icon_1024x1024.png --out icon_64x64.png"), tmpFolderPath);
+        failed |= Platform::RunProcess(TEXT("sips -z 64 64 icon_1024x1024.png --out icon_32x32@2x.png"), tmpFolderPath);
         failed |= Platform::RunProcess(TEXT("sips -z 128 128 icon_1024x1024.png --out icon_128x128.png"), tmpFolderPath);
-        failed |= Platform::RunProcess(TEXT("sips -z 256 256 icon_1024x1024.png --out icon_256x512.png"), tmpFolderPath);
+        failed |= Platform::RunProcess(TEXT("sips -z 256 256 icon_1024x1024.png --out icon_128x128@2x.png"), tmpFolderPath);
+        failed |= Platform::RunProcess(TEXT("sips -z 256 256 icon_1024x1024.png --out icon_256x256.png"), tmpFolderPath);
+        failed |= Platform::RunProcess(TEXT("sips -z 512 512 icon_1024x1024.png --out icon_256x256@2x.png"), tmpFolderPath);
         failed |= Platform::RunProcess(TEXT("sips -z 512 512 icon_1024x1024.png --out icon_512x512.png"), tmpFolderPath);
+        failed |= Platform::RunProcess(TEXT("sips -z 1024 1024 icon_1024x1024.png --out icon_512x512@2x.png"), tmpFolderPath);
         failed |= Platform::RunProcess(TEXT("iconutil -c icns icon.iconset"), iconFolderPath);
         if (failed)
         {
