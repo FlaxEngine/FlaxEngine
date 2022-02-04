5fffee143cb469080df8ddd27c60eecfdace1a4b
PrecisionRender <89754713+PrecisionRender@users.noreply.github.com>
2022-02-04 14:06:45 -0600

    Update CSProjectGenerator to support .NET Framework 4.8 targeting pack

--- Source/Tools/Flax.Build/Projects/VisualStudio/CSProjectGenerator.cs
+++ Source/Tools/Flax.Build/Projects/VisualStudio/CSProjectGenerator.cs
@@ -77,7 +77,7 @@ namespace Flax.Build.Projects.VisualStudio
 
             csProjectFileContent.AppendLine(string.Format("    <RootNamespace>{0}</RootNamespace>", project.Name));
             csProjectFileContent.AppendLine(string.Format("    <AssemblyName>{0}.CSharp</AssemblyName>", project.Name));
-            csProjectFileContent.AppendLine(string.Format("    <TargetFrameworkVersion>{0}</TargetFrameworkVersion>", "v4.5"));
+            csProjectFileContent.AppendLine(string.Format("    <TargetFrameworkVersion>{0}</TargetFrameworkVersion>", "v4.8"));
             csProjectFileContent.AppendLine("    <LangVersion>7.3</LangVersion>");
             csProjectFileContent.AppendLine("    <FileAlignment>512</FileAlignment>");
             csProjectFileContent.AppendLine("    <TargetFrameworkProfile />");
