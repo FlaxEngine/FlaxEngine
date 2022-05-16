8125e406bb139a31543f4cb0002764c0f9772b37
Wojciech Figat <wojtek@figat.pl>
2022-05-16 10:06:12 +0200

    Add new math utilities to shaders library

---
+++ Source/Shaders/Quaternion.hlsl
@@ -0,0 +1,18 @@
+// Copyright (c) 2012-2022 Wojciech Figat. All rights reserved.
+
+#ifndef __QUATERNION__
+#define __QUATERNION__
+
+float4 QuaternionMultiply(float4 q1, float4 q2)
+{
+	return float4(q2.xyz * q1.w + q1.xyz * q2.w + cross(q1.xyz, q2.xyz), q1.w * q2.w - dot(q1.xyz, q2.xyz));
+}
+
+float3 QuaternionRotate(float4 q, float3 v)
+{
+	float3 b = q.xyz;
+	float b2 = dot(b, b);
+	return (v * (q.w * q.w - b2) + b * (dot(v, b) * 2.f) + cross(b, v) * (q.w * 2.f));
+}
+
+#endif
