57279b14eb41a9668a8eea650763b5538a553335
Wojtek Figat <wojtek@figat.pl>
2022-07-13 23:31:55 +0200

    Revert "Attempt to fix Android build"
    This reverts commit b80f2118bda2b0ae963ce4725ca0b9645910c14e.

--- Source/Engine/Main/Android/main.cpp
+++ Source/Engine/Main/Android/main.cpp
@@ -4,10 +4,8 @@
 
 #include "Engine/Engine/Engine.h"
 #include "Engine/Platform/Platform.h"
-extern "C" {
 #include <../../../../../../../sources/android/native_app_glue/android_native_app_glue.h>
 #include <../../../../../../../sources/android/native_app_glue/android_native_app_glue.c>
-}
 
 void android_main(android_app* app)
 {
