ad7a8e528a29738a0e56d816a50d6fa2a041bb98
Wojtek Figat <wojtek@figat.pl>
2022-08-06 12:10:14 +0200

    Fix tab navigation

--- Source/Engine/UI/GUI/Common/TextBoxBase.cs
+++ Source/Engine/UI/GUI/Common/TextBoxBase.cs
@@ -1282,6 +1282,9 @@ namespace FlaxEngine.GUI
                 SetSelection(TextLength);
                 return true;
             }
+            case KeyboardKeys.Tab:
+                // Don't process
+                return false;
             }
 
             return true;
