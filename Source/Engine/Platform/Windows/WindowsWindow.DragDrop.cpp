FlaxEngine/FlaxEngine

commit 4efd411045439dbc92ee39c6be43de0a5d039a70

Add improvements for objects spawning in editor viewport
#367

Source/Engine/Platform/Windows/WindowsWindow.DragDrop.cpp
@@ -10,6 +10,8 @@
 #include "Engine/Core/Collections/Array.h"
 #include "Engine/Engine/Engine.h"
 #include "Engine/Platform/IGuiData.h"
+#include "Engine/Input/Input.h"
+#include "Engine/Input/Mouse.h"
 #include "Engine/Threading/ThreadPoolTask.h"
 #include "Engine/Threading/ThreadPool.h"
 #include "Engine/Scripting/Scripting.h"
@@ -629,6 +631,10 @@ DragDropEffect WindowsWindow::DoDragDrop(const StringView& data)
     // Release allocated data
     dropSource->Release();
     ReleaseStgMedium(&stgmed);
+
+    // Fix hanging mouse state (Windows doesn't send WM_LBUTTONUP when we end the drag and drop)
+    if (Input::GetMouseButton(MouseButton::Left))
+        Input::Mouse->OnMouseUp(Input::Mouse->GetPosition(), MouseButton::Left, this);
 
     return SUCCEEDED(result) ? dropEffectFromOleEnum(dwEffect) : DragDropEffect::None;
 }

Source/Editor/Viewport/MainEditorGizmoViewport.cs
@@ -860,6 +860,13 @@ namespace FlaxEditor.Viewport
             return location;
         }
 
+        private void Spawn(Actor actor, ref Vector3 hitLocation)
+        {
+            actor.Position = PostProcessSpawnedActorLocation(actor, ref hitLocation);
+            Editor.Instance.SceneEditing.Spawn(actor);
+            Focus();
+        }
+
         private void Spawn(AssetItem item, SceneGraphNode hit, ref Vector2 location, ref Vector3 hitLocation)
         {
             if (item is AssetItem assetItem)
@@ -872,8 +879,7 @@ namespace FlaxEditor.Viewport
                         Name = item.ShortName,
                         ParticleSystem = asset
                     };
-                    actor.Position = PostProcessSpawnedActorLocation(actor, ref hitLocation);
-                    Editor.Instance.SceneEditing.Spawn(actor);
+                    Spawn(actor, ref hitLocation);
                     return;
                 }
                 if (assetItem.IsOfType<SceneAnimation>())
@@ -884,8 +890,7 @@ namespace FlaxEditor.Viewport
                         Name = item.ShortName,
                         Animation = asset
                     };
-                    actor.Position = PostProcessSpawnedActorLocation(actor, ref hitLocation);
-                    Editor.Instance.SceneEditing.Spawn(actor);
+                    Spawn(actor, ref hitLocation);
                     return;
                 }
                 if (assetItem.IsOfType<MaterialBase>())
@@ -921,8 +926,7 @@ namespace FlaxEditor.Viewport
                         Name = item.ShortName,
                         SkinnedModel = model
                     };
-                    actor.Position = PostProcessSpawnedActorLocation(actor, ref hitLocation);
-                    Editor.Instance.SceneEditing.Spawn(actor);
+                    Spawn(actor, ref hitLocation);
                     return;
                 }
                 if (assetItem.IsOfType<Model>())
@@ -933,8 +937,7 @@ namespace FlaxEditor.Viewport
                         Name = item.ShortName,
                         Model = model
                     };
-                    actor.Position = PostProcessSpawnedActorLocation(actor, ref hitLocation);
-                    Editor.Instance.SceneEditing.Spawn(actor);
+                    Spawn(actor, ref hitLocation);
                     return;
                 }
                 if (assetItem.IsOfType<AudioClip>())
@@ -945,8 +948,7 @@ namespace FlaxEditor.Viewport
                         Name = item.ShortName,
                         Clip = clip
                     };
-                    actor.Position = PostProcessSpawnedActorLocation(actor, ref hitLocation);
-                    Editor.Instance.SceneEditing.Spawn(actor);
+                    Spawn(actor, ref hitLocation);
                     return;
                 }
                 if (assetItem.IsOfType<Prefab>())
@@ -954,8 +956,7 @@ namespace FlaxEditor.Viewport
                     var prefab = FlaxEngine.Content.LoadAsync<Prefab>(item.ID);
                     var actor = PrefabManager.SpawnPrefab(prefab, null);
                     actor.Name = item.ShortName;
-                    actor.Position = PostProcessSpawnedActorLocation(actor, ref hitLocation);
-                    Editor.Instance.SceneEditing.Spawn(actor);
+                    Spawn(actor, ref hitLocation);
                     return;
                 }
                 if (assetItem.IsOfType<SceneAsset>())
@@ -967,8 +968,7 @@ namespace FlaxEditor.Viewport
                 {
                     var actor = (Actor)visualScriptItem.ScriptType.CreateInstance();
                     actor.Name = item.ShortName;
-                    actor.Position = PostProcessSpawnedActorLocation(actor, ref hitLocation);
-                    Editor.Instance.SceneEditing.Spawn(actor);
+                    Spawn(actor, ref hitLocation);
                     return;
                 }
             }

commit 8694d9f9b427c633df99fb9c67caa3b264b6725c

Fix helper mouse button up event on drag drop end on Windows
Regression from 4efd411045439dbc92ee39c6be43de0a5d039a70

Source/Engine/Platform/Windows/WindowsWindow.DragDrop.cpp
@@ -8,6 +8,7 @@
 
 #include "Engine/Core/Collections/Array.h"
 #include "Engine/Engine/Engine.h"
+#include "Engine/Engine/Screen.h"
 #include "Engine/Platform/IGuiData.h"
 #include "Engine/Input/Input.h"
 #include "Engine/Input/Mouse.h"
@@ -621,7 +622,11 @@ DragDropEffect WindowsWindow::DoDragDrop(const StringView& data)
 
     // Fix hanging mouse state (Windows doesn't send WM_LBUTTONUP when we end the drag and drop)
     if (Input::GetMouseButton(MouseButton::Left))
-        Input::Mouse->OnMouseUp(Input::Mouse->GetPosition(), MouseButton::Left, this);
+    {
+        ::POINT point;
+        ::GetCursorPos(&point);
+        Input::Mouse->OnMouseUp(Float2((float)point.x, (float)point.y), MouseButton::Left, this);
+    }
 
     return SUCCEEDED(result) ? dropEffectFromOleEnum(dwEffect) : DragDropEffect::None;
 }