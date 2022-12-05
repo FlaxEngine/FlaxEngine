1ec7bfb3f7ef6b798c9c601135474d5e7a577e18
Preben Eriksen <plemsoft@plemsoft.com>
2022-12-05 12:45:44 +0100

    PE: When upgrading material set default static fov.

--- Source/Engine/Content/Upgraders/ShaderAssetUpgrader.h
+++ Source/Engine/Content/Upgraders/ShaderAssetUpgrader.h
@@ -68,7 +68,8 @@ private:
             newHeader.Material.Info = oldHeader.Material.Info;
             newHeader.Material.Info.MinDepth = 0.0f;
             newHeader.Material.Info.MaxDepth = 1.0f;
-            for (int i = 0; i < 10; i++)
+            newHeader.Material.Info.StaticFOV = 0.0f;
+            for (int i = 0; i < 9; i++)
             {
                 newHeader.Material.Info.for_future_use[i] = -2.0f;
             }
