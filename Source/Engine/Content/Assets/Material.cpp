FlaxEngine/FlaxEngine

commit dbcb333ba932840b04841d2502ffedbcb0aba592

Fix crash when using material in PostFx global graphics settings (asset loaded before GPU init)

Source/Engine/Content/Assets/Material.cpp
@@ -5,6 +5,7 @@
 #include "Engine/Core/Types/DataContainer.h"
 #include "Engine/Content/Upgraders/ShaderAssetUpgrader.h"
 #include "Engine/Content/Factories/BinaryAssetFactory.h"
+#include "Engine/Graphics/GPUDevice.h"
 #include "Engine/Graphics/RenderTools.h"
 #include "Engine/Graphics/Materials/MaterialShader.h"
 #include "Engine/Graphics/Shaders/Cache/ShaderCacheManager.h"
@@ -123,6 +124,18 @@ Asset::LoadResult Material::load()
 {
     ASSERT(_materialShader == nullptr);
     FlaxChunk* materialParamsChunk;
+
+    // Wait for the GPU Device to be ready (eg. case when loading material before GPU init)
+#define IS_GPU_NOT_READY() (GPUDevice::Instance == nullptr || GPUDevice::Instance->GetState() != GPUDevice::DeviceState::Ready || GPUDevice::Instance->QuadShader == nullptr)
+    if (!IsInMainThread() && IS_GPU_NOT_READY())
+    {
+        int32 timeout = 1000;
+        while (IS_GPU_NOT_READY() && timeout-- > 0)
+            Platform::Sleep(1);
+        if (IS_GPU_NOT_READY())
+            return LoadResult::InvalidData;
+    }
+#undef IS_GPU_NOT_READY
 
     // Special case for Null renderer
     if (IsNullRenderer())

commit 0e1da59f0c425bb012ed7122d8fb7f67e5dd6046

Fix loading materials when using Null renderer
Regression from dbcb333ba932840b04841d2502ffedbcb0aba592

Source/Engine/Content/Assets/Material.cpp
@@ -126,7 +126,7 @@ Asset::LoadResult Material::load()
     FlaxChunk* materialParamsChunk;
 
     // Wait for the GPU Device to be ready (eg. case when loading material before GPU init)
-#define IS_GPU_NOT_READY() (GPUDevice::Instance == nullptr || GPUDevice::Instance->GetState() != GPUDevice::DeviceState::Ready || GPUDevice::Instance->QuadShader == nullptr)
+#define IS_GPU_NOT_READY() (GPUDevice::Instance == nullptr || GPUDevice::Instance->GetState() != GPUDevice::DeviceState::Ready)
     if (!IsInMainThread() && IS_GPU_NOT_READY())
     {
         int32 timeout = 1000;