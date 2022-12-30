a253e01dbd7b925869bd66c3ae482c9a909c96cd
Wojtek Figat <wojtek@figat.pl>
2022-12-31 00:21:28 +0100

    Add unit test for scripting event to ensure generated bindings code works fine

--- Source/Engine/Tests/TestScripting.h
+++ Source/Engine/Tests/TestScripting.h
@@ -3,7 +3,21 @@
 #pragma once
 
 #include "Engine/Core/ISerializable.h"
+#include "Engine/Core/Math/Vector3.h"
+#include "Engine/Core/Collections/Array.h"
 #include "Engine/Scripting/ScriptingObject.h"
+
+// Test structure.
+API_STRUCT(NoDefault) struct TestStruct : public ISerializable
+{
+    API_AUTO_SERIALIZATION();
+    DECLARE_SCRIPTING_TYPE_MINIMAL(TestStruct);
+
+    // Var
+    API_FIELD() Float3 Vector = Float3::One;
+    // Ref
+    API_FIELD() ScriptingObject* Object = nullptr;
+};
 
 // Test class.
 API_CLASS() class TestClassNative : public ScriptingObject, public ISerializable
@@ -15,8 +29,14 @@ public:
     // Test value
     API_FIELD() int32 SimpleField = 1;
 
+    // Test struct
+    API_FIELD() TestStruct SimpleStruct;
+
+    // Test event
+    API_EVENT() Delegate<int32, Float3, const String&, String&, const Array<TestStruct>&, Array<TestStruct>&> SimpleEvent;
+
     // Test virtual method
-    API_FUNCTION() virtual int32 Test(const String& str)
+    API_FUNCTION() virtual int32 TestMethod(const String& str)
     {
         return str.Length();
     }
