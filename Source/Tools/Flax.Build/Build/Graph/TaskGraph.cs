b1ecb2f0eb8e09a5c9a2c5f9c1d3bc70f49e4c85
Wojciech Figat <wojtek@figat.pl>
2023-01-11 14:27:37 +0100

    Fix `xcopy` path on Windows in `Flax.Build`

--- Source/Tools/Flax.Build/Build/Graph/TaskGraph.cs
+++ Source/Tools/Flax.Build/Build/Graph/TaskGraph.cs
@@ -82,7 +82,7 @@ namespace Flax.Build.Graph
             if (Platform.BuildPlatform.Target == TargetPlatform.Windows)
             {
                 task.CommandPath = "xcopy";
-                task.CommandArguments = string.Format("/y \"{0}\" \"{1}\"", srcFile, outputPath);
+                task.CommandArguments = string.Format("/y \"{0}\" \"{1}\"", srcFile.Replace('/', '\\'), outputPath.Replace('/', '\\'));
             }
             else
             {
