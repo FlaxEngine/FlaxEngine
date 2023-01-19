fb12e63bb13540c424e1c95a7ddc04189d6e7a1b
Chandler Cox <mr.chandlercox@gmail.com>
2023-01-19 15:57:51 -0600

    Fixed editor not saving added tags #885

--- Source/Editor/CustomEditors/Editors/TagEditor.cs
+++ Source/Editor/CustomEditors/Editors/TagEditor.cs
@@ -260,6 +260,8 @@ namespace FlaxEditor.CustomEditors.Editors
 
                     // Reload editor window to reflect new tag
                     assetWindow?.RefreshAsset();
+                    assetWindow?.MarkAsEdited();
+                    assetWindow?.Save();
                 }
             };
             dialog.Closed += popup =>
