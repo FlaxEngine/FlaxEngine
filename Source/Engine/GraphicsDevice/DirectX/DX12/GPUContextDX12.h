45a30990bade2963e78ad7a0a7c7b866f5de0cf5
Wojtek Figat <wojtek@figat.pl>
2023-02-01 11:07:48 +0100

    Add `GPUContext::SetBlendFactor`

--- Source/Engine/GraphicsDevice/DirectX/DX12/GPUContextDX12.h
+++ Source/Engine/GraphicsDevice/DirectX/DX12/GPUContextDX12.h
@@ -166,6 +166,7 @@ public:
     void SetRenderTarget(GPUTextureView* rt) override;
     void SetRenderTarget(GPUTextureView* depthBuffer, GPUTextureView* rt) override;
     void SetRenderTarget(GPUTextureView* depthBuffer, const Span<GPUTextureView*>& rts) override;
+    void SetBlendFactor(const Float4& value) override;
     void ResetSR() override;
     void ResetUA() override;
     void ResetCB() override;
