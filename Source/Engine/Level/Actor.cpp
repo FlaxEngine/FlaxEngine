FlaxEngine/FlaxEngine

commit a570d6d1781755917d986b901f6f12984b5eac80

Refactor **Actor tags into hierarchical reusable Tags** system for better gameplay scripting

Source/Engine/Level/Actor.cpp
@@ -73,13 +73,12 @@ Actor::Actor(const SpawnParams& params)
     , _isPrefabRoot(false)
     , _isEnabled(false)
     , _layer(0)
-    , _tag(ACTOR_TAG_INVALID)
-    , _scene(nullptr)
     , _staticFlags(StaticFlags::FullyStatic)
     , _localTransform(Transform::Identity)
     , _transform(Transform::Identity)
     , _sphere(BoundingSphere::Empty)
     , _box(BoundingBox::Zero)
+    , _scene(nullptr)
     , _physicsScene(nullptr)
     , HideFlags(HideFlags::None)
 {
@@ -439,23 +438,24 @@ void Actor::DestroyChildren(float timeLeft)
     }
 }
 
-bool Actor::HasTag(const StringView& tag) const
-{
-    return HasTag() && tag == Level::Tags[_tag];
-}
-
 const String& Actor::GetLayerName() const
 {
     return Level::Layers[_layer];
 }
 
-const String& Actor::GetTag() const
+bool Actor::HasTag() const
 {
-    if (HasTag())
-    {
-        return Level::Tags[_tag];
-    }
-    return String::Empty;
+    return Tags.Count() != 0;
+}
+
+bool Actor::HasTag(const Tag& tag) const
+{
+    return Tags.Contains(tag);
+}
+
+bool Actor::HasTag(const StringView& tag) const
+{
+    return Tags.Contains(tag);
 }
 
 void Actor::SetLayer(int32 layerIndex)
@@ -463,49 +463,8 @@ void Actor::SetLayer(int32 layerIndex)
     layerIndex = Math::Clamp(layerIndex, 0, 31);
     if (layerIndex == _layer)
         return;
-
     _layer = layerIndex;
     OnLayerChanged();
-}
-
-void Actor::SetTagIndex(int32 tagIndex)
-{
-    if (tagIndex == ACTOR_TAG_INVALID)
-    {
-    }
-    else if (Level::Tags.IsEmpty())
-    {
-        tagIndex = ACTOR_TAG_INVALID;
-    }
-    else
-    {
-        tagIndex = tagIndex < 0 ? ACTOR_TAG_INVALID : Math::Min(tagIndex, Level::Tags.Count() - 1);
-    }
-    if (tagIndex == _tag)
-        return;
-
-    _tag = tagIndex;
-    OnTagChanged();
-}
-
-void Actor::SetTag(const StringView& tagName)
-{
-    int32 tagIndex;
-    if (tagName.IsEmpty())
-    {
-        tagIndex = ACTOR_TAG_INVALID;
-    }
-    else
-    {
-        tagIndex = Level::Tags.Find(tagName);
-        if (tagIndex == -1)
-        {
-            LOG(Error, "Cannot change actor tag. Given value is invalid.");
-            return;
-        }
-    }
-
-    SetTagIndex(tagIndex);
 }
 
 void Actor::SetName(const StringView& value)
@@ -897,10 +856,21 @@ void Actor::Serialize(SerializeStream& stream, const void* otherObj)
     SERIALIZE_MEMBER(StaticFlags, _staticFlags);
     SERIALIZE(HideFlags);
     SERIALIZE_MEMBER(Layer, _layer);
-    if (!other || _tag != other->_tag)
+    if (!other || Tags != other->Tags)
     {
-        stream.JKEY("Tag");
-        stream.String(GetTag());
+        if (Tags.Count() == 1)
+        {
+            stream.JKEY("Tag");
+            stream.String(Tags.Get()->ToString());
+        }
+        else
+        {
+            stream.JKEY("Tags");
+            stream.StartArray();
+            for (auto& tag : Tags)
+                stream.String(tag.ToString());
+            stream.EndArray();
+        }
     }
 
     if (isPrefabDiff)
@@ -996,14 +966,27 @@ void Actor::Deserialize(DeserializeStream& stream, ISerializeModifier* modifier)
         _staticFlags |= StaticFlags::Navigation;
     }
 
-    // Resolve tag (it may be missing in the current configuration
     const auto tag = stream.FindMember("Tag");
     if (tag != stream.MemberEnd())
     {
         if (tag->value.IsString() && tag->value.GetStringLength())
         {
-            const String tagName = tag->value.GetText();
-            _tag = Level::GetOrAddTag(tagName);
+            Tags.Clear();
+            Tags.Add(Tags::Get(tag->value.GetText()));
+        }
+    }
+    else
+    {
+        const auto tags = stream.FindMember("Tags");
+        if (tags != stream.MemberEnd() && tags->value.IsArray())
+        {
+            Tags.Clear();
+            for (rapidjson::SizeType i = 0; i < tags->value.Size(); i++)
+            {
+                auto& e = tags->value[i];
+                if (e.IsString() && e.GetStringLength())
+                    Tags.Add(Tags::Get(e.GetText()));
+            }
         }
     }
 

Source/Engine/Level/Actor.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "SceneObject.h"
+#include "Tags.h"
 #include "Engine/Core/Types/Span.h"
 #include "Engine/Core/Math/Transform.h"
 #include "Engine/Core/Math/BoundingBox.h"
@@ -34,7 +35,6 @@ API_CLASS(Abstract) class FLAXENGINE_API Actor : public SceneObject
     friend SceneRendering;
     friend Prefab;
     friend PrefabInstanceData;
-
 protected:
     int16 _isActive : 1;
     int16 _isActiveInHierarchy : 1;
@@ -43,7 +43,6 @@ protected:
     int16 _drawNoCulling : 1;
     int16 _drawCategory : 4;
     byte _layer;
-    byte _tag;
     StaticFlags _staticFlags;
     Transform _localTransform;
     Transform _transform;
@@ -77,6 +76,11 @@ public:
     API_FIELD(Attributes="HideInEditor, NoSerialize")
     HideFlags HideFlags;
 
+    /// <summary>
+    /// Actor tags collection.
+    /// </summary>
+    API_FIELD(Attributes="NoAnimate, EditorDisplay(\"General\"), EditorOrder(-68)") Array<Tag> Tags;
+
 public:
     /// <summary>
     /// Gets the object layer (index). Can be used for selective rendering or ignoring raycasts.
@@ -96,26 +100,10 @@ public:
     }
 
     /// <summary>
-    /// Gets the actor tag (index). Can be used to identify the objects.
+    /// Sets the layer.
     /// </summary>
-    FORCE_INLINE int32 GetTagIndex() const
-    {
-        return _tag;
-    }
-
-    /// <summary>
-    /// Determines whether this actor has tag assigned.
-    /// </summary>
-    API_FUNCTION() FORCE_INLINE bool HasTag() const
-    {
-        return _tag != ACTOR_TAG_INVALID;
-    }
-
-    /// <summary>
-    /// Determines whether this actor has given tag assigned.
-    /// </summary>
-    /// <param name="tag">The tag to check.</param>
-    API_FUNCTION() bool HasTag(const StringView& tag) const;
+    /// <param name="layerIndex">The index of the layer.</param>
+    API_PROPERTY() void SetLayer(int32 layerIndex);
 
     /// <summary>
     /// Gets the name of the layer.
@@ -123,28 +111,21 @@ public:
     API_PROPERTY() const String& GetLayerName() const;
 
     /// <summary>
-    /// Gets the name of the tag.
+    /// Determines whether this actor has any tag assigned.
     /// </summary>
-    API_PROPERTY(Attributes="NoAnimate, EditorDisplay(\"General\"), EditorOrder(-68), CustomEditorAlias(\"FlaxEditor.CustomEditors.Editors.ActorTagEditor\")")
-    const String& GetTag() const;
+    API_FUNCTION() bool HasTag() const;
 
     /// <summary>
-    /// Sets the layer.
+    /// Determines whether this actor has given tag assigned.
     /// </summary>
-    /// <param name="layerIndex">The index of the layer.</param>
-    API_PROPERTY() void SetLayer(int32 layerIndex);
+    /// <param name="tag">The tag to check.</param>
+    API_FUNCTION() bool HasTag(const Tag& tag) const;
 
     /// <summary>
-    /// Sets the tag.
+    /// Determines whether this actor has given tag assigned.
     /// </summary>
-    /// <param name="tagIndex">The index of the tag.</param>
-    void SetTagIndex(int32 tagIndex);
-
-    /// <summary>
-    /// Sets the tag.
-    /// </summary>
-    /// <param name="tagName">Name of the tag.</param>
-    API_PROPERTY() void SetTag(const StringView& tagName);
+    /// <param name="tag">The tag to check.</param>
+    API_FUNCTION() bool HasTag(const StringView& tag) const;
 
     /// <summary>
     /// Gets the actor name.
@@ -952,13 +933,6 @@ public:
     /// Called when layer gets changed.
     /// </summary>
     virtual void OnLayerChanged()
-    {
-    }
-
-    /// <summary>
-    /// Called when tag gets changed.
-    /// </summary>
-    virtual void OnTagChanged()
     {
     }
 

Source/Editor/CustomEditors/Editors/ActorTagEditor.cs
@@ -1,57 +1,0 @@
-// Copyright (c) 2012-2022 Wojciech Figat. All rights reserved.
-
-using FlaxEditor.Content.Settings;
-using FlaxEditor.CustomEditors.Elements;
-using FlaxEditor.GUI;
-
-namespace FlaxEditor.CustomEditors.Editors
-{
-    /// <summary>
-    /// Custom editor for picking actor tag. Instead of choosing tag index or entering tag text it shows a combo box with simple tag picking by name.
-    /// </summary>
-    public sealed class ActorTagEditor : CustomEditor
-    {
-        private ComboBoxElement element;
-        private const string NoTagText = "Untagged";
-
-        /// <inheritdoc />
-        public override DisplayStyle Style => DisplayStyle.Inline;
-
-        /// <inheritdoc />
-        public override void Initialize(LayoutElementsContainer layout)
-        {
-            element = layout.ComboBox();
-            element.ComboBox.SelectedIndexChanged += OnSelectedIndexChanged;
-
-            // Set tag names
-            element.ComboBox.AddItem(NoTagText);
-            element.ComboBox.AddItems(LayersAndTagsSettings.GetCurrentTags());
-        }
-
-        private void OnSelectedIndexChanged(ComboBox comboBox)
-        {
-            string value = comboBox.SelectedItem;
-            if (value == NoTagText)
-                value = string.Empty;
-            SetValue(value);
-        }
-
-        /// <inheritdoc />
-        public override void Refresh()
-        {
-            base.Refresh();
-
-            if (HasDifferentValues)
-            {
-                // TODO: support different values on many actor selected
-            }
-            else
-            {
-                string value = (string)Values[0];
-                if (string.IsNullOrEmpty(value))
-                    value = NoTagText;
-                element.ComboBox.SelectedItem = value;
-            }
-        }
-    }
-}

Source/Editor/CustomEditors/Editors/CultureInfoEditor.cs
@@ -32,6 +32,7 @@ namespace FlaxEditor.CustomEditors.Editors
             {
                 Width = 16.0f,
                 Text = "...",
+                TooltipText = "Edit...",
                 Parent = _label,
             };
             button.SetAnchorPreset(AnchorPresets.MiddleRight, false, true);
@@ -81,30 +82,6 @@ namespace FlaxEditor.CustomEditors.Editors
             }
         }
 
-        private static void UpdateFilter(TreeNode node, string filterText)
-        {
-            // Update children
-            bool isAnyChildVisible = false;
-            for (int i = 0; i < node.Children.Count; i++)
-            {
-                if (node.Children[i] is TreeNode child)
-                {
-                    UpdateFilter(child, filterText);
-                    isAnyChildVisible |= child.Visible;
-                }
-            }
-
-            // Update itself
-            bool noFilter = string.IsNullOrWhiteSpace(filterText);
-            bool isThisVisible = noFilter || QueryFilterHelper.Match(filterText, node.Text);
-            bool isExpanded = isAnyChildVisible;
-            if (isExpanded)
-                node.Expand(true);
-            else
-                node.Collapse(true);
-            node.Visible = isThisVisible | isAnyChildVisible;
-        }
-
         private void ShowPicker()
         {
             var menu = CreatePicker(Culture, value => { Culture = value; });
@@ -147,8 +124,7 @@ namespace FlaxEditor.CustomEditors.Editors
                 if (tree.IsLayoutLocked)
                     return;
                 root.LockChildrenRecursive();
-                var query = searchBox.Text;
-                UpdateFilter(root, query);
+                Utilities.Utils.UpdateSearchPopupFilter(root, searchBox.Text);
                 root.UnlockChildrenRecursive();
                 menu.PerformLayout();
             };

commit dc04d3dc0b3186a1aafaaa4c1eac9130945721af

Restore `Actor.Tag` property as deprecated for backwards compatibility
Regression from a570d6d1781755917d986b901f6f12984b5eac80

Source/Engine/Level/Actor.cpp
@@ -471,6 +471,21 @@ bool Actor::HasTag(const StringView& tag) const
     return Tags.Contains(tag);
 }
 
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+const String& Actor::GetTag() const
+{
+    return Tags.Count() != 0 ? Tags[0].ToString() : String::Empty;
+}
+
+void Actor::SetTag(const StringView& value)
+{
+    const Tag tag = Tags::Get(value);
+    Tags.Set(&tag, 1);
+}
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
 void Actor::SetLayer(int32 layerIndex)
 {
     layerIndex = Math::Clamp(layerIndex, 0, 31);

Source/Engine/Level/Actor.h
@@ -131,6 +131,19 @@ public:
     API_FUNCTION() bool HasTag(const StringView& tag) const;
 
     /// <summary>
+    /// Gets the name of the tag.
+    /// [Deprecated in v1.5]
+    /// </summary>
+    API_PROPERTY(Attributes="HideInEditor, NoSerialize, NoAnimate")
+    DEPRECATED const String& GetTag() const;
+
+    /// <summary>
+    /// Sets the name of the tag.
+    /// [Deprecated in v1.5]
+    /// </summary>
+    API_PROPERTY() DEPRECATED void SetTag(const StringView& value);
+
+    /// <summary>
     /// Gets the actor name.
     /// </summary>
     API_PROPERTY(Attributes="NoAnimate, EditorDisplay(\"General\"), EditorOrder(-100)")