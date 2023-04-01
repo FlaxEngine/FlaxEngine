0d082f5773d4d5f7f67e06bbe001dc0c253bc4d9
Chandler Cox <mr.chandlercox@gmail.com>
2023-04-01 17:23:00 -0500

    Added ability to change the fps of the not focused editor window.

--- Source/Editor/States/EditorState.cs
+++ Source/Editor/States/EditorState.cs
@@ -93,12 +93,13 @@ namespace FlaxEditor.States
         /// </summary>
         public virtual void UpdateFPS()
         {
-            var editorFps = Editor.Options.Options.General.EditorFPS;
+            var generalOptions = Editor.Options.Options.General;
+            var editorFps = generalOptions.EditorFPS;
             if (!Platform.HasFocus)
             {
                 // Drop performance if app has no focus
-                Time.DrawFPS = 15;
-                Time.UpdateFPS = 15;
+                Time.DrawFPS = generalOptions.EditorFPSWhenNotFocused;
+                Time.UpdateFPS = generalOptions.EditorFPSWhenNotFocused;
             }
             else if (editorFps < 1)
             {
