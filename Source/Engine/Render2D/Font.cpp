FlaxEngine/FlaxEngine

commit e8aa2f922b41864f45749950ab75a86a5d91dc6e

Allow word wrapping to wrap on capital letters and underscores.

Source/Engine/Render2D/Font.cpp
@@ -122,6 +122,12 @@ void Font::ProcessText(const StringView& text, Array<FontLineCache>& outputLines
     float lastWhitespaceX = 0;
     bool lastMoveLine = false;
 
+    int32 lastUpperIndex = INVALID_INDEX;
+    float lastUpperX = 0;
+    
+    int32 lastUnderscoreIndex = INVALID_INDEX;
+    float lastUnderscoreX = 0;
+
     // Process each character to split text into single lines
     for (int32 currentIndex = 0; currentIndex < textLength;)
     {
@@ -139,6 +145,21 @@ void Font::ProcessText(const StringView& text, Array<FontLineCache>& outputLines
             // Cache line break point
             lastWhitespaceIndex = currentIndex;
             lastWhitespaceX = cursorX;
+        }
+
+        // Check if character is an upper case letter
+        const bool isUpper = StringUtils::IsUpper(currentChar);
+        if (isUpper && currentIndex != 0)
+        {
+            lastUpperIndex = currentIndex;
+            lastUpperX = cursorX;
+        }
+
+        const bool isUnderscore = currentChar == '_';
+        if (isUnderscore)
+        {
+            lastUnderscoreIndex = currentIndex;
+            lastUnderscoreX = cursorX;
         }
 
         // Check if it's a newline character
@@ -185,6 +206,20 @@ void Font::ProcessText(const StringView& text, Array<FontLineCache>& outputLines
                     currentIndex = lastWhitespaceIndex + 1;
                     nextCharIndex = currentIndex;
                 }
+                else if (lastUpperIndex != INVALID_INDEX)
+                {
+                    cursorX = lastUpperX;
+                    tmpLine.LastCharIndex = lastUpperIndex - 1;
+                    currentIndex = lastUpperIndex;
+                    nextCharIndex = currentIndex;
+                }
+                else if (lastUnderscoreIndex != INVALID_INDEX)
+                {
+                    cursorX = lastUnderscoreX;
+                    tmpLine.LastCharIndex = lastUnderscoreIndex;
+                    currentIndex = lastUnderscoreIndex + 1;
+                    nextCharIndex = currentIndex;
+                }
                 else
                 {
                     nextCharIndex = currentIndex;
@@ -223,6 +258,12 @@ void Font::ProcessText(const StringView& text, Array<FontLineCache>& outputLines
 
             lastWhitespaceIndex = INVALID_INDEX;
             lastWhitespaceX = 0;
+
+            lastUpperIndex = INVALID_INDEX;
+            lastUpperX = 0;
+
+            lastUnderscoreIndex = INVALID_INDEX;
+            lastUnderscoreX = 0;
 
             previous.IsValid = false;
         }

commit 9e222d222c020b558c5f27d1d9ae3d18c2655e90

Fix dead-lock in characters layout calc regression from e8aa2f922b41864f45749950ab75a86a5d91dc6e

Source/Engine/Render2D/Font.cpp
@@ -211,7 +211,7 @@ void Font::ProcessText(const StringView& text, Array<FontLineCache>& outputLines
                 {
                     cursorX = lastUpperX;
                     tmpLine.LastCharIndex = lastUpperIndex - 1;
-                    currentIndex = lastUpperIndex;
+                    currentIndex = lastUpperIndex + 1;
                     nextCharIndex = currentIndex;
                 }
                 else if (lastUnderscoreIndex != INVALID_INDEX)