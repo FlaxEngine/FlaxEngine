385ef7361c803c51fefac8616bc5fb3431fbd798
Wojtek Figat <wojtek@figat.pl>
2023-04-28 23:55:11 +0200

    Fix file size on unix systems if file is missing

--- Source/Engine/Platform/Android/AndroidFileSystem.cpp
+++ Source/Engine/Platform/Android/AndroidFileSystem.cpp
@@ -249,13 +249,13 @@ bool AndroidFileSystem::DeleteFile(const StringView& path)
 uint64 AndroidFileSystem::GetFileSize(const StringView& path)
 {
     struct stat fileInfo;
-    fileInfo.st_size = -1;
+    fileInfo.st_size = 0;
     const StringAsANSI<> pathANSI(*path, path.Length());
     if (stat(pathANSI.Get(), &fileInfo) != -1)
     {
         if (S_ISDIR(fileInfo.st_mode))
         {
-            fileInfo.st_size = -1;
+            fileInfo.st_size = 0;
         }
     }
     else
