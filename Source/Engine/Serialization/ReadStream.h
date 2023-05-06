FlaxEngine/FlaxEngine

commit 5d2a3482c770abb9ee522d6bd39dce1a62f77cfc

Reduce compilation problems by moving `FindObject` fwd decl to be more commonly included
#1040

Source/Engine/Serialization/ReadStream.h
@@ -5,8 +5,6 @@
 #include "Stream.h"
 #include "Engine/Core/Templates.h"
 
-extern FLAXENGINE_API class ScriptingObject* FindObject(const Guid& id, class MClass* type);
-
 /// <summary>
 /// Base class for all data read streams
 /// </summary>

Source/Engine/Scripting/ScriptingObject.h
@@ -267,3 +267,5 @@ API_CLASS(InBuild) class FLAXENGINE_API PersistentScriptingObject : public Scrip
 public:
     PersistentScriptingObject(const SpawnParams& params);
 };
+
+extern FLAXENGINE_API class ScriptingObject* FindObject(const Guid& id, class MClass* type);

Source/Engine/Serialization/Serialization.h
@@ -456,7 +456,7 @@ namespace Serialization
         Guid id;
         Deserialize(stream, id, modifier);
 		modifier->IdsMapping.TryGet(id, id);
-        v = (T*)FindObject(id, T::GetStaticClass());
+        v = (T*)::FindObject(id, T::GetStaticClass());
     }
 
     // Scripting Object Reference

commit efab20e3359c716c08d6d44ced9bcf1cc6b4b3d5

Fix compilation regression from 5d2a3482c770abb9ee522d6bd39dce1a62f77cfc

Source/Engine/Serialization/ReadStream.h
@@ -5,6 +5,8 @@
 #include "Stream.h"
 #include "Engine/Core/Templates.h"
 
+extern FLAXENGINE_API class ScriptingObject* FindObject(const Guid& id, class MClass* type);
+
 /// <summary>
 /// Base class for all data read streams
 /// </summary>