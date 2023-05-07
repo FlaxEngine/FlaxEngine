FlaxEngine/FlaxEngine

commit dbd5c713445a11f8a377eefffa3467fb451af204

Fix RMB deletion to take selection into account

Source/Editor/Windows/ContentWindow.cs
@@ -485,7 +485,7 @@ namespace FlaxEditor.Windows
         /// <param name="item">The item to delete.</param>
         public void Delete(ContentItem item)
         {
-            Delete(new List<ContentItem>(1) { item });
+            Delete(Editor.Instance.Windows.ContentWin.View.Selection);
         }
 
         /// <summary>

commit 159fb8f5eec7e1b4d1c65b4aeb02271a44d01c82

Fix folders delete regression issue from dbd5c713445a11f8a377eefffa3467fb451af204
#1084

Source/Editor/Windows/ContentWindow.cs
@@ -488,7 +488,10 @@ namespace FlaxEditor.Windows
         /// <param name="item">The item to delete.</param>
         public void Delete(ContentItem item)
         {
-            Delete(Editor.Instance.Windows.ContentWin.View.Selection);
+            var items = View.Selection;
+            if (items.Count == 0)
+                items = new List<ContentItem>() { item };
+            Delete(items);
         }
 
         /// <summary>