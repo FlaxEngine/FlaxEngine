a2d4207504b65c4abaeec97950befb0d3519f429
Menotdan <32620310+Menotdan@users.noreply.github.com>
2023-05-10 19:30:58 -0400

    Fix compile errors.

--- Source/Editor/Content/Items/ContentFolder.cs
+++ Source/Editor/Content/Items/ContentFolder.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using FlaxEditor.GUI.Drag;
 using FlaxEngine;
 using FlaxEngine.GUI;
--- Source/Editor/Content/Items/ContentItem.cs
+++ Source/Editor/Content/Items/ContentItem.cs
@@ -2,6 +2,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using FlaxEditor.Content.GUI;
 using FlaxEditor.GUI.Drag;
 using FlaxEngine;
