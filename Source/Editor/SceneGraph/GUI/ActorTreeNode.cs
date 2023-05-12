FlaxEngine/FlaxEngine

commit d813078e91062f031520ec55d8745a81b4519171

Made it so the visuals of dragging an actor will only show between the nodes and on the node.

Source/Editor/SceneGraph/GUI/ActorTreeNode.cs
@@ -706,6 +706,8 @@ namespace FlaxEditor.SceneGraph.GUI
         {
             DragData data;
             var tree = ParentTree;
+            if (tree.Selection.Count == 1)
+                Select();
 
             // Check if this node is selected
             if (tree.Selection.Contains(this))

Source/Editor/GUI/Tree/Tree.cs
@@ -41,6 +41,11 @@ namespace FlaxEditor.GUI.Tree
         private bool _autoSize = true;
 
         /// <summary>
+        /// The TreeNode that is being dragged over. This could have a value when not dragging.
+        /// </summary>
+        public TreeNode DraggedOverNode = null;
+
+        /// <summary>
         /// Action fired when tree nodes selection gets changed.
         /// </summary>
         public event SelectionChangedDelegate SelectedChanged;

Source/Editor/GUI/Tree/TreeNode.cs
@@ -659,7 +659,7 @@ namespace FlaxEditor.GUI.Tree
             Render2D.DrawText(TextFont.GetFont(), _text, textRect, _cachedTextColor, TextAlignment.Near, TextAlignment.Center);
 
             // Draw drag and drop effect
-            if (IsDragOver)
+            if (IsDragOver && _tree.DraggedOverNode == this)
             {
                 Color dragOverColor = style.BackgroundSelected * 0.6f;
                 Rectangle rect;
@@ -669,10 +669,10 @@ namespace FlaxEditor.GUI.Tree
                     rect = textRect;
                     break;
                 case DragItemPositioning.Above:
-                    rect = new Rectangle(textRect.X, textRect.Y - DefaultDragInsertPositionMargin - DefaultNodeOffsetY, textRect.Width, DefaultDragInsertPositionMargin * 2.0f);
+                    rect = new Rectangle(textRect.X, textRect.Top - DefaultDragInsertPositionMargin - DefaultNodeOffsetY - _margin.Top, textRect.Width, DefaultDragInsertPositionMargin * 2.0f);
                     break;
                 case DragItemPositioning.Below:
-                    rect = new Rectangle(textRect.X, textRect.Bottom - DefaultDragInsertPositionMargin, textRect.Width, DefaultDragInsertPositionMargin * 2.0f);
+                    rect = new Rectangle(textRect.X, textRect.Bottom + _margin.Bottom - DefaultDragInsertPositionMargin, textRect.Width, DefaultDragInsertPositionMargin * 2.0f);
                     break;
                 default:
                     rect = Rectangle.Empty;
@@ -922,6 +922,7 @@ namespace FlaxEditor.GUI.Tree
             if (result == DragDropEffect.None)
             {
                 UpdateDrawPositioning(ref location);
+                _tree.DraggedOverNode = this;
 
                 // Check if mouse is over header
                 _isDragOverHeader = TestHeaderHit(ref location);

commit 1af69421fa5c9bb36238c215f4644044aadfee29

Fix actor scene tree dragging regression from d813078e91062f031520ec55d8745a81b4519171
#1104

Source/Editor/SceneGraph/GUI/ActorTreeNode.cs
@@ -729,8 +729,6 @@ namespace FlaxEditor.SceneGraph.GUI
         {
             DragData data;
             var tree = ParentTree;
-            if (tree.Selection.Count == 1)
-                Select();
 
             // Check if this node is selected
             if (tree.Selection.Contains(this))