7a6c1b8b86a2095d72a1ca718daca21433077dc0
Menotdan <aaron.rex08@gmail.com>
2023-05-17 20:59:42 -0400

    Add linux implementation definitions.

---
+++ Source/Editor/Utilities/ScreenUtilities/ScreenUtilitiesLinux.cpp
@@ -0,0 +1,32 @@
+#if PLATFORM_LINUX
+
+#include "ScreenUtilities.h"
+#include "Engine/Core/Math/Color32.h"
+#include "Engine/Core/Math/Vector2.h"
+#include "Engine/Core/Delegate.h"
+#include "Engine/Core/Log.h"
+
+Color32 ScreenUtilities::GetPixelAt(int32 x, int32 y)
+{
+}
+
+Int2 ScreenUtilities::GetScreenCursorPosition()
+{
+}
+
+class ScreenUtilitiesLinux
+{
+public:
+};
+
+void ScreenUtilities::BlockAndReadMouse()
+{
+}
+
+Delegate<Color32> ScreenUtilities::PickColorDone;
+
+void ScreenUtilities::PickColor()
+{
+}
+
+#endif
