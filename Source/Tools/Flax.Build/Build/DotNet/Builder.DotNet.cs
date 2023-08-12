3df044d07bd73888ca5aa4154419bf61ce4a976c
Ari Vuollet <ari.vuollet@kapsi.fi>
2023-08-12 14:34:06 +0300

    Add build option to change code optimization level in C# modules

--- Source/Tools/Flax.Build/Build/DotNet/Builder.DotNet.cs
+++ Source/Tools/Flax.Build/Build/DotNet/Builder.DotNet.cs
@@ -255,11 +255,13 @@ namespace Flax.Build
 #endif
             if (buildOptions.ScriptingAPI.IgnoreMissingDocumentationWarnings)
                 args.Add("-nowarn:1591");
-#if USE_NETCORE
+
             // Optimizations prevent debugging, only enable in release builds
-            args.Add(buildData.Configuration == TargetConfiguration.Release ? "/optimize+" : "/optimize-");
-#else
-            args.Add(buildData.Configuration == TargetConfiguration.Debug ? "/optimize-" : "/optimize+");
+            var optimize = buildData.Configuration == TargetConfiguration.Release;
+            if (buildData.TargetOptions.ScriptingAPI.Optimization.HasValue)
+                optimize = buildData.TargetOptions.ScriptingAPI.Optimization.Value;
+            args.Add(optimize ? "/optimize+" : "/optimize-");
+#if !USE_NETCORE
             args.Add(string.Format("/reference:\"{0}mscorlib.dll\"", referenceAssemblies));
 #endif
             args.Add(string.Format("/out:\"{0}\"", outputFile));
