74acc93c0cf23e4109f86e03c3efabe1db88fc10
Red Kitsune <krakers114pl@gmail.com>
2023-09-21 01:03:35 +0200

    Add XML comments

--- Source/Engine/Level/Components/MissingScript.h
+++ Source/Engine/Level/Components/MissingScript.h
@@ -5,23 +5,38 @@
 #include "Engine/Scripting/ScriptingObjectReference.h"
 #include "Engine/Serialization/JsonWriters.h"
 
+/// <summary>
+/// Actor script component that represents missing script.
+/// </summary>
 API_CLASS(Attributes="HideInEditor") class FLAXENGINE_API MissingScript : public Script
 {
     API_AUTO_SERIALIZATION();
     DECLARE_SCRIPTING_TYPE(MissingScript);
 
 public:
-    
+
+    /// <summary>
+    /// Namespace and type name of missing script.
+    /// </summary>
     API_FIELD(Attributes="ReadOnly") String MissingTypeName;
-    
+
+    /// <summary>
+    /// Missing script serialized data.
+    /// </summary>
     API_FIELD(Hidden, Attributes="HideInEditor") String Data;
-    
+
+    /// <summary>
+    /// Field for assigning new script to transfer data to.
+    /// </summary>
     API_PROPERTY()
     FORCE_INLINE ScriptingObjectReference<Script> GetReferenceScript() const
     {
         return _referenceScript;
     }
-    
+
+    /// <summary>
+    /// Field for assigning new script to transfer data to.
+    /// </summary>
     API_PROPERTY()
     void SetReferenceScript(ScriptingObjectReference<Script> value)
     {
