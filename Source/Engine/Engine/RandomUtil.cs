65d682acab6ef90f9839b64469d1e74a31540b32
DevHrytsan <3axapHrytsan@gmail.com>
2023-09-21 19:21:40 +0300

    Update RandomUtil.cs

--- Source/Engine/Engine/RandomUtil.cs
+++ Source/Engine/Engine/RandomUtil.cs
@@ -24,5 +24,80 @@ namespace FlaxEngine
         {
             return Random.Next(0, int.MaxValue) / (float)int.MaxValue;
         }
+
+        /// <summary>
+        ///Generates a random number within a specified range.
+        /// </summary>
+        /// <param name="min">The minimum value of the range.</param>
+        /// <param name="max">The maximum value of the range.</param>
+        /// <returns>The random number.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float RandRange(float min, float max)
+        {
+            return Rand() * (max - min) + min;
+        }
+
+        /// <summary>
+        ///Generates a random number within a specified range.
+        /// </summary>
+        /// <param name="min">The minimum value of the range.</param>
+        /// <param name="max">The maximum value of the range.</param>
+        /// <returns>The random number.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double RandRange(double min, double max)
+        {
+            return Random.NextDouble() * (max - min) + min;
+        }
+
+        /// <summary>
+        /// Generates a random number within a specified range.
+        /// </summary>
+        /// <param name="min">The minimum value of the range.</param>
+        /// <param name="max">The maximum value of the range.</param>
+        /// <returns>The random number.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int RandRange(int min, int max)
+        {
+            return Random.Next(min, max);
+        }
+
+        /// <summary>
+        /// Generates a random point inside a sphere with radius 1.0.
+        /// </summary>
+        /// <returns>A random point within the sphere.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Float3 InsideSphere()
+        {
+            float u = RandRange(-1, 1); 
+            float v = RandRange(-1, 1); 
+            float w = RandRange(-1, 1); 
+
+            // Normalize the vector to ensure it's inside the unit sphere
+            float length = Mathf.Sqrt(u * u + v * v + w * w);
+            u /= length;
+            v /= length;
+            w /= length;
+
+            return new Float3(u, v, w);
+        }
+
+        /// <summary>
+        /// Generates a random point on the surface of a sphere with radius 1.0.
+        /// </summary>
+        /// <returns>A random point on the sphere's surface.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Float3 OnSphere()
+        {
+            float phi = Rand() * 2f * Mathf.Pi;
+            float cosTheta = Rand() * 2f - 1f;
+            float sinTheta = Mathf.Sqrt(1f - cosTheta * cosTheta);
+
+            float x = Mathf.Cos(phi) * sinTheta;
+            float y = Mathf.Sin(phi) * sinTheta;
+            float z = cosTheta;
+
+            return new Float3(x, y, z);
+        }
+
     }
 }
