2c4b491e0148389a892e644d2aed28c0af4d141f
Olly Rybak <ollyrybak@gmail.com>
2023-09-24 23:21:24 +1000

    Added UI dropdown to serialized field/property

--- Source/Editor/CustomEditors/Editors/InputEditor.cs
+++ Source/Editor/CustomEditors/Editors/InputEditor.cs
@@ -64,6 +64,63 @@ namespace FlaxEditor.CustomEditors.Editors
     }
 
     /// <summary>
+    /// Default implementation of the inspector used to edit input event properties.
+    /// </summary>
+    [CustomEditor(typeof(InputEventState)), DefaultEditor]
+    public class InputEventStateEditor : CustomEditor
+    {
+        private Dropdown _dropdown;
+
+        /// <inheritdoc />
+        public override DisplayStyle Style => DisplayStyle.Inline;
+
+        /// <inheritdoc />
+        public override void Initialize(LayoutElementsContainer layout)
+        {
+            var dropdownElement = layout.Custom<Dropdown>();
+            _dropdown = dropdownElement.CustomControl;
+            var names = new List<LocalizedString>();
+            foreach (var mapping in Input.ActionMappings)
+            {
+                if (!names.Contains(mapping.Name))
+                    names.Add(mapping.Name);
+            }
+            _dropdown.Items = names;
+            if (Values[0] is InputEventState inputEvent && names.Contains(inputEvent.Name))
+                _dropdown.SelectedItem = inputEvent.Name;
+            _dropdown.SelectedIndexChanged += OnSelectedIndexChanged;
+        }
+
+        private void OnSelectedIndexChanged(Dropdown dropdown)
+        {
+            SetValue(new InputEventState(dropdown.SelectedItem));
+        }
+
+        /// <inheritdoc />
+        public override void Refresh()
+        {
+            base.Refresh();
+
+            if (HasDifferentValues)
+            {
+            }
+            else
+            {
+                if (Values[0] is InputEventState inputEvent && _dropdown.Items.Contains(inputEvent.Name))
+                    _dropdown.SelectedItem = inputEvent.Name;
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Deinitialize()
+        {
+            if (_dropdown != null)
+                _dropdown.SelectedIndexChanged -= OnSelectedIndexChanged;
+            _dropdown = null;
+        }
+    }
+
+    /// <summary>
     /// Default implementation of the inspector used to edit input axis properties.
     /// </summary>
     [CustomEditor(typeof(InputAxis)), DefaultEditor]
