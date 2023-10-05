FlaxEngine/FlaxEngine

commit fd679f0af59eb73244c39cb69107faf93b9358b9

Add automatic game settings apply when saving json file in Editor
#1440

Source/Engine/Content/JsonAsset.cpp
@@ -12,6 +12,7 @@
 #include "FlaxEngine.Gen.h"
 #include "Cache/AssetsCache.h"
 #include "Engine/Core/Log.h"
+#include "Engine/Core/Config/Settings.h"
 #include "Engine/Serialization/JsonTools.h"
 #include "Engine/Serialization/JsonWriters.h"
 #include "Engine/Content/Factories/JsonAssetFactory.h"
@@ -126,8 +127,7 @@ void FindIds(ISerializable::DeserializeStream& node, Array<Guid>& output)
     }
     else if (node.IsString())
     {
-        const auto length = node.GetStringLength();
-        if (length == 32)
+        if (node.GetStringLength() == 32)
         {
             // Try parse as Guid in format `N` (32 hex chars)
             Guid id;
@@ -362,6 +362,7 @@ void JsonAsset::unload(bool isReloading)
 #endif
     Scripting::ScriptsUnload.Unbind<JsonAsset, &JsonAsset::DeleteInstance>(this);
     DeleteInstance();
+    _isAfterReload |= isReloading;
 
     JsonAssetBase::unload(isReloading);
 }
@@ -406,6 +407,13 @@ bool JsonAsset::CreateInstance()
             break;
         }
         }
+    }
+
+    // Special case for Settings assets to flush them after edited and saved in Editor
+    if (typeHandle && typeHandle.IsSubclassOf(SettingsBase::TypeInitializer) && _isAfterReload)
+    {
+        _isAfterReload = false;
+        ((SettingsBase*)Instance)->Apply();
     }
 
     return false;

Source/Engine/Content/JsonAsset.h
@@ -118,6 +118,7 @@ API_CLASS(NoSpawn) class FLAXENGINE_API JsonAsset : public JsonAssetBase
     DECLARE_ASSET_HEADER(JsonAsset);
 private:
     ScriptingType::Dtor _dtor;
+    bool _isAfterReload = false;
 
 public:
     /// <summary>

commit 86d1d298206697b80ef83bd75eb93faf2762e2de

Fix deadlock regression from fd679f0af59eb73244c39cb69107faf93b9358b9 when applying Game Settings itself

Source/Engine/Content/Asset.h
@@ -227,7 +227,7 @@ protected:
 
     bool onLoad(LoadAssetTask* task);
     void onLoaded();
-    void onLoaded_MainThread();
+    virtual void onLoaded_MainThread();
     virtual void onUnload_MainThread();
 #if USE_EDITOR
     virtual void onRename(const StringView& newPath) = 0;

Source/Engine/Content/JsonAsset.cpp
@@ -367,6 +367,20 @@ void JsonAsset::unload(bool isReloading)
     JsonAssetBase::unload(isReloading);
 }
 
+void JsonAsset::onLoaded_MainThread()
+{
+    JsonAssetBase::onLoaded_MainThread();
+
+    // Special case for Settings assets to flush them after edited and saved in Editor
+    const StringAsANSI<> dataTypeNameAnsi(DataTypeName.Get(), DataTypeName.Length());
+    const auto typeHandle = Scripting::FindScriptingType(StringAnsiView(dataTypeNameAnsi.Get(), DataTypeName.Length()));
+    if (Instance && typeHandle && typeHandle.IsSubclassOf(SettingsBase::TypeInitializer) && _isAfterReload)
+    {
+        _isAfterReload = false;
+        ((SettingsBase*)Instance)->Apply();
+    }
+}
+
 bool JsonAsset::CreateInstance()
 {
     ScopeLock lock(Locker);
@@ -407,13 +421,6 @@ bool JsonAsset::CreateInstance()
             break;
         }
         }
-    }
-
-    // Special case for Settings assets to flush them after edited and saved in Editor
-    if (typeHandle && typeHandle.IsSubclassOf(SettingsBase::TypeInitializer) && _isAfterReload)
-    {
-        _isAfterReload = false;
-        ((SettingsBase*)Instance)->Apply();
     }
 
     return false;

Source/Engine/Content/JsonAsset.h
@@ -150,6 +150,7 @@ protected:
     // [JsonAssetBase]
     LoadResult loadAsset() override;
     void unload(bool isReloading) override;
+    void onLoaded_MainThread() override;
 
 private:
     bool CreateInstance();