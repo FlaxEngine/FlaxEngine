56c3080b10ab44a585ede8d257c718cc38090173
Ruan Lucas <79365912+RuanLucasGD@users.noreply.github.com>
2023-10-07 11:06:23 -0400

    fix edit text box

--- Source/Engine/UI/GUI/Common/TextBoxBase.cs
+++ Source/Engine/UI/GUI/Common/TextBoxBase.cs
@@ -769,13 +769,13 @@ namespace FlaxEngine.GUI
             {
                 SetSelection(SelectionLeft);
             }
-            else if (SelectionLeft > 0)
+            else if (SelectionLeft >= 0)
             {
                 int position;
                 if (ctrl)
                     position = FindPrevWordBegin();
                 else
-                    position = _selectionEnd - 1;
+                    position = Mathf.Max(_selectionEnd - 1, 0);
 
                 if (shift)
                 {
@@ -1302,7 +1302,7 @@ namespace FlaxEngine.GUI
                 MoveRight(shiftDown, ctrDown);
                 return true;
             case KeyboardKeys.ArrowLeft:
-                MoveLeft(shiftDown, ctrDown);
+                    MoveLeft(shiftDown, ctrDown);
                 return true;
             case KeyboardKeys.ArrowUp:
                 MoveUp(shiftDown, ctrDown);
