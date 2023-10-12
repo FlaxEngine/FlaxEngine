FlaxEngine/FlaxEngine

commit 6f7f22eb47a1687295c58436925dcf9780008e16

Add outputting shader source code in Editor to text file when compilation fails for debugging

Source/Engine/ShadersCompilation/ShadersCompilation.cpp
@@ -143,9 +143,20 @@ bool ShadersCompilation::Compile(ShaderCompilationOptions& options)
 #endif
     }
 
-    // Print info if succeed
-    if (result == false)
+    if (result)
     {
+        // Output shader source to easily investigate errors (eg. for generated shaders like materials or particles)
+        const String outputSourceFolder = Globals::ProjectCacheFolder / TEXT("/Shaders/Source");
+        const String outputSourcePath = outputSourceFolder / options.TargetName + TEXT(".hlsl");
+        if (!FileSystem::DirectoryExists(outputSourceFolder))
+            FileSystem::CreateDirectory(outputSourceFolder);
+        File::WriteAllBytes(outputSourcePath, (const byte*)options.Source, options.SourceLength);
+        LOG(Error, "Shader compilation '{0}' failed (profile: {1})", options.TargetName, ::ToString(options.Profile));
+        LOG(Error, "Source: {0}", outputSourcePath);
+    }
+    else
+    {
+        // Success
         const DateTime endTime = DateTime::NowUTC();
         LOG(Info, "Shader compilation '{0}' succeed in {1} ms (profile: {2})", options.TargetName, Math::CeilToInt(static_cast<float>((endTime - startTime).GetTotalMilliseconds())), ::ToString(options.Profile));
     }

commit 677cd4e382bc7c8af5d1fa006f4d621bc5e66820

Fix build regression from 6f7f22eb47a1687295c58436925dcf9780008e16

Source/Engine/ShadersCompilation/ShadersCompilation.cpp
@@ -28,6 +28,8 @@
 #include "Engine/ContentImporters/AssetsImportingManager.h"
 #include "Engine/Platform/FileSystemWatcher.h"
 #include "Engine/Platform/FileSystem.h"
+#include "Engine/Platform/File.h"
+#include "Engine/Engine/Globals.h"
 #include "Editor/Editor.h"
 #include "Editor/ProjectInfo.h"
 #endif
@@ -145,6 +147,7 @@ bool ShadersCompilation::Compile(ShaderCompilationOptions& options)
 
     if (result)
     {
+#if USE_EDITOR
         // Output shader source to easily investigate errors (eg. for generated shaders like materials or particles)
         const String outputSourceFolder = Globals::ProjectCacheFolder / TEXT("/Shaders/Source");
         const String outputSourcePath = outputSourceFolder / options.TargetName + TEXT(".hlsl");
@@ -153,6 +156,7 @@ bool ShadersCompilation::Compile(ShaderCompilationOptions& options)
         File::WriteAllBytes(outputSourcePath, (const byte*)options.Source, options.SourceLength);
         LOG(Error, "Shader compilation '{0}' failed (profile: {1})", options.TargetName, ::ToString(options.Profile));
         LOG(Error, "Source: {0}", outputSourcePath);
+#endif
     }
     else
     {