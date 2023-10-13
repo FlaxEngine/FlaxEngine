FlaxEngine/FlaxEngine

commit 88eca13eb3539aa43e0bc2863f2f2fb3250d4188

Add default font bundling (optional) and setup UI Style to match editor logic
#641

Source/Engine/Core/Config/BuildSettings.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "Engine/Core/Config/Settings.h"
-#include "Engine/Serialization/Serialization.h"
 #include "Engine/Content/Asset.h"
 #include "Engine/Content/AssetReference.h"
 #include "Engine/Content/SceneReference.h"
@@ -14,6 +13,7 @@
 API_CLASS(sealed, Namespace="FlaxEditor.Content.Settings") class FLAXENGINE_API BuildSettings : public SettingsBase
 {
     DECLARE_SCRIPTING_TYPE_MINIMAL(BuildSettings);
+    API_AUTO_SERIALIZATION();
 
 public:
     /// <summary>
@@ -77,6 +77,12 @@ public:
     bool ShadersGenerateDebugData = false;
 
     /// <summary>
+    /// If checked, skips bundling default engine fonts for UI. Use if to reduce build size if you don't use default engine fonts but custom ones only.
+    /// </summary>
+    API_FIELD(Attributes="EditorOrder(2100), EditorDisplay(\"Content\")")
+    bool SkipDefaultFonts = false;
+
+    /// <summary>
     /// If checked, .NET Runtime won't be packaged with a game and will be required by user to be installed on system upon running game build. Available only on supported platforms such as Windows, Linux and macOS.
     /// </summary>
     API_FIELD(Attributes="EditorOrder(3000), EditorDisplay(\"Scripting\", \"Skip .NET Runtime Packaging\")")
@@ -93,20 +99,4 @@ public:
     /// Gets the instance of the settings asset (default value if missing). Object returned by this method is always loaded with valid data to use.
     /// </summary>
     static BuildSettings* Get();
-
-    // [SettingsBase]
-    void Deserialize(DeserializeStream& stream, ISerializeModifier* modifier) final override
-    {
-        DESERIALIZE(MaxAssetsPerPackage);
-        DESERIALIZE(MaxPackageSizeMB);
-        DESERIALIZE(ContentKey);
-        DESERIALIZE(ForDistribution);
-        DESERIALIZE(SkipPackaging);
-        DESERIALIZE(AdditionalAssets);
-        DESERIALIZE(AdditionalAssetFolders);
-        DESERIALIZE(ShadersNoOptimize);
-        DESERIALIZE(ShadersGenerateDebugData);
-        DESERIALIZE(SkipDotnetPackaging);
-        DESERIALIZE(SkipUnusedDotnetLibsPackaging);
-    }
 };

Source/Editor/Cooker/Steps/DeployDataStep.cpp
@@ -360,7 +360,7 @@ bool DeployDataStep::Perform(CookingData& data)
     data.AddRootEngineAsset(PRE_INTEGRATED_GF_ASSET_NAME);
     data.AddRootEngineAsset(SMAA_AREA_TEX);
     data.AddRootEngineAsset(SMAA_SEARCH_TEX);
-    if (data.Configuration != BuildConfiguration::Release)
+    if (!buildSettings.SkipDefaultFonts)
         data.AddRootEngineAsset(TEXT("Editor/Fonts/Roboto-Regular"));
 
     // Register custom assets (eg. plugins)

Source/Editor/Options/OptionsModule.cs
@@ -244,11 +244,11 @@ namespace FlaxEditor.Options
                 CollectionBackgroundColor = Color.FromBgra(0x14CCCCCC),
                 ProgressNormal = Color.FromBgra(0xFF0ad328),
 
-                Statusbar = new Style.StatusbarStyle()
+                Statusbar = new Style.StatusbarStyle
                 {
                     PlayMode = Color.FromBgra(0xFF2F9135),
                     Failed = Color.FromBgra(0xFF9C2424),
-                    Loading = Color.FromBgra(0xFF2D2D30)
+                    Loading = Color.FromBgra(0xFF2D2D30),
                 },
 
                 // Fonts
@@ -271,7 +271,7 @@ namespace FlaxEditor.Options
                 Scale = Editor.Icons.Scale32,
                 Scalar = Editor.Icons.Scalar32,
 
-                SharedTooltip = new Tooltip()
+                SharedTooltip = new Tooltip(),
             };
             style.DragWindow = style.BackgroundSelected * 0.7f;
 

Source/Engine/Scripting/Scripting.cs
@@ -278,18 +278,29 @@ namespace FlaxEngine
                 BackgroundNormal = Color.FromBgra(0xFF3F3F46),
                 BorderNormal = Color.FromBgra(0xFF54545C),
                 TextBoxBackground = Color.FromBgra(0xFF333337),
-                ProgressNormal = Color.FromBgra(0xFF0ad328),
                 TextBoxBackgroundSelected = Color.FromBgra(0xFF3F3F46),
                 CollectionBackgroundColor = Color.FromBgra(0x14CCCCCC),
-                SharedTooltip = new Tooltip(),
-                Statusbar = new Style.StatusbarStyle()
+                ProgressNormal = Color.FromBgra(0xFF0ad328),
+                Statusbar = new Style.StatusbarStyle
                 {
                     PlayMode = Color.FromBgra(0xFF2F9135),
                     Failed = Color.FromBgra(0xFF9C2424),
-                    Loading = Color.FromBgra(0xFF2D2D30)
-                }
+                    Loading = Color.FromBgra(0xFF2D2D30),
+                },
+
+                SharedTooltip = new Tooltip(),
             };
             style.DragWindow = style.BackgroundSelected * 0.7f;
+
+            // Use optionally bundled default font (matches Editor)
+            var defaultFont = Content.LoadAsyncInternal<FontAsset>("Editor/Fonts/Roboto-Regular");
+            if (defaultFont)
+            {
+                style.FontTitle = defaultFont.CreateFont(18);
+                style.FontLarge = defaultFont.CreateFont(14);
+                style.FontMedium = defaultFont.CreateFont(9);
+                style.FontSmall = defaultFont.CreateFont(9);
+            }
 
             Style.Current = style;
         }

commit 702356f1e42082ec11a1dceb4388a23c1b523226

Missing change for c01614b8f5b30dc11f762ace6f28c5c063c8de6b and regression from 88eca13eb3539aa43e0bc2863f2f2fb3250d4188

Source/Engine/Core/Config/BuildSettings.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "Engine/Core/Config/Settings.h"
+#include "Engine/Serialization/Serialization.h"
+#include "Engine/Core/Collections/Array.h"
 #include "Engine/Content/Asset.h"
 #include "Engine/Content/AssetReference.h"
 #include "Engine/Content/SceneReference.h"
@@ -13,7 +15,6 @@
 API_CLASS(sealed, Namespace="FlaxEditor.Content.Settings") class FLAXENGINE_API BuildSettings : public SettingsBase
 {
     DECLARE_SCRIPTING_TYPE_MINIMAL(BuildSettings);
-    API_AUTO_SERIALIZATION();
 
 public:
     /// <summary>
@@ -99,4 +100,21 @@ public:
     /// Gets the instance of the settings asset (default value if missing). Object returned by this method is always loaded with valid data to use.
     /// </summary>
     static BuildSettings* Get();
+
+    // [SettingsBase]
+    void Deserialize(DeserializeStream& stream, ISerializeModifier* modifier) final override
+    {
+        DESERIALIZE(MaxAssetsPerPackage);
+        DESERIALIZE(MaxPackageSizeMB);
+        DESERIALIZE(ContentKey);
+        DESERIALIZE(ForDistribution);
+        DESERIALIZE(SkipPackaging);
+        DESERIALIZE(AdditionalAssets);
+        DESERIALIZE(AdditionalAssetFolders);
+        DESERIALIZE(ShadersNoOptimize);
+        DESERIALIZE(ShadersGenerateDebugData);
+        DESERIALIZE(SkipDefaultFonts);
+        DESERIALIZE(SkipDotnetPackaging);
+        DESERIALIZE(SkipUnusedDotnetLibsPackaging);
+    }
 };

Source/Engine/Serialization/Serialization.h
@@ -405,14 +405,14 @@ namespace Serialization
 
     // ISerializable
 
-    inline bool ShouldSerialize(ISerializable& v, const void* otherObj)
+    inline bool ShouldSerialize(const ISerializable& v, const void* otherObj)
     {
         return true;
     }
-    inline void Serialize(ISerializable::SerializeStream& stream, ISerializable& v, const void* otherObj)
+    inline void Serialize(ISerializable::SerializeStream& stream, const ISerializable& v, const void* otherObj)
     {
         stream.StartObject();
-        v.Serialize(stream, otherObj);
+        const_cast<ISerializable*>(&v)->Serialize(stream, otherObj);
         stream.EndObject();
     }
     inline void Deserialize(ISerializable::DeserializeStream& stream, ISerializable& v, ISerializeModifier* modifier)
@@ -421,15 +421,15 @@ namespace Serialization
     }
 
     template<typename T>
-    inline typename TEnableIf<TIsBaseOf<ISerializable, T>::Value, bool>::Type ShouldSerialize(ISerializable& v, const void* otherObj)
+    inline typename TEnableIf<TIsBaseOf<ISerializable, T>::Value, bool>::Type ShouldSerialize(const ISerializable& v, const void* otherObj)
     {
         return true;
     }
     template<typename T>
-    inline typename TEnableIf<TIsBaseOf<ISerializable, T>::Value>::Type Serialize(ISerializable::SerializeStream& stream, ISerializable& v, const void* otherObj)
+    inline typename TEnableIf<TIsBaseOf<ISerializable, T>::Value>::Type Serialize(ISerializable::SerializeStream& stream, const ISerializable& v, const void* otherObj)
     {
         stream.StartObject();
-        v.Serialize(stream, otherObj);
+        const_cast<ISerializable*>(&v)->Serialize(stream, otherObj);
         stream.EndObject();
     }
     template<typename T>
@@ -441,12 +441,12 @@ namespace Serialization
     // Scripting Object
 
     template<typename T>
-    inline typename TEnableIf<TIsBaseOf<ScriptingObject, T>::Value, bool>::Type ShouldSerialize(T*& v, const void* otherObj)
+    inline typename TEnableIf<TIsBaseOf<ScriptingObject, T>::Value, bool>::Type ShouldSerialize(const T*& v, const void* otherObj)
     {
         return !otherObj || v != *(T**)otherObj;
     }
     template<typename T>
-    inline typename TEnableIf<TIsBaseOf<ScriptingObject, T>::Value>::Type Serialize(ISerializable::SerializeStream& stream, T*& v, const void* otherObj)
+    inline typename TEnableIf<TIsBaseOf<ScriptingObject, T>::Value>::Type Serialize(ISerializable::SerializeStream& stream, const T*& v, const void* otherObj)
     {
         stream.Guid(v ? v->GetID() : Guid::Empty);
     }
@@ -568,12 +568,13 @@ namespace Serialization
     {
         if (!otherObj)
             return true;
-        const auto other = (Array<T, AllocationType>*)otherObj;
+        const auto other = (const Array<T, AllocationType>*)otherObj;
         if (v.Count() != other->Count())
             return true;
+        const T* vPtr = v.Get();
         for (int32 i = 0; i < v.Count(); i++)
         {
-            if (ShouldSerialize((T&)v[i], (const void*)&other->At(i)))
+            if (ShouldSerialize(vPtr[i], (const void*)&other->At(i)))
                 return true;
         }
         return false;
@@ -582,8 +583,9 @@ namespace Serialization
     inline void Serialize(ISerializable::SerializeStream& stream, const Array<T, AllocationType>& v, const void* otherObj)
     {
         stream.StartArray();
+        const T* vPtr = v.Get();
         for (int32 i = 0; i < v.Count(); i++)
-            Serialize(stream, (T&)v[i], nullptr);
+            Serialize(stream, vPtr[i], nullptr);
         stream.EndArray();
     }
     template<typename T, typename AllocationType = HeapAllocation>
@@ -593,8 +595,9 @@ namespace Serialization
         {
             const auto& streamArray = stream.GetArray();
             v.Resize(streamArray.Size());
+            T* vPtr = v.Get();
             for (int32 i = 0; i < v.Count(); i++)
-                Deserialize(streamArray[i], v[i], modifier);
+                Deserialize(streamArray[i], vPtr[i], modifier);
         }
         else if (TIsPODType<T>::Value && stream.IsString())
         {