FlaxEngine/FlaxEngine

commit cf94cd937a7a72aa9828bf63f47d945084963d8f

Fix logging performance and crashes on non-Windows platforms
#662

Source/Engine/Core/Log.cpp
@@ -199,35 +199,35 @@ void Log::Logger::WriteFloor()
 void Log::Logger::ProcessLogMessage(LogType type, const StringView& msg, fmt_flax::memory_buffer& w)
 {
     const TimeSpan time = DateTime::Now() - LogStartTime;
+    const int32 msgLength = msg.Length();
 
     fmt_flax::format(w, TEXT("[ {0} ]: [{1}] "), *time.ToString('a'), ToString(type));
 
     // On Windows convert all '\n' into '\r\n'
 #if PLATFORM_WINDOWS
-    const int32 msgLength = msg.Length();
-    if (msgLength > 1)
+    bool hasWindowsNewLine = false;
+    for (int32 i = 1; i < msgLength && !hasWindowsNewLine; i++)
+        hasWindowsNewLine |= msg.Get()[i - 1] != '\r' && msg.Get()[i] == '\n';
+    if (hasWindowsNewLine)
     {
         MemoryWriteStream msgStream(msgLength * sizeof(Char));
-        msgStream.WriteChar(msg[0]);
+        msgStream.WriteChar(msg.Get()[0]);
         for (int32 i = 1; i < msgLength; i++)
         {
-            if (msg[i - 1] != '\r' && msg[i] == '\n')
+            if (msg.Get()[i - 1] != '\r' && msg.Get()[i] == '\n')
                 msgStream.WriteChar(TEXT('\r'));
-            msgStream.WriteChar(msg[i]);
+            msgStream.WriteChar(msg.Get()[i]);
         }
-        msgStream.WriteChar(msg[msgLength]);
         msgStream.WriteChar(TEXT('\0'));
-        fmt_flax::format(w, TEXT("{}"), (const Char*)msgStream.GetHandle());
-        //w.append(msgStream.GetHandle(), msgStream.GetHandle() + msgStream.GetPosition());
+        w.append((const Char*)msgStream.GetHandle(), (const Char*)msgStream.GetHandle() + msgStream.GetPosition());
+        //fmt_flax::format(w, TEXT("{}"), (const Char*)msgStream.GetHandle());
+        return;
     }
-    else
-    {
-        //w.append(msg.Get(), msg.Get() + msg.Length());
-        fmt_flax::format(w, TEXT("{}"), msg);
-    }
-#else
-    fmt_flax::format(w, TEXT("{}"), msg);
 #endif
+
+    // Output raw message to the log
+    w.append(msg.Get(), msg.Get() + msg.Length());
+    //fmt_flax::format(w, TEXT("{}"), msg);
 }
 
 void Log::Logger::Write(LogType type, const StringView& msg)

commit f8f1a66ab99ff1d394da5dbe4f956f2545d599bd

Fix multi-line logs on Windows regression from cf94cd937a7a72aa9828bf63f47d945084963d8f

Source/Engine/Core/Log.cpp
@@ -9,7 +9,6 @@
 #include "Engine/Platform/FileSystem.h"
 #include "Engine/Platform/CriticalSection.h"
 #include "Engine/Serialization/FileWriteStream.h"
-#include "Engine/Serialization/MemoryWriteStream.h"
 #include "Engine/Debug/Exceptions/Exceptions.h"
 #if USE_EDITOR
 #include "Engine/Core/Collections/Array.h"
@@ -210,17 +209,18 @@ void Log::Logger::ProcessLogMessage(LogType type, const StringView& msg, fmt_fla
         hasWindowsNewLine |= msg.Get()[i - 1] != '\r' && msg.Get()[i] == '\n';
     if (hasWindowsNewLine)
     {
-        MemoryWriteStream msgStream(msgLength * sizeof(Char));
-        msgStream.WriteChar(msg.Get()[0]);
+        Array<Char> msgStream;
+        msgStream.EnsureCapacity(msgLength);
+        msgStream.Add(msg.Get()[0]);
         for (int32 i = 1; i < msgLength; i++)
         {
             if (msg.Get()[i - 1] != '\r' && msg.Get()[i] == '\n')
-                msgStream.WriteChar(TEXT('\r'));
-            msgStream.WriteChar(msg.Get()[i]);
+                msgStream.Add(TEXT('\r'));
+            msgStream.Add(msg.Get()[i]);
         }
-        msgStream.WriteChar(TEXT('\0'));
-        w.append((const Char*)msgStream.GetHandle(), (const Char*)msgStream.GetHandle() + msgStream.GetPosition());
-        //fmt_flax::format(w, TEXT("{}"), (const Char*)msgStream.GetHandle());
+        msgStream.Add(TEXT('\0'));
+        w.append(msgStream.Get(), (const Char*)(msgStream.Get() + msgStream.Count()));
+        //fmt_flax::format(w, TEXT("{}"), (const Char*)msgStream.Get());
         return;
     }
 #endif