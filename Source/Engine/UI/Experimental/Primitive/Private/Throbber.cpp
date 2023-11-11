d37f2f6b7aa4447e74cf105305c19e6a57679675
NoriteSC <53096989+NoriteSC@users.noreply.github.com>
2023-11-11 18:25:58 +0100

    Started to implement Throbber

--- Source/Engine/UI/Experimental/Primitive/Private/Throbber.cpp
+++ Source/Engine/UI/Experimental/Primitive/Private/Throbber.cpp
@@ -1,7 +1,32 @@
 #include "Engine/UI/Experimental/Primitive/Throbber.h"
+#include "Engine/Core/Math/Rectangle.h"
+#include "Engine/Render2D/Render2D.h"
+#include "Engine/Engine/Time.h"
 
-Throbber::Throbber(const SpawnParams& params) : UIElement(params) {}
+Throbber::Throbber(const SpawnParams& params) : UIElement(params) 
+{
+
+}
 
 void Throbber::OnDraw()
 {
+    auto deltaTime = Time::Draw.DeltaTime;
+    if ((States.Count()-1) != NumberOfPieces) 
+    {
+        States.Resize(NumberOfPieces, true);
+    }
+    auto size = GetSlot()->GetSize();
+    if (EnumHasAllFlags(Animate, AnimateFlags::Horizontal))
+        States[CurentElement].X = Math::PingPong(States[CurentElement].X, size.X);
+    if (EnumHasAllFlags(Animate, AnimateFlags::Vertical))
+        States[CurentElement].Y = Math::PingPong(States[CurentElement].Y, size.Y / NumberOfPieces);
+    if (EnumHasAllFlags(Animate, AnimateFlags::Alpha))
+        States[CurentElement].Z = Math::PingPong(States[CurentElement].Z, 1.0f);
+
+    Float3 sce = States[CurentElement];
+    for (auto i = 0; i < States.Count(); i++)
+    {
+        Render2D::FillRectangle(Rectangle(Float2(0, i * GetSlot()->GetSize().X), Float2(sce.X, sce.Y)), Color(Color::White).AlphaMultiplied(sce.Z));
+    }
+    CurentElement++;
 }
--- Source/Engine/UI/Experimental/Primitive/Throbber.h
+++ Source/Engine/UI/Experimental/Primitive/Throbber.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Engine/UI/Experimental/Types/UIElement.h"
+#include "Engine/UI/Experimental/Types/IBrush.h"
 
 /// <summary>
 /// Shows serverals zooming images in the row
@@ -8,7 +9,43 @@ API_CLASS()
 class FLAXENGINE_API Throbber : public UIElement
 {
     DECLARE_SCRIPTING_TYPE(Throbber);
+private:
+    Array<Float3> States;
+    int CurentElement;
+public:
+    API_ENUM()
+    enum AnimateFlags
+    {
+        None = 0,
+        Vertical = 1,
+        Horizontal = 2,
+        Alpha = 4,
+    };
+    /// <summary>
+    /// The brush
+    /// </summary>
+    API_FIELD() IBrush* Brush;
+
+    /// <summary>
+    /// [ToDo] add summary
+    /// </summary>
+    API_FIELD() byte NumberOfPieces;
+
+    /// <summary>
+    /// [ToDo] add summary
+    /// </summary>
+    API_FIELD() byte NumberOfPieces;
+
+    /// <summary>
+    /// [ToDo] add summary
+    /// </summary>
+    API_FIELD() AnimateFlags Animate = (AnimateFlags)(AnimateFlags::Vertical | AnimateFlags::Horizontal | AnimateFlags::Alpha);
 
     /// <inheritdoc />
     void OnDraw() override;
+
+    API_FUNCTION() virtual Float2 GetDesiredSize() override 
+    {
+        return Float2(NumberOfPieces * GetSlot()->GetSize().X, GetSlot()->GetSize().Y);
+    }
 };
