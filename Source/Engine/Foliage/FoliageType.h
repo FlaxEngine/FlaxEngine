bc3107d1db6cc8744cf7d24797f74a1d0258c894
Wojtek Figat <wojtek@figat.pl>
2023-11-14 19:03:46 +0100

    Fix build warnings on the latest XCode

--- Source/Engine/Foliage/FoliageType.h
+++ Source/Engine/Foliage/FoliageType.h
@@ -46,7 +46,7 @@ API_CLASS(Sealed, NoSpawn) class FLAXENGINE_API FoliageType : public ScriptingOb
     DECLARE_SCRIPTING_TYPE_NO_SPAWN(FoliageType);
     friend Foliage;
 private:
-    int8 _isReady : 1;
+    uint8 _isReady : 1;
 
 public:
     /// <summary>
