4707f98fab98e6155e97eb9bb5b3b8029d8581b5
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2023-12-08 14:13:50 -0500

    Update Time.cpp

--- Source/Engine/Engine/Time.cpp
+++ Source/Engine/Engine/Time.cpp
@@ -160,7 +160,7 @@ bool Time::FixedStepTickData::OnTickBegin(float targetFps, float maxDeltaTime)
 
         if (targetFps > ZeroTolerance)
         {
-            int skip = (int)(1 + (time - NextBegin) / (1.0 / targetFps));
+            int skip = (int)(1 + (time - NextBegin) * targetFps);
             NextBegin += (1.0 / targetFps) * skip;
         }
     }
