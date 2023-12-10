c895e310cbec7a6fbca418ff419cd64e08b13cb4
Menotdan <32620310+Menotdan@users.noreply.github.com>
2023-12-09 19:19:03 -0500

    Improve box collider creation behavior to account for child actors of the collider's parent.

--- Source/Engine/Level/Actors/BoxCollider.cs
+++ Source/Engine/Level/Actors/BoxCollider.cs
@@ -1,16 +1,39 @@
-using System;
+using FlaxEditor.Windows;
 
 namespace FlaxEngine
 {
     partial class BoxCollider
     {
+        private void BoxExcluding(Actor target, ref BoundingBox output, Actor excluded)
+        {
+            foreach (Actor child in target.Children)
+            {
+                if (child == excluded)
+                {
+                    continue;
+                }
+
+                output = BoundingBox.Merge(output, child.Box);
+                BoxExcluding(child, ref output, excluded);
+            }
+        }
+
         /// <inheritdoc />
         public override void OnActorSpawned()
         {
             base.OnActorSpawned();
             Vector3 parentScale = Parent.Scale;
-            Vector3 parentSize = Parent.Box.Size;
-            Vector3 parentCenter = Parent.Box.Center - Parent.Position;
+            BoundingBox parentBox = Parent.Box;
+            BoxExcluding(Parent, ref parentBox, this);
+
+            Vector3 parentSize = parentBox.Size;
+            Vector3 parentCenter = parentBox.Center - Parent.Position;
+
+            // Avoid division by zero
+            if (parentScale.X == 0 || parentScale.Y == 0 || parentScale.Z == 0)
+            {
+                return;
+            }
 
             Size = parentSize / parentScale;
             Center = parentCenter / parentScale;
