bf6cb90a78b2c493dcb21e54318173be96e86dc0
Menotdan <32620310+Menotdan@users.noreply.github.com>
2023-12-31 15:09:07 -0500

    Expose partition mode, and cascade spacing.

--- Source/Editor/SceneGraph/Actors/DirectionalLightNode.cs
+++ Source/Editor/SceneGraph/Actors/DirectionalLightNode.cs
@@ -2,6 +2,17 @@
 
 using FlaxEngine;
 
+namespace FlaxEngine
+{
+    public partial class DirectionalLight
+    {
+        bool ShowCascade1 => CascadeCount >= 1 && PartitionMode == PartitionMode.Manual;
+        bool ShowCascade2 => CascadeCount >= 2 && PartitionMode == PartitionMode.Manual;
+        bool ShowCascade3 => CascadeCount >= 3 && PartitionMode == PartitionMode.Manual;
+        bool ShowCascade4 => CascadeCount >= 4 && PartitionMode == PartitionMode.Manual;
+    }
+}
+
 namespace FlaxEditor.SceneGraph.Actors
 {
     /// <summary>
