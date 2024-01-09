d9d8047cef5c9bbd6ab9a1b24aee8eadb821b3d0
envision3d <envision3d@envision3d.org>
2024-01-09 04:18:18 -0600

    Fixes terrain smoothing separation issue

--- Source/Editor/Tools/Terrain/Sculpt/SmoothMode.cs
+++ Source/Editor/Tools/Terrain/Sculpt/SmoothMode.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2012-2023 Wojciech Figat. All rights reserved.
 
 using System;
+using System.Security.Cryptography.X509Certificates;
 using FlaxEngine;
 
 namespace FlaxEditor.Tools.Terrain.Sculpt
@@ -24,7 +25,6 @@ namespace FlaxEditor.Tools.Terrain.Sculpt
             // Prepare
             var brushPosition = p.Gizmo.CursorPosition;
             var radius = Mathf.Max(Mathf.CeilToInt(FilterRadius * 0.01f * p.Brush.Size), 2);
-            var max = p.HeightmapSize - 1;
             var strength = Mathf.Saturate(p.Strength);
 
             // Apply brush modification
@@ -42,41 +42,88 @@ namespace FlaxEditor.Tools.Terrain.Sculpt
 
                     var paintAmount = p.Brush.Sample(ref brushPosition, ref samplePositionWorld) * strength;
 
-                    if (paintAmount > 0)
-                    {
-                        // Sum the nearby values
-                        float smoothValue = 0;
-                        int smoothValueSamples = 0;
-                        int minX = Math.Max(x - radius + p.ModifiedOffset.X, 0);
-                        int minZ = Math.Max(z - radius + p.ModifiedOffset.Y, 0);
-                        int maxX = Math.Min(x + radius + p.ModifiedOffset.X, max);
-                        int maxZ = Math.Min(z + radius + p.ModifiedOffset.Y, max);
-                        for (int dz = minZ; dz <= maxZ; dz++)
-                        {
-                            for (int dx = minX; dx <= maxX; dx++)
-                            {
-                                var height = p.SourceHeightMap[dz * p.HeightmapSize + dx];
-                                smoothValue += height;
-                                smoothValueSamples++;
-                            }
-                        }
-
-                        // Normalize
-                        smoothValue /= smoothValueSamples;
-
-                        // Blend between the height and smooth value
-                        p.TempBuffer[z * p.ModifiedSize.X + x] = Mathf.Lerp(sourceHeight, smoothValue, paintAmount);
-                    }
-                    else
+                    if (paintAmount <= 0)
                     {
                         p.TempBuffer[z * p.ModifiedSize.X + x] = sourceHeight;
+                        continue;
                     }
+
+                    float smoothValue = 0;
+                    smoothValue = SampleHeightAverage(p.Terrain, p.HeightmapSize, ref p.ModifiedOffset, radius);
+
+                    // Blend between the height and smooth value
+                    p.TempBuffer[z * p.ModifiedSize.X + x] = Mathf.Lerp(sourceHeight, smoothValue, paintAmount);
+
                 }
             }
             Profiler.EndEvent();
 
             // Update terrain patch
             TerrainTools.ModifyHeightMap(p.Terrain, ref p.PatchCoord, p.TempBuffer, ref p.ModifiedOffset, ref p.ModifiedSize);
+        }
+
+        float SampleHeightAverage(FlaxEngine.Terrain terrain, int heightmapSize, ref Int2 startPosition, int radius)
+        {
+            int minPatchOffsetX = Mathf.FloorToInt((float)startPosition.X - radius / heightmapSize);
+            int minPatchOffsetZ = Mathf.FloorToInt((float)startPosition.Y - radius / heightmapSize);
+
+            int maxPatchOffsetX = Mathf.CeilToInt(((float)startPosition.X + radius) / heightmapSize);
+            int maxPatchOffsetZ = Mathf.CeilToInt(((float)startPosition.Y + radius) / heightmapSize);
+
+            float total = 0;
+            int totalPoints = 0;
+
+            for (int patchOffsetZ = minPatchOffsetZ; patchOffsetZ <= maxPatchOffsetZ; patchOffsetZ++)
+            {
+                for (int patchOffsetX = minPatchOffsetX; patchOffsetX <= maxPatchOffsetX; patchOffsetX++)
+                {
+                    var patchCoords = new Int2(startPosition.X + patchOffsetX, startPosition.Y + patchOffsetZ);
+                    var relativeStartPosition = new Int2(-patchOffsetX * heightmapSize, -patchOffsetZ * heightmapSize);
+
+                    ReadHeightsFromPatch(terrain, heightmapSize, ref patchCoords, ref relativeStartPosition, radius, out var accumulation, out var points);
+
+                    total += accumulation;
+                    totalPoints += points;
+                }
+            }
+
+            if (totalPoints == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return total / totalPoints;
+            }
+        }
+
+        unsafe void ReadHeightsFromPatch(FlaxEngine.Terrain terrain, int heightmapSize, ref Int2 patchCoords, ref Int2 startPosition, int radius, out float accumulation, out int points)
+        {
+            accumulation = 0;
+            points = 0;
+
+            if (!terrain.HasPatch(ref patchCoords))
+            {
+                return;
+            }
+
+            var patch = TerrainTools.GetHeightmapData(terrain, ref patchCoords);
+
+            var max = heightmapSize - 1;
+            var minX = Math.Max(startPosition.X - radius, 0);
+            var minZ = Math.Max(startPosition.Y - radius, 0);
+            var maxX = Math.Min(startPosition.X + radius, max);
+            var maxZ = Math.Min(startPosition.Y + radius, max);
+
+            for (int dz = minZ; dz <= maxZ; dz++)
+            {
+                for (int dx = minX; dx <= maxX; dx++)
+                {
+                    var height = patch[dz * heightmapSize + dx];
+                    accumulation += height;
+                    points++;
+                }
+            }
         }
     }
 }
