3611316e3878ae6a7ed78d7249409a84e0ba8d88
NoriteSC <53096989+NoriteSC@users.noreply.github.com>
2024-02-14 02:47:00 +0100

    extended cheeks to BinaryAssetFactory and enabled only in debug mode

--- Source/Engine/Content/Factories/JsonAssetFactory.h
+++ Source/Engine/Content/Factories/JsonAssetFactory.h
@@ -49,21 +49,8 @@ protected:
 	class CONCAT_MACROS(Factory, type) : public JsonAssetFactory<type> \
 	{ \
 		public: \
-		CONCAT_MACROS(Factory, type)() \
-        { \
-            if (IAssetFactory::Get().ContainsKey(type::TypeName)) \
-            { \
-                if (Platform::IsDebuggerPresent()) \
-                { \
-                    PLATFORM_DEBUG_BREAK; \
-                } \
-                Platform::Assert("[IAssetFactory] Can't Register "#type \
-                " it is already registered", __FILE__, __LINE__); \
-                return; \
-            } \
-            IAssetFactory::Get().Add(type::TypeName, this); \
-        } \
-		~CONCAT_MACROS(Factory, type)() { IAssetFactory::Get().Remove(type::TypeName); } \
+		CONCAT_MACROS(Factory, type)() { INTERNAL_FACTORY_REGISTER_CHEAK(type) IAssetFactory::Get().Add(type::TypeName, this); } \
+		~CONCAT_MACROS(Factory, type)() {INTERNAL_FACTORY_UNREGISTER_CHEAK(type) IAssetFactory::Get().Remove(type::TypeName); } \
 		bool SupportsVirtualAssets() const override { return supportsVirtualAssets; } \
 	}; \
 	static CONCAT_MACROS(Factory, type) CONCAT_MACROS(CFactory, type)
