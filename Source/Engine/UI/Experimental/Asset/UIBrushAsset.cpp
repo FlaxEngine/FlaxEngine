2183fe25d79fde5f4f3b20595b4f76c4be3403b7
NoriteSC <53096989+NoriteSC@users.noreply.github.com>
2024-02-14 14:20:48 +0100

    Update UIBrushAsset.cpp

--- Source/Engine/UI/Experimental/Asset/UIBrushAsset.cpp
+++ Source/Engine/UI/Experimental/Asset/UIBrushAsset.cpp
@@ -5,7 +5,8 @@
 #include "Engine/Content/Factories/JsonAssetFactory.h"
 #include <Engine/Serialization/JsonWriters.h>
 
-REGISTER_JSON_ASSET(UIBrushAsset, "FlaxEngine.UIBlueprintAsset", false);
+REGISTER_JSON_ASSET(UIBrushAsset, "FlaxEngine.UIBrushAsset", false);
 UIBrushAsset::UIBrushAsset(const SpawnParams& params, const AssetInfo* info) : JsonAssetBase(params, info)
 {
+    Brush = nullptr;
 }
