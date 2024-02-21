8ee2bf8d76b151bf3d467a5186584b8e760eb558
Chandler Cox <mr.chandlercox@gmail.com>
2024-02-20 20:57:15 -0600

    Change category drop panel o look similar to other panels. Add flax engine scripts to flax engine category.

--- Source/Editor/GUI/ItemsListContextMenu.cs
+++ Source/Editor/GUI/ItemsListContextMenu.cs
@@ -134,9 +134,13 @@ namespace FlaxEditor.GUI
                         Render2D.FillRectangle(rect, color);
                     }
                 }
+                // Indent for drop panel items is handled by drop panel margin
+                var indent = Float2.Zero;
+                if (Parent is not DropPanel)
+                    indent = new Float2(Editor.Instance.Icons.ArrowRight12.Size.X + 2, 0);
 
                 // Draw name
-                Render2D.DrawText(style.FontSmall, Name, textRect, TintColor * (Enabled ? style.Foreground : style.ForegroundDisabled), TextAlignment.Near, TextAlignment.Center);
+                Render2D.DrawText(style.FontSmall, Name, textRect + indent, TintColor * (Enabled ? style.Foreground : style.ForegroundDisabled), TextAlignment.Near, TextAlignment.Center);
             }
 
             /// <inheritdoc />
@@ -338,9 +342,14 @@ namespace FlaxEditor.GUI
                     var categoryPanel = new DropPanel
                     {
                         HeaderText = item.Category,
+                        ArrowImageOpened = new SpriteBrush(Editor.Instance.Icons.ArrowDown12),
+                        ArrowImageClosed = new SpriteBrush(Editor.Instance.Icons.ArrowRight12),
+                        EnableDropDownIcon = true,
+                        ItemsMargin = new Margin(28, 0, 2, 2),
+                        HeaderColor = Style.Current.Background,
                         Parent = parent,
                     };
-                    categoryPanel.Open(false);
+                    categoryPanel.Close(false);
                     _categoryPanels.Add(categoryPanel);
                     parent = categoryPanel;
                 }
--- Source/Engine/Networking/Components/NetworkTransform.h
+++ Source/Engine/Networking/Components/NetworkTransform.h
@@ -10,7 +10,7 @@
 /// Actor script component that synchronizes the Transform over the network.
 /// </summary>
 /// <remarks>Interpolation and prediction logic based on https://www.gabrielgambetta.com/client-server-game-architecture.html.</remarks>
-API_CLASS(Namespace="FlaxEngine.Networking") class FLAXENGINE_API NetworkTransform : public Script, public INetworkSerializable
+API_CLASS(Namespace="FlaxEngine.Networking", Attributes="Category(\"Flax Engine\")") class FLAXENGINE_API NetworkTransform : public Script, public INetworkSerializable
 {
     API_AUTO_SERIALIZATION();
     DECLARE_SCRIPTING_TYPE(NetworkTransform);
