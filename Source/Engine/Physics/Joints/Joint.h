4853f215dd0ec29fdb4fd65c40a25e41dc299945
Norite SC <162097313+cNori@users.noreply.github.com>
2024-03-16 02:20:55 +0100

    fixed joints including scale in calculations
    the PxJoint  is not expecting to get scaled offset and is collapsing auto anchor also if A nd B actor is scaled it will snap the joint in to wrong location
    
    added SetTargetAnchor(const Transform& value)

--- Source/Engine/Physics/Joints/Joint.h
+++ Source/Engine/Physics/Joints/Joint.h
@@ -99,6 +99,18 @@ public:
     {
         return _targetAnchor;
     }
+    /// <summary>
+    /// Gets the target anchor.
+    /// </summary>
+    /// <remarks>
+    /// This is the relative pose which locates the joint frame relative to the target actor.
+    /// </remarks>
+    API_PROPERTY(Attributes = "EditorOrder(40), DefaultValue(typeof(Vector3), \"0,0,0\"), EditorDisplay(\"Joint\"), VisibleIf(nameof(EnableAutoAnchor), true)")
+    FORCE_INLINE Transform GetTargetAnchor()
+    {
+        return Transform(_targetAnchor,_targetAnchorRotation,Float3::One);
+    }
+
 
     /// <summary>
     /// Sets the target anchor.
@@ -107,6 +119,14 @@ public:
     /// This is the relative pose which locates the joint frame relative to the target actor.
     /// </remarks>
     API_PROPERTY() void SetTargetAnchor(const Vector3& value);
+
+    /// <summary>
+    /// Sets the target anchor.
+    /// </summary>
+    /// <remarks>
+    /// This is the relative pose which locates the joint frame relative to the target actor.
+    /// </remarks>
+    API_PROPERTY() void SetTargetAnchor(const Transform& value);
 
     /// <summary>
     /// Gets the target anchor rotation.
