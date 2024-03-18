eb8db60ead137fe49f61142ef8e11fc6125e98c1
Wojtek Figat <wojtek@figat.pl>
2024-03-18 21:12:54 +0100

    Revert eye adaptation back to original value (in favor of #2023)

--- Source/Engine/Graphics/PostProcessSettings.h
+++ Source/Engine/Graphics/PostProcessSettings.h
@@ -948,7 +948,7 @@ API_STRUCT() struct FLAXENGINE_API EyeAdaptationSettings : ISerializable
     /// The effect rendering mode used for the exposure processing.
     /// </summary>
     API_FIELD(Attributes="EditorOrder(0), PostProcessSetting((int)EyeAdaptationSettingsOverride.Mode)")
-    EyeAdaptationMode Mode = EyeAdaptationMode::None;
+    EyeAdaptationMode Mode = EyeAdaptationMode::AutomaticHistogram;
 
     /// <summary>
     /// The speed at which the exposure changes when the scene brightness moves from a dark area to a bright area (brightness goes up).
