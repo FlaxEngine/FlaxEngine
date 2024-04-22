FlaxEngine/FlaxEngine

commit f19977a9561caf5a199379ed183aa6321e240cbb

Fix surface node context menu to not show when moving surface
#2466

Source/Editor/Surface/SurfaceNode.cs
@@ -1063,6 +1063,20 @@ namespace FlaxEditor.Surface
         }
 
         /// <inheritdoc />
+        public override bool OnMouseDown(Float2 location, MouseButton button)
+        {
+            if (base.OnMouseDown(location, button))
+                return true;
+
+            if (button == MouseButton.Left && (Archetype.Flags & NodeFlags.NoCloseButton) == 0)
+                return true;
+            if (button == MouseButton.Right)
+                return true;
+
+            return false;
+        }
+
+        /// <inheritdoc />
         public override bool OnMouseUp(Float2 location, MouseButton button)
         {
             if (base.OnMouseUp(location, button))

commit d8a1de64d1ff0c5a59dfbcc8d2e891f11b74be81

Fix surface node moving regression from f19977a9561caf5a199379ed183aa6321e240cbb
#2466

Source/Editor/Surface/SurfaceNode.cs
@@ -1068,7 +1068,7 @@ namespace FlaxEditor.Surface
             if (base.OnMouseDown(location, button))
                 return true;
 
-            if (button == MouseButton.Left && (Archetype.Flags & NodeFlags.NoCloseButton) == 0)
+            if (button == MouseButton.Left && (Archetype.Flags & NodeFlags.NoCloseButton) == 0 && _closeButtonRect.Contains(ref location))
                 return true;
             if (button == MouseButton.Right)
                 return true;
@@ -1083,13 +1083,10 @@ namespace FlaxEditor.Surface
                 return true;
 
             // Close
-            if (button == MouseButton.Left && (Archetype.Flags & NodeFlags.NoCloseButton) == 0)
+            if (button == MouseButton.Left && (Archetype.Flags & NodeFlags.NoCloseButton) == 0 && _closeButtonRect.Contains(ref location))
             {
-                if (_closeButtonRect.Contains(ref location))
-                {
-                    Surface.Delete(this);
-                    return true;
-                }
+                Surface.Delete(this);
+                return true;
             }
 
             // Secondary Context Menu