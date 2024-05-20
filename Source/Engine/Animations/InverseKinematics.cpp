a69c8ce6a2400a41cde5e2de39407dfa414f7dae
Wojtek Figat <wojtek@figat.pl>
2024-05-20 18:37:15 +0200

    Revert 73f68c102d9a84cbbd360c6ac53663c457b7686d and reopen #2610

--- Source/Engine/Animations/InverseKinematics.cpp
+++ Source/Engine/Animations/InverseKinematics.cpp
@@ -113,7 +113,7 @@ void InverseKinematics::SolveTwoBoneIK(Transform& rootTransform, Transform& midJ
         newMidJointPos = rootTransform.Translation + projJointDist * toTargetDir + jointLineDist * bendDirection;
     }
     // TODO: fix the new IK impl (https://github.com/FlaxEngine/FlaxEngine/pull/2421) to properly work for character from https://github.com/PrecisionRender/CharacterControllerPro
-#define OLD 1
+#define OLD 0
     // Update root joint orientation
     {
 #if OLD
