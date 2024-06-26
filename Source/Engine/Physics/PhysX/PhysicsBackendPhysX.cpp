91d3216a005b4179ce4448d2537d80ae72ad9126
Wojtek Figat <wojtek@figat.pl>
2024-06-26 18:19:36 +0200

    Fix crash on shutdown when physical material asset was left alone

--- Source/Engine/Physics/PhysX/PhysicsBackendPhysX.cpp
+++ Source/Engine/Physics/PhysX/PhysicsBackendPhysX.cpp
@@ -4476,6 +4476,8 @@ void PhysicsBackend::DestroyController(void* controller)
 
 void PhysicsBackend::DestroyMaterial(void* material)
 {
+    if (!PhysX)
+        return; // Skip when called by Content unload after Physics is disposed
     ASSERT_LOW_LAYER(material);
     auto materialPhysX = (PxMaterial*)material;
     materialPhysX->userData = nullptr;
@@ -4486,6 +4488,8 @@ void PhysicsBackend::DestroyMaterial(void* material)
 
 void PhysicsBackend::DestroyObject(void* object)
 {
+    if (!PhysX)
+        return; // Skip when called by Content unload after Physics is disposed
     ASSERT_LOW_LAYER(object);
     auto objectPhysX = (PxBase*)object;
     FlushLocker.Lock();
