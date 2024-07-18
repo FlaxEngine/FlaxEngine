583b012f37ebb78a2dd897e672333e44530aa62d
Ari Vuollet <ari.vuollet@kapsi.fi>
2024-07-18 22:58:42 +0300

    Fix initial position of Tooltips

--- Source/Engine/UI/GUI/Tooltip.cs
+++ Source/Engine/UI/GUI/Tooltip.cs
@@ -77,8 +77,11 @@ namespace FlaxEngine.GUI
             var dpiSize = Size * dpiScale;
             var locationWS = target.PointToWindow(location);
             var locationSS = parentWin.PointToScreen(locationWS);
+            var mousePos = Input.MouseScreenPosition;
             _showTarget = target;
-            WrapPosition(ref locationSS);
+            //WrapPosition(ref locationSS);
+            WrapPosition(ref mousePos, 10);
+            locationSS = mousePos + new Float2(15, 10);
 
             // Create window
             var desc = CreateWindowSettings.Default;
