2da908d9b85d1605ab40d5475ec0f94ec86f5f96
Ari Vuollet <ari.vuollet@kapsi.fi>
2024-07-20 19:59:29 +0300

    Add flag for Window types

--- Source/Editor/GUI/Docking/DockHintWindow.cs
+++ Source/Editor/GUI/Docking/DockHintWindow.cs
@@ -441,7 +441,7 @@ namespace FlaxEditor.GUI.Docking
                     settings.AllowMinimize = false;
                     settings.HasBorder = false;
                     settings.HasSizingFrame = false;
-                    settings.IsRegularWindow = false;
+                    settings.Type = WindowType.Utility;
                     settings.SupportsTransparency = true;
                     settings.ShowInTaskbar = false;
                     settings.ShowAfterFirstPaint = false;
@@ -473,7 +473,7 @@ namespace FlaxEditor.GUI.Docking
                 settings.AllowMinimize = false;
                 settings.HasBorder = false;
                 settings.HasSizingFrame = false;
-                settings.IsRegularWindow = false;
+                settings.Type = WindowType.Utility;
                 settings.SupportsTransparency = true;
                 settings.ShowInTaskbar = false;
                 settings.ActivateWhenFirstShown = false;
