e05eedfc76d88a2b54ce2df944d19edb0f67d954
Chandler Cox <mr.chandlercox@gmail.com>
2024-07-25 17:28:55 -0500

    Force rebuild on proxy changes if rebuild is true

--- Source/Editor/Modules/ContentDatabaseModule.cs
+++ Source/Editor/Modules/ContentDatabaseModule.cs
@@ -737,7 +737,7 @@ namespace FlaxEditor.Modules
         {
             Proxy.Insert(0, proxy);
             if (rebuild)
-                Rebuild();
+                Rebuild(true);
         }
 
         /// <summary>
@@ -749,7 +749,7 @@ namespace FlaxEditor.Modules
         {
             Proxy.Remove(proxy);
             if (rebuild)
-                Rebuild();
+                Rebuild(true);
         }
 
         /// <summary>
@@ -1275,10 +1275,6 @@ namespace FlaxEditor.Modules
                 }
                 _dirtyNodes.Clear();
             }
-
-            // Lazy-rebuilds
-            if (_rebuildFlag)
-                RebuildInternal();
         }
 
         /// <inheritdoc />
