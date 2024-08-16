cc157718cd1cf89ab2fbcc299fb0b7fb163fad2f
Chandler Cox <mr.chandlercox@gmail.com>
2024-08-16 11:17:25 -0500

    Only allow adding unique proxy types to content database Proxy list.

--- Source/Editor/Modules/ContentDatabaseModule.cs
+++ Source/Editor/Modules/ContentDatabaseModule.cs
@@ -735,6 +735,8 @@ namespace FlaxEditor.Modules
         /// <param name="rebuild">Should rebuild entire database after addition.</param>
         public void AddProxy(ContentProxy proxy, bool rebuild = false)
         {
+            if (Proxy.Find(x => x.GetType().ToString().Equals(proxy.GetType().ToString(), StringComparison.Ordinal)) != null)
+                return;
             Proxy.Insert(0, proxy);
             if (rebuild)
                 Rebuild(true);
