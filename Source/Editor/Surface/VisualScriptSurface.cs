f050621e3e2e10e8133c356256b2dbf823bf089c
Wojtek Figat <wojtek@figat.pl>
2024-09-17 00:14:43 +0200

    Blacklist various std lib namespaces in Visual Scripting

--- Source/Editor/Surface/VisualScriptSurface.cs
+++ Source/Editor/Surface/VisualScriptSurface.cs
@@ -35,6 +35,14 @@ namespace FlaxEditor.Surface
             Archetypes = new List<NodeArchetype>(),
         };
 
+        private static readonly string[] _blacklistedTypeNames =
+        {
+            "Newtonsoft.Json.",
+            "System.Array",
+            "System.Linq.Expressions.",
+            "System.Reflection.",
+        };
+
         private static NodesCache _nodesCache = new NodesCache(IterateNodesCache);
         private DragActors _dragActors;
 
@@ -268,8 +276,11 @@ namespace FlaxEditor.Surface
         {
             // Skip Newtonsoft.Json stuff
             var scriptTypeTypeName = scriptType.TypeName;
-            if (scriptTypeTypeName.StartsWith("Newtonsoft.Json."))
-                return;
+            for (var i = 0; i < _blacklistedTypeNames.Length; i++)
+            {
+                if (scriptTypeTypeName.StartsWith(_blacklistedTypeNames[i]))
+                    return;
+            }
             var scriptTypeName = scriptType.Name;
 
             // Enum
