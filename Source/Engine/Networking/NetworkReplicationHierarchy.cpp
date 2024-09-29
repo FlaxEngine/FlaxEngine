FlaxEngine/FlaxEngine

commit 5f7075d2f7559e60605a6f4e11095ba198e71e01

Add `NetworkReplicationNode::SetObject` to customize object replication settings at runtime

Source/Engine/Networking/NetworkReplicationHierarchy.cpp
@@ -74,6 +74,17 @@ bool NetworkReplicationNode::GetObject(ScriptingObject* obj, NetworkReplicationH
     return false;
 }
 
+bool NetworkReplicationNode::SetObject(const NetworkReplicationHierarchyObject& value)
+{
+    const int32 index = Objects.Find(value.Object);
+    if (index != -1)
+    {
+        Objects[index] = value;
+        return true;
+    }
+    return false;
+}
+
 bool NetworkReplicationNode::DirtyObject(ScriptingObject* obj)
 {
     const int32 index = Objects.Find(obj);

Source/Engine/Networking/NetworkReplicationHierarchy.h
@@ -209,6 +209,13 @@ API_CLASS(Abstract, Namespace = "FlaxEngine.Networking") class FLAXENGINE_API Ne
     API_FUNCTION() virtual bool GetObject(ScriptingObject* obj, NetworkReplicationHierarchyObject& result);
 
     /// <summary>
+    /// Sets object properties in the hierarchy. Can be used to modify replication settings at runtime.
+    /// </summary>
+    /// <param name="value">The object data to update.</param>
+    /// <returns>True on successful update, otherwise false (eg, if specific object has not been added to this node).</returns>
+    API_FUNCTION() virtual bool SetObject(const NetworkReplicationHierarchyObject& value);
+
+    /// <summary>
     /// Force replicates the object during the next update. Resets any internal tracking state to force the synchronization.
     /// </summary>
     /// <param name="obj">The object to update.</param>

commit dd5b66386ac571513371d92c46e87864222b7253

Fix compilation regression from 5f7075d2f7559e60605a6f4e11095ba198e71e01

Source/Engine/Networking/NetworkReplicationHierarchy.cpp
@@ -76,7 +76,7 @@ bool NetworkReplicationNode::GetObject(ScriptingObject* obj, NetworkReplicationH
 
 bool NetworkReplicationNode::SetObject(const NetworkReplicationHierarchyObject& value)
 {
-    const int32 index = Objects.Find(value.Object);
+    const int32 index = Objects.Find(value.Object.Get());
     if (index != -1)
     {
         Objects[index] = value;