c6958357c38cf44c1b6ad54d5d8e9aa85a2f0cdc
Wojtek Figat <wojtek@figat.pl>
2024-10-04 15:58:47 +0200

    Allow placing `DebugCommand` attribute on fields and properties

--- Source/Engine/Debug/DebugCommands.cs
+++ Source/Engine/Debug/DebugCommands.cs
@@ -8,7 +8,7 @@ namespace FlaxEngine
     /// Marks static method as debug command that can be executed from the command line or via console.
     /// </summary>
     [Serializable]
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property)]
     public sealed class DebugCommand : Attribute
     {
     }
