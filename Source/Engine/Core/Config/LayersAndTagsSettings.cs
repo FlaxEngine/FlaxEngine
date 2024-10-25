a0aee15267b3fe19d11b65b0640088f297fc3eb4
xxSeys1 <kaktusreiniger@gmail.com>
2024-10-25 15:30:54 +0200

    add named terrain layers

--- Source/Engine/Core/Config/LayersAndTagsSettings.cs
+++ Source/Engine/Core/Config/LayersAndTagsSettings.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2012-2024 Wojciech Figat. All rights reserved.
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
 using FlaxEngine;
@@ -28,6 +29,29 @@ namespace FlaxEditor.Content.Settings
         public static string[] GetCurrentLayers()
         {
             return GetCurrentLayers(out int _);
+        }
+
+        /// <summary>
+        /// The layers names.
+        /// </summary>
+        [EditorOrder(10), EditorDisplay("Terrain Layers", EditorDisplayAttribute.InlineStyle), Collection(CanResize = false, Display = CollectionAttribute.DisplayType.Inline)]
+        public string[] TerrainLayers = new string[8];
+
+        /// <summary>
+        /// Gets the current terrain layer names. Returns "Layer" + index for layers without a name.
+        /// </summary>
+        /// <returns>The layer names.</returns>
+        public static string[] GetCurrentTerrainLayers()
+        {
+            string[] layerNames = GameSettings.Load<LayersAndTagsSettings>().TerrainLayers.ToArray();
+
+            for (int i = 0; i < layerNames.Length; i++)
+            {
+                if (string.IsNullOrEmpty(layerNames[i]))
+                    layerNames[i] = $"Terrain Layer {i}";
+            }
+
+            return layerNames;
         }
 
         [LibraryImport("FlaxEngine", EntryPoint = "LayersAndTagsSettingsInternal_GetCurrentLayers", StringMarshalling = StringMarshalling.Custom, StringMarshallingCustomType = typeof(FlaxEngine.Interop.StringMarshaller))]
