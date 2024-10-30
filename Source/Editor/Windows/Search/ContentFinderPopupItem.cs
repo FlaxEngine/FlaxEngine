0d005107b680f4ea8b11f2a662072e373816ffba
xxSeys1 <kaktusreiniger@gmail.com>
2024-10-30 13:48:16 +0100

    add popupitem for content finder

--- Source/Editor/Windows/Search/ContentFinderPopupItem.cs
+++ Source/Editor/Windows/Search/ContentFinderPopupItem.cs
@@ -123,3 +123,156 @@ namespace FlaxEditor.Windows.Search
             base.OnDestroy();
         }
     }
+
+    /// <summary>
+    /// The <see cref="PopupItemBase"/> for assets. Supports using content item thumbnail.
+    /// </summary>
+    /// <seealso cref="FlaxEditor.Windows.Search.PopupItemBase" />
+    /// <seealso cref="FlaxEditor.Content.IContentItemOwner" />
+    internal class ContentFinderPopupItem : PopupItemBase, IContentItemOwner
+    {
+        private string Type;
+
+        private ContentItem _asset;
+
+        /// <inheritdoc />
+        public ContentFinderPopupItem(string name, string type, ContentItem item, SearchableEditorPopup finder, float width, float height)
+        : base(name, item, finder, width, height)
+        {
+            _asset = item;
+            _asset.AddReference(this);
+            Type = type;
+        }
+
+        /// <inheritdoc />
+        protected override bool ShowTooltip => true;
+
+        public override void Select()
+        {
+            Editor.Instance.ContentFinding.Open(Item);
+        }
+
+        /// <inheritdoc />
+        public override bool OnShowTooltip(out string text, out Float2 location, out Rectangle area)
+        {
+            if (string.IsNullOrEmpty(TooltipText) && Item is ContentItem contentItem)
+            {
+                contentItem.UpdateTooltipText();
+                TooltipText = contentItem.TooltipText;
+            }
+            return base.OnShowTooltip(out text, out location, out area);
+        }
+
+        /// <inheritdoc />
+        public override bool OnMouseDown(Float2 location, MouseButton button)
+        {
+            if (base.OnMouseDown(location, button))
+                return true;
+            if (button == MouseButton.Right && Item is ContentItem)
+                return true;
+            return false;
+        }
+
+        /// <inheritdoc />
+        public override bool OnMouseUp(Float2 location, MouseButton button)
+        {
+            if (base.OnMouseUp(location, button))
+                return true;
+            if (button == MouseButton.Right && Item is ContentItem contentItem)
+            {
+                // Show context menu
+                var proxy = Editor.Instance.ContentDatabase.GetProxy(contentItem);
+                ContextMenuButton b;
+                var cm = new ContextMenu { Tag = contentItem };
+                b = cm.AddButton("Open", () => Editor.Instance.ContentFinding.Open(Item));
+                cm.AddSeparator();
+                cm.AddButton(Utilities.Constants.ShowInExplorer, () => FileSystem.ShowFileExplorer(System.IO.Path.GetDirectoryName(contentItem.Path)));
+                cm.AddButton("Show in Content window", () => Editor.Instance.Windows.ContentWin.Select(contentItem, true));
+                b.Enabled = proxy != null && proxy.CanReimport(contentItem);
+                if (contentItem is BinaryAssetItem binaryAsset)
+                {
+                    if (!binaryAsset.GetImportPath(out string importPath))
+                    {
+                        string importLocation = System.IO.Path.GetDirectoryName(importPath);
+                        if (!string.IsNullOrEmpty(importLocation) && System.IO.Directory.Exists(importLocation))
+                        {
+                            cm.AddButton("Show import location", () => FileSystem.ShowFileExplorer(importLocation));
+                        }
+                    }
+                }
+                cm.AddSeparator();
+                if (contentItem is AssetItem assetItem)
+                {
+                    cm.AddButton("Copy asset ID", () => Clipboard.Text = FlaxEngine.Json.JsonSerializer.GetStringID(assetItem.ID));
+                    cm.AddButton("Select actors using this asset", () => Editor.Instance.SceneEditing.SelectActorsUsingAsset(assetItem.ID));
+                    cm.AddButton("Show asset references graph", () => Editor.Instance.Windows.Open(new AssetReferencesGraphWindow(Editor.Instance, assetItem)));
+                    cm.AddButton("Copy name to Clipboard", () => Clipboard.Text = assetItem.NamePath);
+                    cm.AddButton("Copy path to Clipboard", () => Clipboard.Text = assetItem.Path);
+                    cm.AddSeparator();
+                }
+                proxy?.OnContentWindowContextMenu(cm, contentItem);
+                contentItem.OnContextMenu(cm);
+                cm.Show(this, location);
+                Cm = cm;
+
+                _finder.ResultPanel.VScrollBar.ValueChanged += () =>
+                {
+                    if (Cm != null)
+                        Cm.Hide();
+                };
+
+                return true;
+            }
+            return false;
+        }
+
+        /// <inheritdoc />
+        public override void Draw()
+        {
+            // Draw context menu hint
+            if (Cm != null && Cm.Visible)
+                Render2D.FillRectangle(new Rectangle(Float2.Zero, Size), Color.Gray);
+
+            base.Draw();
+
+            // Draw icon
+            var iconRect = _icon.Bounds;
+            _asset.DrawThumbnail(ref iconRect);
+        }
+
+        /// <inheritdoc />
+        public override void OnDestroy()
+        {
+            Cm = null;
+            if (_asset != null)
+            {
+                _asset.RemoveReference(this);
+                _asset = null;
+            }
+
+            base.OnDestroy();
+        }
+
+        /// <inheritdoc />
+        public void OnItemDeleted(ContentItem item)
+        {
+            Dispose();
+        }
+
+        /// <inheritdoc />
+        public void OnItemRenamed(ContentItem item)
+        {
+        }
+
+        /// <inheritdoc />
+        public void OnItemReimported(ContentItem item)
+        {
+        }
+
+        /// <inheritdoc />
+        public void OnItemDispose(ContentItem item)
+        {
+            Dispose();
+        }
+    }
+
