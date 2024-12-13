c6b7077c2d4f29c15872c5044e93d96e06d9f80c
Gary M <exkage@yahoo.com>
2024-12-12 22:23:21 -0800

    Remove unnecessary comments

--- Source/Engine/Tools/ModelTool/ModelTool.Assimp.cpp
+++ Source/Engine/Tools/ModelTool/ModelTool.Assimp.cpp
@@ -766,8 +766,6 @@ bool ModelTool::ImportDataAssimp(const String& path, ModelData& data, Options& o
         if (options.CalculateTangents)
             flags |= aiProcess_CalcTangentSpace;
         if (options.ReverseWindingOrder)
-            // actually we need to remove this flag
-            // flags |= aiProcess_FlipWindingOrder;
             flags &= ~aiProcess_FlipWindingOrder;
         if (options.OptimizeMeshes)
             flags |= aiProcess_OptimizeMeshes | aiProcess_SplitLargeMeshes | aiProcess_ImproveCacheLocality;
