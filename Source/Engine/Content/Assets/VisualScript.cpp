8b1d678f2e104e33f19bdd21699779dd80f22f02
Wojtek Figat <wojtek@figat.pl>
2025-02-17 10:23:07 +0100

    Fix scene object reference serialization in Visual Script properties
    #3136

--- Source/Engine/Content/Assets/VisualScript.cpp
+++ Source/Engine/Content/Assets/VisualScript.cpp
@@ -19,6 +19,7 @@
 #include "Engine/Profiler/ProfilerCPU.h"
 #include "Engine/Utilities/StringConverter.h"
 #include "Engine/Threading/MainThreadTask.h"
+#include "Engine/Level/SceneObject.h"
 #include "FlaxEngine.Gen.h"
 
 namespace
@@ -39,6 +40,22 @@ namespace
         Log::Logger::Write(type, TEXT("Visual Script stack trace:"));
         Log::Logger::Write(type, stack);
         Log::Logger::Write(type, TEXT(""));
+    }
+
+    bool SerializeValue(const Variant& a, const Variant& b)
+    {
+        bool result = a != b;
+        if (result)
+        {
+            // Special case for scene objects to handle prefab object references
+            auto* aSceneObject = ScriptingObject::Cast<SceneObject>((ScriptingObject*)a);
+            auto* bSceneObject = ScriptingObject::Cast<SceneObject>((ScriptingObject*)b);
+            if (aSceneObject && bSceneObject)
+            {
+                result = Serialization::ShouldSerialize(aSceneObject, bSceneObject);
+            }
+        }
+        return result;
     }
 }
 
@@ -1960,7 +1977,7 @@ void VisualScriptingBinaryModule::SerializeObject(JsonWriter& stream, ScriptingO
                         auto& param = asset->Graph.Parameters[paramIndex];
                         auto& value = params[paramIndex];
                         auto& otherValue = otherParams->Value.Params[paramIndex];
-                        if (value != otherValue)
+                        if (SerializeValue(value, otherValue))
                         {
                             param.Identifier.ToString(idName, Guid::FormatType::N);
                             stream.Key(idName, 32);
@@ -1975,7 +1992,7 @@ void VisualScriptingBinaryModule::SerializeObject(JsonWriter& stream, ScriptingO
                         auto& param = asset->Graph.Parameters[paramIndex];
                         auto& value = params[paramIndex];
                         auto& otherValue = param.Value;
-                        if (value != otherValue)
+                        if (SerializeValue(value, otherValue))
                         {
                             param.Identifier.ToString(idName, Guid::FormatType::N);
                             stream.Key(idName, 32);
