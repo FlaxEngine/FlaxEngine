d54e8c210485b82b4d5aaeac113e3f9d3b7f0f87
xxSeys1 <kaktusreiniger@gmail.com>
2025-03-17 23:59:06 +0100

    add option to add a canvas scaler to new widget

--- Source/Editor/Content/Create/PrefabCreateEntry.cs
+++ Source/Editor/Content/Create/PrefabCreateEntry.cs
@@ -109,6 +109,12 @@ namespace FlaxEditor.Content.Create
             [Tooltip("The control type of the root of the new Widget's root control."), VisibleIf(nameof(ShowRoot))]
             public Type RootControlType = typeof(Button);
 
+            /// <summary>
+            /// If a <see cref="CanvasScaler"/> should be added to the widget.
+            /// </summary>
+            [Tooltip("If checked, a Canvas Scaler will be added to the widget."), VisibleIf(nameof(ShowRoot), true)]
+            public bool AddCanvasScaler = true;
+
             private static bool IsValid(Type type)
             {
                 return (type.IsPublic || type.IsNestedPublic) && !type.IsAbstract && !type.IsGenericType;
@@ -158,6 +164,20 @@ namespace FlaxEditor.Content.Create
             else if (_options.WidgetInitializationMode == Options.WidgetMode.Canvas)
             {
                 actor = new UICanvas();
+                
+                if (_options.AddCanvasScaler)
+                {
+                    Actor canvasScaler;
+                    Control control = new CanvasScaler();
+
+                    canvasScaler = new UIControl
+                    {
+                        Control = control,
+                        Name = typeof(CanvasScaler).Name
+                    };
+
+                    canvasScaler.SetParent(actor, true);
+                }
             }
 
             if (actor == null)
