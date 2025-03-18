9459eff58c64acd3520248f72fada5c0f9534032
Wojtek Figat <wojtek@figat.pl>
2025-03-18 20:07:30 +0100

    Fix regression in game viewport DPI scale usage
    #2976 809fd2653ad0ef667f286b0dc7dc150fe138a63f

--- Source/Editor/Editor.cs
+++ Source/Editor/Editor.cs
@@ -1514,7 +1514,8 @@ namespace FlaxEditor
             var win = Windows.GameWin?.Root;
             if (win?.RootWindow is WindowRootControl root)
             {
-                pos = Float2.Round(Windows.GameWin.Viewport.PointFromScreen(pos) * root.DpiScale);
+                pos = Windows.GameWin.Viewport.PointFromScreen(pos);
+                pos = Float2.Round(pos);
             }
             else
             {
@@ -1527,7 +1528,8 @@ namespace FlaxEditor
             var win = Windows.GameWin?.Root;
             if (win?.RootWindow is WindowRootControl root)
             {
-                pos = Float2.Round(Windows.GameWin.Viewport.PointToScreen(pos / root.DpiScale));
+                pos = Windows.GameWin.Viewport.PointToScreen(pos);
+                pos = Float2.Round(pos);
             }
             else
             {
@@ -1559,6 +1561,7 @@ namespace FlaxEditor
                 else
                     result = gameWin.Viewport.Size;
 
+                result *= root.DpiScale;
                 result = Float2.Round(result);
             }
         }
