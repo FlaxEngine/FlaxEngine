7a91a2322064e51d455ff44068398850a1e0c41e
Ari Vuollet <ari.vuollet@goat.moe>
2025-03-26 19:12:30 +0200

    Release rubber band selection when viewport starts controlling the view

--- Source/Editor/Viewport/MainEditorGizmoViewport.cs
+++ Source/Editor/Viewport/MainEditorGizmoViewport.cs
@@ -611,8 +611,16 @@ namespace FlaxEditor.Viewport
 
             // Don't allow rubber band selection when gizmo is controlling mouse, vertex painting mode, or cloth painting is enabled
             bool canStart = !(IsControllingMouse || IsRightMouseButtonDown || IsAltKeyDown) &&
-                            Gizmos?.Active is TransformGizmo && !Gizmos.Active.IsControllingMouse;
+                            Gizmos?.Active is TransformGizmo;
             _rubberBandSelector.TryCreateRubberBand(canStart, _viewMousePos);
+        }
+
+        /// <inheritdoc />
+        protected override void OnControlMouseBegin(Window win)
+        {
+            _rubberBandSelector.ReleaseRubberBandSelection();
+
+            base.OnControlMouseBegin(win);
         }
 
         /// <inheritdoc />
