ec93b2ff06991c26785f5bee696ab88485e753b1
xxSeys1 <kaktusreiniger@gmail.com>
2025-04-07 21:45:21 +0200

    move animation related actors to context submenu

--- Source/Engine/Level/Actors/BoneSocket.h
+++ Source/Engine/Level/Actors/BoneSocket.h
@@ -7,7 +7,7 @@
 /// <summary>
 /// Actor that links to the animated model skeleton node transformation.
 /// </summary>
-API_CLASS(Attributes="ActorContextMenu(\"New/Other/Bone Socket\"), ActorToolbox(\"Other\")")
+API_CLASS(Attributes="ActorContextMenu(\"New/Animation/Bone Socket\"), ActorToolbox(\"Other\")")
 class FLAXENGINE_API BoneSocket : public Actor
 {
     DECLARE_SCENE_OBJECT(BoneSocket);
