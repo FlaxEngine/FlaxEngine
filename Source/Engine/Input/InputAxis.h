691a71c14c4effb06f0030f44c53eccc7863803a
Chandler Cox <mr.chandlercox@gmail.com>
2025-04-24 17:43:46 -0500

    Add input event and input axis to Cpp scripting.

---
+++ Source/Engine/Input/InputAxis.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#include "Input.h"
+
+#include "Engine/Core/ISerializable.h"
+
+API_CLASS() class FLAXENGINE_API InputAxis : public ScriptingObject, public ISerializable
+{
+    API_AUTO_SERIALIZATION();
+    DECLARE_SCRIPTING_TYPE(InputAxis);
+
+public:
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InputEvent"/> class.
+    /// </summary>
+    /// <param name="name">The action name.</param>
+    InputAxis(String name);
+
+    ~InputAxis();
+
+    /// <summary>
+    /// The name of the axis to use. See <see cref="Input.AxisMappings"/>.
+    /// </summary>
+    API_FIELD() String Name;
+
+    /// <summary>
+    /// Gets the current axis value.
+    /// </summary>
+    API_PROPERTY() FORCE_INLINE float GetValue() const { return Input::GetAxis(Name); }
+
+    /// <summary>
+    /// Gets the current axis raw value.
+    /// </summary>
+    API_PROPERTY() FORCE_INLINE float GetValueRaw() const { return Input::GetAxisRaw(Name); }
+
+    /// <summary>
+    /// Occurs when axis is changed. Called before scripts update.
+    /// </summary>
+    API_EVENT() Action ValueChanged;
+
+    /// <summary>
+    /// Dispose of this object.
+    /// </summary>
+    API_FUNCTION() void Dispose();
+
+private:
+    void Handler(StringView name);
+};
---
+++ Source/Engine/Input/InputEvent.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+#include "Input.h"
+
+#include "Engine/Core/ISerializable.h"
+
+API_CLASS() class FLAXENGINE_API InputEvent : public ScriptingObject, public ISerializable
+{
+    API_AUTO_SERIALIZATION();
+    DECLARE_SCRIPTING_TYPE(InputEvent);
+
+public:
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InputEvent"/> class.
+    /// </summary>
+    /// <param name="name">The action name.</param>
+    InputEvent(String name);
+
+    ~InputEvent();
+
+    /// <summary>
+    /// The name of the action to use. See <see cref="Input.ActionMappings"/>.
+    /// </summary>
+    API_FIELD() String Name;
+
+    /// <summary>
+    /// Returns true if the event has been triggered during the current frame (e.g. user pressed a key). Use <see cref="Pressed"/> to catch events without active waiting.
+    /// </summary>
+    API_PROPERTY() FORCE_INLINE bool GetActive() const { return Input::GetAction(Name); }
+
+    /// <summary>
+    /// Returns the event state. Use <see cref="Pressed"/>, <see cref="Pressing"/>, <see cref="Released"/> to catch events without active waiting.
+    /// </summary>
+    API_PROPERTY() FORCE_INLINE InputActionState GetState() const { return Input::GetActionState(Name); }
+
+    /// <summary>
+    /// Occurs when event is pressed (e.g. user pressed a key). Called before scripts update.
+    /// </summary>
+    API_EVENT() Action Pressed;
+
+    /// <summary>
+    /// Occurs when event is being pressing (e.g. user pressing a key). Called before scripts update.
+    /// </summary>
+    API_EVENT() Action Pressing;
+
+    /// <summary>
+    /// Occurs when event is released (e.g. user releases a key). Called before scripts update.
+    /// </summary>
+    API_EVENT() Action Released;
+
+    /// <summary>
+    /// Dispose of this object.
+    /// </summary>
+    API_FUNCTION() void Dispose();
+    
+private:
+    void Handler(StringView name, InputActionState state);
+};
