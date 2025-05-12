3b73401bc48ba22072eb6837f9caa7695e7181ea
Phantom <salvini.michael01@gmail.com>
2025-05-12 23:47:44 +0200

    Create Audio Mixer Group for Editor side

---
+++ Source/Editor/CustomEditors/Dedicated/AudioMixerGroupEditor.cs
@@ -0,0 +1,51 @@
+// Copyright (c) Wojciech Figat. All rights reserved.
+
+using FlaxEditor.Content.Settings;
+using FlaxEditor.CustomEditors.Elements;
+using FlaxEditor.GUI;
+using FlaxEngine;
+
+namespace FlaxEditor.CustomEditors.Dedicated
+{
+    /// <summary>
+    /// Custom editor for <see cref="AudioMixerGroup"/> index.
+    /// </summary>
+    internal class AudioMixerGroupEditor : CustomEditor
+    {
+        private ComboBoxElement _element;
+
+        /// <inheritdoc />
+        public override DisplayStyle Style => DisplayStyle.Inline;
+
+        /// <inheritdoc />
+        public override void Initialize(LayoutElementsContainer layout)
+        {
+            _element = layout.ComboBox();
+            _element.ComboBox.SelectedIndexChanged += OnSelectedIndexChanged;
+            _element.ComboBox.AddItem("None");
+            var groups = GameSettings.Load<AudioSettings>();
+            if (groups?.AudioMixerGroups != null)
+            {
+                for (int i = 0; i < groups.AudioMixerGroups.Length; i++)
+                {
+                    _element.ComboBox.AddItem(groups.AudioMixerGroups[i].Name);
+                }
+            }
+        }
+
+        private void OnSelectedIndexChanged(ComboBox comboBox)
+        {
+            var value = comboBox.HasSelection ? comboBox.SelectedIndex - 1 : -1;
+            SetValue(value);
+        }
+
+        /// <inheritdoc />
+        public override void Refresh()
+        {
+            base.Refresh();
+
+            var value = (int)Values[0];
+            _element.ComboBox.SelectedIndex = value + 1;
+        }
+    }
+}
