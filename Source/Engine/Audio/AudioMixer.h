ea3bd5bb39d37b3857d0579dcce6f05632790f25
Phantom <salvini.michael01@gmail.com>
2025-05-19 15:08:58 +0200

    Update Audio Mixer

--- Source/Engine/Audio/AudioMixer.h
+++ Source/Engine/Audio/AudioMixer.h
@@ -10,4 +10,25 @@
 API_CLASS(NoSpawn) class FLAXENGINE_API AudioMixer : public BinaryAsset 
 {
     DECLARE_BINARY_ASSET_HEADER(AudioMixer,2);
+
+public:
+    /// <summary>
+    /// The collection of audio mixer variables identified by the name.
+    /// </summary>
+    Dictionary<String, float> AudioMixerVariables;
+
+
+
+public:
+    // [BinaryAsset]
+   void InitAsVirtual() override;
+#if USE_EDITOR
+   bool Save(const StringView& path = StringView::Empty) override;
+#endif
+
+protected:
+    // [BinaryAsset]
+    LoadResult load() override;
+    void unload(bool isReloading) override;
+    AssetChunksFlag getChunksToPreload() const override;
 };
