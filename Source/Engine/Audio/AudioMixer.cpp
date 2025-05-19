afd7596c6c2c222dd482c98f9e326c1ee05d16f8
Phantom <salvini.michael01@gmail.com>
2025-05-19 18:30:35 +0200

    -u

--- Source/Engine/Audio/AudioMixer.cpp
+++ Source/Engine/Audio/AudioMixer.cpp
@@ -8,6 +8,7 @@
 #include "Engine/Content/Factories/BinaryAssetFactory.h"
 #include "Engine/Content/Upgraders/BinaryAssetUpgrader.h"
 #include "Engine/Threading/Threading.h"
+#include "AudioSettings.h"
 
 #if USE_EDITOR
     bool AudioMixer::Save(const StringView& path)
@@ -17,13 +18,17 @@
         ScopeLock lock(Locker);
 
         // Save to bytes
+        const auto AudioMixerGroup = AudioSettings::Get()->AudioMixerGroups;
         MemoryWriteStream stream(1024);
         stream.Write(AudioMixerVariables.Count());
-
-        for (auto& e : AudioMixerVariables) 
+        for (int index = 0; index < AudioMixerGroup.Count(); index++) 
         {
-            stream.Write(e.Key, 71);
-            stream.Write(e.Value.DefaultValue);
+            for (auto& e : AudioMixerVariables)
+            {
+                e.Key = AudioMixerGroup[index].Name; e.Value.DefaultValue = AudioMixerGroup[index].MixerVolume;
+                stream.Write(e.Key, 71);
+                stream.Write(e.Value.DefaultValue);
+            }
         }
 
         // Set chunk data
@@ -41,6 +46,17 @@
         // Save
         AssetInitData data;
         data.SerializedVersion = SerializedVersion;
+        const bool saveResult = path.HasChars() ? SaveAsset(path, data) : SaveAsset(data, true);
+        if (IsVirtual()) 
+        {
+            _header.Chunks[0] = nullptr;
+            Delete(chunk);
+        }
+        if (saveResult) 
+        {
+            LOG(Error, "Cannot save \'{0}\'", ToString());
+            return true;
+        }
 
         return false;
     }
@@ -51,4 +67,44 @@ void AudioMixer::InitAsVirtual()
     BinaryAsset::InitAsVirtual();
 
     AudioMixerVariables.Clear();
+}
+
+Asset::LoadResult AudioMixer::load() 
+{
+    // Get data
+    const auto chunk = GetChunk(0);
+    if (!chunk || !chunk->IsLoaded())
+        return LoadResult::MissingDataChunk;
+    MemoryReadStream stream(chunk->Get(), chunk->Size());
+
+    // Load all variables
+    int32 count;
+    stream.Read(count);
+    AudioMixerVariables.EnsureCapacity(count);
+    String Name;
+    for (int i = 0; i < count; i++) 
+    {
+        stream.Read(Name, 71);
+        auto& e = AudioMixerVariables[Name];
+        stream.Read(e.DefaultValue);
+        e.Value = e.DefaultValue;
+    }
+    if(stream.HasError())
+    {
+        // Failed to load data
+        AudioMixerVariables.Clear();
+        return LoadResult::InvalidData;
+    }
+
+    return LoadResult::Ok;
+}
+
+void AudioMixer::unload(bool isReloading)
+{
+    AudioMixerVariables.Clear();
+}
+
+AssetChunksFlag AudioMixer::getChunksToPreload() const
+{
+    return GET_CHUNK_FLAG(0);
 }
