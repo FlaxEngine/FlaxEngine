bdc87c7bc6582f1be10774cf03664a7f9ee8e724
Wojtek Figat <wojtek@figat.pl>
2025-06-12 17:26:39 +0200

    Update min supported version of macOS to 13 and iOS to 15

--- Source/Editor/Cooker/Platform/Mac/MacPlatformTools.cpp
+++ Source/Editor/Cooker/Platform/Mac/MacPlatformTools.cpp
@@ -186,7 +186,7 @@ bool MacPlatformTools::OnPostProcess(CookingData& data)
         ADD_ENTRY("CFBundlePackageType", "APPL");
         ADD_ENTRY("NSPrincipalClass", "NSApplication");
         ADD_ENTRY("LSApplicationCategoryType", "public.app-category.games");
-        ADD_ENTRY("LSMinimumSystemVersion", "10.15");
+        ADD_ENTRY("LSMinimumSystemVersion", "13");
         ADD_ENTRY("CFBundleIconFile", "icon.icns");
         ADD_ENTRY_STR("CFBundleExecutable", executableName);
         ADD_ENTRY_STR("CFBundleIdentifier", appIdentifier);
