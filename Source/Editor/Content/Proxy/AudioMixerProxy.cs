8519d6cef67af171fbb89d22719b863c4c2ba6b0
Phantom <salvini.michael01@gmail.com>
2025-06-23 13:44:01 +0200

    Create Audio Mixer Proxy class

---
+++ Source/Editor/Content/Proxy/AudioMixerProxy.cs
@@ -0,0 +1,47 @@
+// Copyright (c) Wojciech Figat. All rights reserved.
+
+using System;
+using FlaxEditor.Windows;
+using FlaxEditor.Windows.Assets;
+using FlaxEngine;
+
+namespace FlaxEditor.Content
+{
+    /// <summary>
+    /// A <see cref="AudioMixer"/> asset proxy object.
+    /// </summary>
+    /// <seealso cref="FlaxEditor.Content.BinaryAssetProxy" />
+    [ContentContextMenu("New/Audio/New Mixer")]
+    public class AudioMixerProxy : BinaryAssetProxy
+    {
+        /// <inheritdoc />
+        public override string Name => "Audio Mixer";
+
+        /// <inheritdoc />
+        public override EditorWindow Open(Editor editor, ContentItem item)
+        {
+            return new AudioMixerWindow(editor, (AssetItem)item);
+        }
+
+        /// <inheritdoc />
+        public override Color AccentColor => Color.FromRGB(0xccff33);
+
+        /// <inheritdoc />
+        public override Type AssetType => typeof(GameplayGlobals);
+
+        /// <inheritdoc />
+        public override bool CanCreate(ContentFolder targetLocation)
+        {
+            return targetLocation.CanHaveAssets;
+        }
+
+        /// <inheritdoc />
+        public override void Create(string outputPath, object arg)
+        {
+            var asset = FlaxEngine.Content.CreateVirtualAsset<AudioMixer>();
+            if (asset.Save(outputPath))
+                throw new Exception("Failed to create new asset.");
+            FlaxEngine.Object.Destroy(asset);
+        }
+    }
+}
