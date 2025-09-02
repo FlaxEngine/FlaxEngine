ad1163bccc06faa3842dffd2bf47efa9786a2167
Wojtek Figat <wojtek@figat.pl>
2025-09-02 21:41:54 +0200

    Fix `HashSet::Add` returning incorrect value

--- Source/Engine/Core/Collections/HashSet.h
+++ Source/Engine/Core/Collections/HashSet.h
@@ -408,7 +408,7 @@ public:
     template<typename ItemType>
     bool Add(const ItemType& item)
     {
-        Bucket* bucket = Base::OnAdd(item, false);
+        Bucket* bucket = Base::OnAdd(item, false, true);
         if (bucket)
             bucket->Occupy(item);
         return bucket != nullptr;
@@ -421,7 +421,7 @@ public:
     /// <returns>True if element has been added to the collection, otherwise false if the element is already present.</returns>
     bool Add(T&& item)
     {
-        Bucket* bucket = Base::OnAdd(item, false);
+        Bucket* bucket = Base::OnAdd(item, false, true);
         if (bucket)
             bucket->Occupy(MoveTemp(item));
         return bucket != nullptr;
