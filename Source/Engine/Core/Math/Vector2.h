9fe015de26d9f64f3aa50cc30de7982f500edcb9
Phantom <salvini.michael01@gmail.com>
2025-09-16 01:30:24 +0200

    Add Slerp on Vector2

--- Source/Engine/Core/Math/Vector2.h
+++ Source/Engine/Core/Math/Vector2.h
@@ -558,6 +558,26 @@ public:
         return result;
     }
 
+    // Performs a spherical linear interpolation between two vectors.
+    static void Slerp(const Vector2Base& start, const Vector2Base& end, T amount, Vector2Base& result) 
+    {
+        float dot = Vector2::Dot(start, end);
+        dot = Math::Clamp(dot, -1.0f, 1.0f);
+        float theta = Math::Acos(dot) * amount;
+        Vector3Base RelativeVector = end - start * dot;
+        RelativeVector.Normalize();
+        result = ((start * Math::Cos(theta)) + (RelativeVector * Math::Sin(theta)));
+        return result;
+    }
+
+    // Performs a spherical linear interpolation between two vectors.
+    static Vector2Base Slerp(const Vector2Base& start, const Vector2Base& end, T amount) 
+    {
+        Vector2Base result;
+        Slerp(start, end, amount, result);
+        return result;
+    }
+
 public:
     /// <summary>
     /// Calculates the area of the triangle.
