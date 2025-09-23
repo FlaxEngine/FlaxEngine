FlaxEngine/FlaxEngine

commit a471861e9257c426404984b14e53ef61f769c243

Fix editing UI Brush in prefabs
#3501

Source/Editor/CustomEditors/Editors/ObjectSwitcherEditor.cs
@@ -3,6 +3,7 @@
 using System;
 using FlaxEditor.GUI;
 using FlaxEditor.Scripting;
+using FlaxEngine;
 using FlaxEngine.Utilities;
 
 namespace FlaxEditor.CustomEditors.Editors
@@ -81,8 +82,12 @@ namespace FlaxEditor.CustomEditors.Editors
 
         private OptionType[] _options;
         private ScriptType _type;
+        private Elements.PropertiesListElement _typeItem;
 
         private ScriptType Type => Values[0] == null ? Values.Type : TypeUtils.GetObjectType(Values[0]);
+
+        /// <inheritdoc />
+        public override bool RevertValueWithChildren => false; // Always revert value for a whole object
 
         /// <inheritdoc />
         public override void Initialize(LayoutElementsContainer layout)
@@ -98,7 +103,8 @@ namespace FlaxEditor.CustomEditors.Editors
             _type = type;
 
             // Type
-            var typeEditor = layout.ComboBox(TypeComboBoxName, "Type of the object value. Use it to change the object.");
+            _typeItem = layout.AddPropertyItem(TypeComboBoxName, "Type of the object value. Use it to change the object.");
+            var typeEditor = _typeItem.ComboBox();
             for (int i = 0; i < _options.Length; i++)
             {
                 typeEditor.ComboBox.AddItem(_options[i].Name);
@@ -126,6 +132,8 @@ namespace FlaxEditor.CustomEditors.Editors
 
             // Value
             var values = new CustomValueContainer(type, (instance, index) => instance);
+            if (Values.HasReferenceValue)
+                values.SetReferenceValue(Values.ReferenceValue);
             values.AddRange(Values);
             var editor = CustomEditorsUtil.CreateEditor(type);
             var style = editor.Style;
@@ -169,6 +177,12 @@ namespace FlaxEditor.CustomEditors.Editors
         public override void Refresh()
         {
             base.Refresh();
+
+            // Show prefab diff when reference value type is different
+            var color = Color.Transparent;
+            if (Values.HasReferenceValue && CanRevertReferenceValue && Values[0].GetType() != Values.ReferenceValue.GetType())
+                color = FlaxEngine.GUI.Style.Current.BackgroundSelected;
+            _typeItem.Labels[0].HighlightStripColor = color;
 
             // Check if type has been modified outside the editor (eg. from code)
             if (Type != _type)

Source/Engine/UI/GUI/Brushes/GPUTextureBrush.cs
@@ -1,12 +1,14 @@
 // Copyright (c) Wojciech Figat. All rights reserved.
 
+using System;
+
 namespace FlaxEngine.GUI
 {
     /// <summary>
     /// Implementation of <see cref="IBrush"/> for <see cref="FlaxEngine.GPUTexture"/>.
     /// </summary>
     /// <seealso cref="IBrush" />
-    public sealed class GPUTextureBrush : IBrush
+    public sealed class GPUTextureBrush : IBrush, IEquatable<GPUTextureBrush>
     {
         /// <summary>
         /// The GPU texture.
@@ -46,6 +48,30 @@ namespace FlaxEngine.GUI
                 Render2D.DrawTexturePoint(Texture, rect, color);
             else
                 Render2D.DrawTexture(Texture, rect, color);
+        }
+
+        /// <inheritdoc />
+        public bool Equals(GPUTextureBrush other)
+        {
+            return other != null && Texture == other.Texture && Filter == other.Filter;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is GPUTextureBrush other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Texture, (int)Filter);
+        }
+
+        /// <inheritdoc />
+        public int CompareTo(object obj)
+        {
+            return Equals(obj) ? 1 : 0;
         }
     }
 }

Source/Engine/UI/GUI/Brushes/IBrush.cs
@@ -1,5 +1,7 @@
 // Copyright (c) Wojciech Figat. All rights reserved.
 
+using System;
+
 namespace FlaxEngine.GUI
 {
     /// <summary>
@@ -23,7 +25,7 @@ namespace FlaxEngine.GUI
     /// <summary>
     /// Interface that unifies input source textures, sprites, render targets, and any other brushes to be used in a more generic way.
     /// </summary>
-    public interface IBrush
+    public interface IBrush : IComparable
     {
         /// <summary>
         /// Gets the size of the image brush in pixels (if relevant).

Source/Engine/UI/GUI/Brushes/LinearGradientBrush.cs
@@ -1,12 +1,14 @@
 // Copyright (c) Wojciech Figat. All rights reserved.
 
+using System;
+
 namespace FlaxEngine.GUI
 {
     /// <summary>
     /// Implementation of <see cref="IBrush"/> for linear color gradient (made of 2 color).
     /// </summary>
     /// <seealso cref="IBrush" />
-    public sealed class LinearGradientBrush : IBrush
+    public sealed class LinearGradientBrush : IBrush, IEquatable<LinearGradientBrush>
     {
         /// <summary>
         /// The brush start color.
@@ -49,6 +51,30 @@ namespace FlaxEngine.GUI
             var startColor = StartColor * color;
             var endColor = EndColor * color;
             Render2D.FillRectangle(rect, startColor, startColor, endColor, endColor);
+        }
+
+        /// <inheritdoc />
+        public bool Equals(LinearGradientBrush other)
+        {
+            return other != null && StartColor == other.StartColor && EndColor == other.EndColor;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is LinearGradientBrush other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(StartColor, EndColor);
+        }
+
+        /// <inheritdoc />
+        public int CompareTo(object obj)
+        {
+            return Equals(obj) ? 1 : 0;
         }
     }
 }

Source/Engine/UI/GUI/Brushes/MaterialBrush.cs
@@ -1,12 +1,14 @@
 // Copyright (c) Wojciech Figat. All rights reserved.
 
+using System;
+
 namespace FlaxEngine.GUI
 {
     /// <summary>
     /// Implementation of <see cref="IBrush"/> for <see cref="FlaxEngine.MaterialBase"/> rendering.
     /// </summary>
     /// <seealso cref="IBrush" />
-    public sealed class MaterialBrush : IBrush
+    public sealed class MaterialBrush : IBrush, IEquatable<MaterialBrush>
     {
         /// <summary>
         /// The material.
@@ -37,6 +39,30 @@ namespace FlaxEngine.GUI
         public void Draw(Rectangle rect, Color color)
         {
             Render2D.DrawMaterial(Material, rect, color);
+        }
+
+        /// <inheritdoc />
+        public bool Equals(MaterialBrush other)
+        {
+            return other != null && Material == other.Material;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is MaterialBrush other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Material);
+        }
+
+        /// <inheritdoc />
+        public int CompareTo(object obj)
+        {
+            return Equals(obj) ? 1 : 0;
         }
     }
 }

Source/Engine/UI/GUI/Brushes/SolidColorBrush.cs
@@ -1,12 +1,14 @@
 // Copyright (c) Wojciech Figat. All rights reserved.
 
+using System;
+
 namespace FlaxEngine.GUI
 {
     /// <summary>
     /// Implementation of <see cref="IBrush"/> for single color fill.
     /// </summary>
     /// <seealso cref="IBrush" />
-    public sealed class SolidColorBrush : IBrush
+    public sealed class SolidColorBrush : IBrush, IEquatable<SolidColorBrush>
     {
         /// <summary>
         /// The brush color.
@@ -38,6 +40,30 @@ namespace FlaxEngine.GUI
         public void Draw(Rectangle rect, Color color)
         {
             Render2D.FillRectangle(rect, Color * color);
+        }
+
+        /// <inheritdoc />
+        public bool Equals(SolidColorBrush other)
+        {
+            return other != null && Color == other.Color;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is SolidColorBrush other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Color);
+        }
+
+        /// <inheritdoc />
+        public int CompareTo(object obj)
+        {
+            return Equals(obj) ? 1 : 0;
         }
     }
 }

Source/Engine/UI/GUI/Brushes/SpriteBrush.cs
@@ -1,12 +1,14 @@
 // Copyright (c) Wojciech Figat. All rights reserved.
 
+using System;
+
 namespace FlaxEngine.GUI
 {
     /// <summary>
     /// Implementation of <see cref="IBrush"/> for <see cref="FlaxEngine.Sprite"/>.
     /// </summary>
     /// <seealso cref="IBrush" />
-    public sealed class SpriteBrush : IBrush
+    public sealed class SpriteBrush : IBrush, IEquatable<SpriteBrush>
     {
         /// <summary>
         /// The sprite.
@@ -46,6 +48,30 @@ namespace FlaxEngine.GUI
                 Render2D.DrawSpritePoint(Sprite, rect, color);
             else
                 Render2D.DrawSprite(Sprite, rect, color);
+        }
+
+        /// <inheritdoc />
+        public bool Equals(SpriteBrush other)
+        {
+            return other != null && Sprite == other.Sprite && Filter == other.Filter;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is SpriteBrush other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Sprite, (int)Filter);
+        }
+
+        /// <inheritdoc />
+        public int CompareTo(object obj)
+        {
+            return Equals(obj) ? 1 : 0;
         }
     }
 
@@ -120,6 +146,30 @@ namespace FlaxEngine.GUI
                 Render2D.DrawRectangle(bordersRect, Color.YellowGreen, 2.0f);
             }
 #endif
+        }
+
+        /// <inheritdoc />
+        public bool Equals(Sprite9SlicingBrush other)
+        {
+            return other != null && Sprite == other.Sprite && Filter == other.Filter;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is Sprite9SlicingBrush other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Sprite, (int)Filter);
+        }
+
+        /// <inheritdoc />
+        public int CompareTo(object obj)
+        {
+            return Equals(obj) ? 1 : 0;
         }
     }
 }

Source/Engine/UI/GUI/Brushes/TextureBrush.cs
@@ -1,12 +1,14 @@
 // Copyright (c) Wojciech Figat. All rights reserved.
 
+using System;
+
 namespace FlaxEngine.GUI
 {
     /// <summary>
     /// Implementation of <see cref="IBrush"/> for <see cref="FlaxEngine.Texture"/>.
     /// </summary>
     /// <seealso cref="IBrush" />
-    public sealed class TextureBrush : IBrush
+    public sealed class TextureBrush : IBrush, IEquatable<TextureBrush>
     {
         /// <summary>
         /// The texture.
@@ -47,13 +49,37 @@ namespace FlaxEngine.GUI
             else
                 Render2D.DrawTexture(Texture, rect, color);
         }
+
+        /// <inheritdoc />
+        public bool Equals(TextureBrush other)
+        {
+            return other != null && Texture == other.Texture && Filter == other.Filter;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is TextureBrush other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Texture, (int)Filter);
+        }
+
+        /// <inheritdoc />
+        public int CompareTo(object obj)
+        {
+            return Equals(obj) ? 1 : 0;
+        }
     }
 
     /// <summary>
     /// Implementation of <see cref="IBrush"/> for <see cref="FlaxEngine.Texture"/> using 9-slicing.
     /// </summary>
     /// <seealso cref="IBrush" />
-    public sealed class Texture9SlicingBrush : IBrush
+    public sealed class Texture9SlicingBrush : IBrush, IEquatable<Texture9SlicingBrush>
     {
         /// <summary>
         /// The texture.
@@ -129,6 +155,30 @@ namespace FlaxEngine.GUI
                 Render2D.DrawRectangle(bordersRect, Color.YellowGreen, 2.0f);
             }
 #endif
+        }
+
+        /// <inheritdoc />
+        public bool Equals(Texture9SlicingBrush other)
+        {
+            return other != null && Texture == other.Texture && Filter == other.Filter && BorderSize == other.BorderSize && Border == other.Border;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is Texture9SlicingBrush other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Texture, (int)Filter, BorderSize, Border.GetHashCode());
+        }
+
+        /// <inheritdoc />
+        public int CompareTo(object obj)
+        {
+            return Equals(obj) ? 1 : 0;
         }
     }
 }

commit 7060cb5696976eb8029e2795b9445160de3df1cc

Fix UI Brush editing in prefab regression from a471861e9257c426404984b14e53ef61f769c243
#3694

Source/Editor/CustomEditors/Editors/ObjectSwitcherEditor.cs
@@ -180,7 +180,7 @@ namespace FlaxEditor.CustomEditors.Editors
 
             // Show prefab diff when reference value type is different
             var color = Color.Transparent;
-            if (Values.HasReferenceValue && CanRevertReferenceValue && Values[0].GetType() != Values.ReferenceValue.GetType())
+            if (Values.HasReferenceValue && CanRevertReferenceValue && Values[0]?.GetType() != Values.ReferenceValue?.GetType())
                 color = FlaxEngine.GUI.Style.Current.BackgroundSelected;
             _typeItem.Labels[0].HighlightStripColor = color;
 