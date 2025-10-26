a0689f3f00790607fe1a45e39e31466be0877b90
Wojtek Figat <wojtek@figat.pl>
2025-10-27 00:01:36 +0100

    Update all Linux deps with Clang 14 on Ubuntu 24

--- Source/Tools/Flax.Build/Deps/Dependencies/Assimp.cs
+++ Source/Tools/Flax.Build/Deps/Dependencies/Assimp.cs
@@ -127,11 +127,12 @@ namespace Flax.Deps.Dependencies
                         { "CC", "clang-" + Configuration.LinuxClangMinVer },
                         { "CC_FOR_BUILD", "clang-" + Configuration.LinuxClangMinVer },
                         { "CXX", "clang++-" + Configuration.LinuxClangMinVer },
+                        { "CMAKE_BUILD_PARALLEL_LEVEL", CmakeBuildParallel },
                     };
 
                     // Build for Linux
                     RunCmake(root, platform, TargetArchitecture.x64, " -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF " + globalConfig, envVars);
-                    Utilities.Run("make", null, null, root, Utilities.RunOptions.ThrowExceptionOnError, envVars);
+                    Utilities.Run("make", null, null, root, Utilities.RunOptions.DefaultTool, envVars);
                     configHeaderFilePath = Path.Combine(root, "include", "assimp", "config.h");
                     var depsFolder = GetThirdPartyFolder(options, platform, TargetArchitecture.x64);
                     Utilities.FileCopy(Path.Combine(root, "lib", "libassimp.a"), Path.Combine(depsFolder, "libassimp.a"));
@@ -143,11 +144,11 @@ namespace Flax.Deps.Dependencies
                     foreach (var architecture in new[] { TargetArchitecture.x64, TargetArchitecture.ARM64 })
                     {
                         RunCmake(root, platform, architecture, " -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF " + globalConfig);
-                        Utilities.Run("make", null, null, root, Utilities.RunOptions.ThrowExceptionOnError);
+                        Utilities.Run("make", null, null, root, Utilities.RunOptions.DefaultTool);
                         configHeaderFilePath = Path.Combine(root, "include", "assimp", "config.h");
                         var depsFolder = GetThirdPartyFolder(options, platform, architecture);
                         Utilities.FileCopy(Path.Combine(root, "lib", "libassimp.a"), Path.Combine(depsFolder, "libassimp.a"));
-                        Utilities.Run("make", "clean", null, root, Utilities.RunOptions.ThrowExceptionOnError);
+                        Utilities.Run("make", "clean", null, root, Utilities.RunOptions.DefaultTool);
                     }
                     break;
                 }
