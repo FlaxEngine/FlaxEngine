b27e55636302ad5eaf1c4cad1027b7ced01c0fa5
Saas <kaktusreiniger@gmail.com>
2026-02-02 21:35:40 +0100

    expose material instance

--- Source/Engine/UI/GUI/Special/RadialMenu.cs
+++ Source/Engine/UI/GUI/Special/RadialMenu.cs
@@ -130,6 +130,12 @@ namespace FlaxEngine.GUI
         }
 
         /// <summary>
+        /// The material instance of <paramref name="Material"/> used to draw the menu.
+        /// </summary>
+        [HideInEditor]
+        public MaterialInstance MaterialInstance => _materialInstance;
+
+        /// <summary>
         /// The selected callback.
         /// </summary>
         [HideInEditor]
