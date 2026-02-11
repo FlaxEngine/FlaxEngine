ebd20dd816f6e0ddba28e3709d3e79e2ac3b589b
Wojtek Figat <wojtek@figat.pl>
2026-02-11 18:05:56 +0100

    Fix Android compilation since `.c` files are now compiled

--- Source/Engine/Main/Android/main.cpp
+++ Source/Engine/Main/Android/main.cpp
@@ -5,7 +5,6 @@
 #include "Engine/Engine/Engine.h"
 #include "Engine/Platform/Platform.h"
 #include "android_native_app_glue.h"
-#include "android_native_app_glue.c"
 
 void android_main(android_app* app)
 {
