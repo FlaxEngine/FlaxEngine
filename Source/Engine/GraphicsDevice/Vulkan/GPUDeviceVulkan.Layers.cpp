FlaxEngine/FlaxEngine

commit f55ac81fe4f9ed74daa5c5618400225bd898ab02

Update to the latest Vulkan SDK `1.4.350`
Update Vulkan API to `1.2` on Windows
Update Vulkan API to `1.1` on Android

Source/Engine/GraphicsDevice/Vulkan/GPUDeviceVulkan.Layers.cpp
@@ -434,7 +434,7 @@ void GPUDeviceVulkan::GetInstanceLayersAndExtensions(Array<const char*>& outInst
     }
 }
 
-void GPUDeviceVulkan::GetDeviceExtensionsAndLayers(VkPhysicalDevice gpu, Array<const char*>& outDeviceExtensions, Array<const char*>& outDeviceLayers)
+void GPUDeviceVulkan::GetDeviceExtensions(VkPhysicalDevice gpu, Array<const char*>& outDeviceExtensions)
 {
     Array<LayerExtension> deviceLayerExtensions;
     deviceLayerExtensions.AddDefault(1);
@@ -495,70 +495,19 @@ void GPUDeviceVulkan::GetDeviceExtensionsAndLayers(VkPhysicalDevice gpu, Array<c
     {
         IsDebugToolAttached = true;
     }
-#if VULKAN_USE_DEBUG_LAYER
-    bool hasKhronosStandardValidationLayer = false, hasLunargStandardValidationLayer = false;
-#if VULKAN_USE_KHRONOS_STANDARD_VALIDATION
-    const char* vkLayerKhronosValidation = "VK_LAYER_KHRONOS_validation";
-    hasKhronosStandardValidationLayer = ContainsLayer(deviceLayerExtensions, vkLayerKhronosValidation);
-    if (hasKhronosStandardValidationLayer && _debugLayer)
-    {
-        outDeviceLayers.Add(vkLayerKhronosValidation);
-    }
-#endif
-#if VULKAN_USE_LUNARG_STANDARD_VALIDATION
-    if (!hasKhronosStandardValidationLayer && _debugLayer)
-    {
-        const char* vkLayerLunargStandardValidation = "VK_LAYER_LUNARG_standard_validation";
-        hasLunargStandardValidationLayer = ContainsLayer(deviceLayerExtensions, vkLayerLunargStandardValidation);
-        if (hasLunargStandardValidationLayer)
-        {
-            outDeviceLayers.Add(vkLayerLunargStandardValidation);
-        }
-    }
-#endif
-    if (!hasKhronosStandardValidationLayer && !hasLunargStandardValidationLayer && _debugLayer)
-    {
-        for (uint32 i = 0; GValidationLayers[i] != nullptr; i++)
-        {
-            const char* validationLayer = GValidationLayers[i];
-            if (ContainsLayer(deviceLayerExtensions, validationLayer))
-            {
-                outDeviceLayers.Add(validationLayer);
-            }
-        }
-    }
-#endif
 
     // Find all extensions
     Array<const char*> availableExtensions;
+    availableExtensions.Resize(deviceLayerExtensions[0].Extensions.Count());
+    for (int32 i = 0; i < deviceLayerExtensions[0].Extensions.Count(); i++)
     {
-        for (int32 i = 0; i < deviceLayerExtensions[0].Extensions.Count(); i++)
-        {
-            availableExtensions.Add(deviceLayerExtensions[0].Extensions[i].extensionName);
-        }
-
-        for (int32 layerIndex = 0; layerIndex < outDeviceLayers.Count(); layerIndex++)
-        {
-            int32 findLayerIndex;
-            for (findLayerIndex = 1; findLayerIndex < deviceLayerExtensions.Count(); findLayerIndex++)
-            {
-                if (!StringUtils::Compare(deviceLayerExtensions[findLayerIndex].Layer.layerName, outDeviceLayers[layerIndex]))
-                {
-                    break;
-                }
-            }
-
-            if (findLayerIndex < deviceLayerExtensions.Count())
-            {
-                deviceLayerExtensions[findLayerIndex].GetExtensions(availableExtensions);
-            }
-        }
+        availableExtensions[i] = deviceLayerExtensions[0].Extensions[i].extensionName;
     }
     TrimDuplicates(availableExtensions);
 
     // Pick extensions to use
     Array<const char*> platformExtensions;
-    VulkanPlatform::GetDeviceExtensions(platformExtensions, outDeviceLayers);
+    VulkanPlatform::GetDeviceExtensions(platformExtensions);
     for (const char* extension : platformExtensions)
     {
         if (ListContains(availableExtensions, extension))
@@ -581,15 +530,6 @@ void GPUDeviceVulkan::GetDeviceExtensionsAndLayers(VkPhysicalDevice gpu, Array<c
         for (const char* extension : outDeviceExtensions)
         {
             LOG(Info, "- {0}", String(extension));
-        }
-    }
-
-    if (outDeviceLayers.HasItems())
-    {
-        LOG(Info, "Using device layers:");
-        for (const char* layer : outDeviceLayers)
-        {
-            LOG(Info, "- {0}", String(layer));
         }
     }
 }

.github/actions/vulkan/action.yml
@@ -3,7 +3,7 @@ description: Downloads and installs Vulkan SDK.
 inputs:
   vulkan-version:
     description: 'Vulkan SDK release version (e.g. 1.2.198.1).'
-    default: '1.3.290.0'
+    default: '1.4.350.0'
     required: false
 runs:
   using: "composite"

Source/Editor/Cooker/Steps/CookAssetsStep.cpp
@@ -433,6 +433,7 @@ bool ProcessShaderBase(CookAssetsStep::AssetCookData& data, ShaderAssetBase* ass
     options.GenerateDebugData = data.Cache.Settings.Global.ShadersGenerateDebugData;
     options.TreatWarningsAsErrors = false;
     options.Output = &cacheStream;
+    options.Platform = data.Data.Tools->GetPlatform();
     Array<String> includes;
 
 #define COMPILE_PROFILE(profile, cacheChunk) \
@@ -535,7 +536,6 @@ bool ProcessShaderBase(CookAssetsStep::AssetCookData& data, ShaderAssetBase* ass
 #if PLATFORM_TOOLS_XBOX_SCARLETT
     case BuildPlatform::XboxScarlett:
     {
-        options.Platform = PlatformType::XboxScarlett;
         const char* platformDefineName = "PLATFORM_XBOX_SCARLETT";
         COMPILE_PROFILE(DirectX_SM6, SHADER_FILE_CHUNK_INTERNAL_D3D_SM6_CACHE);
         break;

Source/Engine/Graphics/Shaders/Cache/ShaderAssetBase.cpp
@@ -264,10 +264,13 @@ bool ShaderAssetBase::LoadShaderCache(ShaderCacheResult& result)
         auto& platformDefine = options.Macros.AddOne();
 #if PLATFORM_WINDOWS
         platformDefine.Name = "PLATFORM_WINDOWS";
+        options.Platform = PlatformType::Windows;
 #elif PLATFORM_LINUX
         platformDefine.Name = "PLATFORM_LINUX";
+        options.Platform = PlatformType::Linux;
 #elif PLATFORM_MAC
         platformDefine.Name = "PLATFORM_MAC";
+        options.Platform = PlatformType::Mac;
 #else
 #error "Unknown platform."
 #endif

Source/Engine/Graphics/Shaders/Cache/ShaderCacheManager.cpp
@@ -188,6 +188,7 @@ bool ShaderCacheManagerService::Init()
         int32 ShaderCacheVersion = -1;
         int32 MaterialGraphVersion = -1;
         int32 ParticleGraphVersion = -1;
+        int32 Platform = -1;
         union
         {
             struct
@@ -210,6 +211,7 @@ bool ShaderCacheManagerService::Init()
             ShaderCacheVersion = GPU_SHADER_CACHE_VERSION;
             MaterialGraphVersion = MATERIAL_GRAPH_VERSION;
             ParticleGraphVersion = PARTICLE_GPU_GRAPH_VERSION;
+            Platform = (int32)PLATFORM_TYPE;
             Flags = 0;
 #if USE_EDITOR
             ShaderDebug = CommandLine::Options.ShaderDebug.IsTrue();
@@ -235,6 +237,7 @@ bool ShaderCacheManagerService::Init()
         || cacheVersion.ShaderCacheVersion != cacheVersionDefault.ShaderCacheVersion
         || cacheVersion.MaterialGraphVersion != cacheVersionDefault.MaterialGraphVersion
         || cacheVersion.ParticleGraphVersion != cacheVersionDefault.ParticleGraphVersion
+        || cacheVersion.Platform != cacheVersionDefault.Platform
         || cacheVersion.Flags != cacheVersionDefault.Flags
     )
     {

Source/Engine/GraphicsDevice/Vulkan/Android/AndroidVulkanPlatform.cpp
@@ -12,7 +12,7 @@ void AndroidVulkanPlatform::GetInstanceExtensions(Array<const char*>& extensions
 	extensions.Add(VK_KHR_ANDROID_SURFACE_EXTENSION_NAME);
 }
 
-void AndroidVulkanPlatform::GetDeviceExtensions(Array<const char*>& extensions, Array<const char*>& layers)
+void AndroidVulkanPlatform::GetDeviceExtensions(Array<const char*>& extensions)
 {
 	extensions.Add(VK_KHR_SURFACE_EXTENSION_NAME);
 	extensions.Add(VK_KHR_ANDROID_SURFACE_EXTENSION_NAME);

Source/Engine/GraphicsDevice/Vulkan/Android/AndroidVulkanPlatform.h
@@ -6,6 +6,9 @@
 
 #if GRAPHICS_API_VULKAN && PLATFORM_ANDROID
 
+// Android has 99.4% of Vulkan 1.1 and API level 29 (Android 10.0)
+#define VULKAN_API_VERSION VK_API_VERSION_1_1
+
 // Support more backbuffers in case driver decides to use more
 #define VULKAN_BACK_BUFFERS_COUNT_MAX 8
 
@@ -16,7 +19,7 @@ class AndroidVulkanPlatform : public VulkanPlatformBase
 {
 public:
 	static void GetInstanceExtensions(Array<const char*>& extensions, Array<const char*>& layers);
-	static void GetDeviceExtensions(Array<const char*>& extensions, Array<const char*>& layers);
+	static void GetDeviceExtensions(Array<const char*>& extensions);
 	static void CreateSurface(Window* window, GPUDeviceVulkan* device, VkInstance instance, VkSurfaceKHR* surface);
 };
 

Source/Engine/GraphicsDevice/Vulkan/GPUContextVulkan.cpp
@@ -210,7 +210,11 @@ void GPUContextVulkan::AddImageBarrier(GPUTextureViewVulkan* handle, VkImageLayo
                 range.baseMipLevel = 0;
                 range.levelCount = mipLevels;
                 range.baseArrayLayer = 0;
+#if VK_KHR_maintenance1
+                range.layerCount = VK_REMAINING_ARRAY_LAYERS; // maintenance9 could be enabled for per-layer masking
+#else
                 range.layerCount = handle->Owner->ArraySlices;
+#endif
                 AddImageBarrier(handle->Image, srcLayout, dstLayout, range, handle);
                 state.SetResourceState(dstLayout);
             }
@@ -229,6 +233,10 @@ void GPUContextVulkan::AddImageBarrier(GPUTextureViewVulkan* handle, VkImageLayo
                     range.levelCount = 1;
                     range.baseArrayLayer = i / mipLevels;
                     range.layerCount = 1;
+#if VK_KHR_maintenance1
+                    if (handle->Owner->ArraySlices == 1)
+                        range.layerCount = VK_REMAINING_ARRAY_LAYERS; // maintenance9 could be enabled for per-layer masking
+#endif
                     AddImageBarrier(handle->Image, srcLayout, dstLayout, range, handle);
                     state.SetSubresourceState(i, dstLayout);
                 }
@@ -264,6 +272,10 @@ void GPUContextVulkan::AddImageBarrier(GPUTextureVulkan* texture, int32 mipSlice
     range.levelCount = 1;
     range.baseArrayLayer = arraySlice;
     range.layerCount = 1;
+#if VK_KHR_maintenance1
+    if (texture->IsVolume())
+        range.layerCount = VK_REMAINING_ARRAY_LAYERS; // maintenance9 could be enabled for per-layer masking
+#endif
     AddImageBarrier(texture->GetHandle(), srcLayout, dstLayout, range, nullptr);
     state.SetSubresourceState(subresourceIndex, dstLayout);
 }
@@ -285,6 +297,10 @@ void GPUContextVulkan::AddImageBarrier(GPUTextureVulkan* texture, VkImageLayout 
         range.levelCount = texture->MipLevels();
         range.baseArrayLayer = 0;
         range.layerCount = texture->ArraySize();
+#if VK_KHR_maintenance1
+        if (texture->IsVolume())
+            range.layerCount = VK_REMAINING_ARRAY_LAYERS; // maintenance9 could be enabled for per-layer masking
+#endif
         AddImageBarrier(texture->GetHandle(), srcLayout, dstLayout, range, nullptr);
         state.SetResourceState(dstLayout);
     }
@@ -431,7 +447,7 @@ void GPUContextVulkan::BeginRenderPass()
     PendingClear clear;
     for (int32 i = 0; i < GPU_MAX_RT_BINDED; i++)
     {
-        auto handle = _rtHandles[i];
+        auto handle = _rtTargets[i];
         if (handle)
         {
             layout.RTVsFormats[i] = handle->GetFormat();
@@ -490,7 +506,7 @@ void GPUContextVulkan::BeginRenderPass()
     }
     else
     {
-        handle = _rtHandles[0];
+        handle = _rtTargets[0];
 #if !BUILD_RELEASE
         if (!handle)
         {
@@ -507,9 +523,8 @@ void GPUContextVulkan::BeginRenderPass()
     layout.Layers = handle->Layers;
 
     // Clear textures that are not bind to the render pass
-    for (auto& e : _pendingClears)
-        ManualClear(e);
-    _pendingClears.Clear();
+    if (_pendingClears.HasItems())
+        FlushManualClears();
 
     // Get or create objects
     auto renderPass = _device->GetOrCreateRenderPass(layout);
@@ -567,6 +582,17 @@ void GPUContextVulkan::ManualClear(const PendingClear& clear)
     {
         vkCmdClearColorImage(cmdBuffer->GetHandle(), clear.View->Image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, &clear.Value.color, 1, &clear.View->Info.subresourceRange);
     }
+}
+
+void GPUContextVulkan::FlushManualClears()
+{
+    // Batch barriers
+    for (auto& clear : _pendingClears)
+        AddImageBarrier(clear.View, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+
+    for (auto& clear : _pendingClears)
+        ManualClear(clear);
+    _pendingClears.Clear();
 }
 
 void GPUContextVulkan::UpdateDescriptorSets(const SpirvShaderDescriptorInfo& descriptorInfo, DescriptorSetWriterVulkan& dsWriter, bool& needsWrite)
@@ -736,6 +762,27 @@ void GPUContextVulkan::OnDrawCall()
     ASSERT(pipelineState && pipelineState->IsValid());
     const auto cmdBuffer = _cmdBufferManager->GetCmdBuffer();
 
+    // Flush pending image clears before binding descriptors but skip ones that are going to be used as render targets
+    // (they will be cleared in the render pass with proper loadOp and without extra barriers)
+    if (_pendingClears.HasItems())
+    {
+        auto rtHandles = ToSpan(_rtHandles, ARRAY_COUNT(_rtHandles));
+        for (auto& clear : _pendingClears)
+        {
+            if (!SpanContains(rtHandles, clear.View))
+                AddImageBarrier(clear.View, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+        }
+        for (int32 i = 0; i < _pendingClears.Count(); i++)
+        {
+            auto& clear = _pendingClears.Get()[i];
+            if (!SpanContains(rtHandles, clear.View))
+            {
+                ManualClear(clear);
+                _pendingClears.RemoveAtKeepOrder(i--);
+            }
+        }
+    }
+
     // End previous render pass if render targets layout was modified
     if (_rtDirtyFlag && cmdBuffer->IsInsideRenderPass())
         EndRenderPass();
@@ -837,7 +884,6 @@ void GPUContextVulkan::FrameBegin()
     _currentState = nullptr;
     _currentCompute = nullptr;
     _vertexLayout = nullptr;
-    _rtDepth = nullptr;
     Platform::MemoryClear(_rtHandles, sizeof(_rtHandles));
     Platform::MemoryClear(_cbHandles, sizeof(_cbHandles));
     Platform::MemoryClear(_srHandles, sizeof(_srHandles));
@@ -918,6 +964,17 @@ bool GPUContextVulkan::IsDepthBufferBinded()
 
 void GPUContextVulkan::Clear(GPUTextureView* rt, const Color& color)
 {
+    for (auto& e : _pendingClears)
+    {
+        if (e.View == rt)
+        {
+            // Use existing slot
+            Platform::MemoryCopy(e.Value.color.float32, color.Raw, sizeof(color.Raw));
+            return;
+        }
+    }
+
+    // Add a new entry
     auto& clear = _pendingClears.AddOne();
     clear.View = (GPUTextureViewVulkan*)rt;
     Platform::MemoryCopy(clear.Value.color.float32, color.Raw, sizeof(color.Raw));
@@ -925,6 +982,18 @@ void GPUContextVulkan::Clear(GPUTextureView* rt, const Color& color)
 
 void GPUContextVulkan::ClearDepth(GPUTextureView* depthBuffer, float depthValue, uint8 stencilValue)
 {
+    for (auto& e : _pendingClears)
+    {
+        if (e.View == depthBuffer)
+        {
+            // Use existing slot
+            e.Value.depthStencil.depth = depthValue;
+            e.Value.depthStencil.stencil = stencilValue;
+            return;
+        }
+    }
+
+    // Add a new entry
     auto& clear = _pendingClears.AddOne();
     clear.View = (GPUTextureViewVulkan*)depthBuffer;
     clear.Value.depthStencil.depth = depthValue;
@@ -1001,7 +1070,6 @@ void GPUContextVulkan::ResetRenderTarget()
         _rtDirtyFlag = true;
         _psDirtyFlag = true;
         _rtCount = 0;
-        _rtDepth = nullptr;
         Platform::MemoryClear(_rtHandles, sizeof(_rtHandles));
 
         const auto cmdBuffer = _cmdBufferManager->GetActiveCmdBuffer();
@@ -1014,13 +1082,13 @@ void GPUContextVulkan::SetRenderTarget(GPUTextureView* rt)
 {
     const auto rtVulkan = static_cast<GPUTextureViewVulkan*>(rt);
 
-    if (_rtDepth != nullptr || _rtCount != 1 || _rtHandles[0] != rtVulkan)
+    if (_rtDepth != nullptr || _rtCount != 1 || _rtTargets[0] != rtVulkan)
     {
         _rtDirtyFlag = true;
         _psDirtyFlag = true;
         _rtCount = 1;
         _rtDepth = nullptr;
-        _rtHandles[0] = rtVulkan;
+        _rtTargets[0] = rtVulkan;
     }
 }
 
@@ -1030,13 +1098,13 @@ void GPUContextVulkan::SetRenderTarget(GPUTextureView* depthBuffer, GPUTextureVi
     const auto depthBufferVulkan = static_cast<GPUTextureViewVulkan*>(depthBuffer);
     const auto rtCount = rtVulkan ? 1 : 0;
 
-    if (_rtDepth != depthBufferVulkan || _rtCount != rtCount || _rtHandles[0] != rtVulkan)
+    if (_rtDepth != depthBufferVulkan || _rtCount != rtCount || _rtTargets[0] != rtVulkan)
     {
         _rtDirtyFlag = true;
         _psDirtyFlag = true;
         _rtCount = rtCount;
         _rtDepth = depthBufferVulkan;
-        _rtHandles[0] = rtVulkan;
+        _rtTargets[0] = rtVulkan;
     }
 }
 
@@ -1053,13 +1121,13 @@ void GPUContextVulkan::SetRenderTarget(GPUTextureView* depthBuffer, const Span<G
     }
     const int32 rtvsSize = sizeof(GPUTextureViewVulkan*) * rts.Length();
 
-    if (_rtDepth != depthBufferVulkan || _rtCount != rts.Length() || Platform::MemoryCompare(_rtHandles, rtvs, rtvsSize) != 0)
+    if (_rtDepth != depthBufferVulkan || _rtCount != rts.Length() || Platform::MemoryCompare(_rtTargets, rtvs, rtvsSize) != 0)
     {
         _rtDirtyFlag = true;
         _psDirtyFlag = true;
         _rtCount = rts.Length();
         _rtDepth = depthBufferVulkan;
-        Platform::MemoryCopy(_rtHandles, rtvs, rtvsSize);
+        Platform::MemoryCopy(_rtTargets, rtvs, rtvsSize);
     }
 }
 
@@ -1486,11 +1554,7 @@ void GPUContextVulkan::FlushState()
     if (cmdBuffer->IsInsideRenderPass())
         EndRenderPass();
 
-    // Flush pending clears
-    for (auto& clear : _pendingClears)
-        ManualClear(clear);
-    _pendingClears.Clear();
-
+    FlushManualClears();
     FlushBarriers();
 }
 
@@ -2012,7 +2076,7 @@ void GPUContextVulkan::BeginDrawPass(GPUDrawPass& pass)
     _drawPassCanClear = true;
     _rtCount = pass.RenderTargetsCount;
     _rtDepth = (GPUTextureViewVulkan*)pass.DepthBuffer;
-    Platform::MemoryCopy(_rtHandles, pass.RenderTargets, pass.RenderTargetsCount * sizeof(void*));
+    Platform::MemoryCopy(_rtTargets, pass.RenderTargets, pass.RenderTargetsCount * sizeof(void*));
 }
 
 void GPUContextVulkan::EndDrawPass()

commit 4b6c22c43f6ca21e17097b74003cbc0ecca6a9bb

Fix game build regression from f55ac81fe4f9ed74daa5c5618400225bd898ab02

Source/Engine/GraphicsDevice/Vulkan/GPUDeviceVulkan.Layers.cpp
@@ -280,12 +280,12 @@ void GPUDeviceVulkan::GetInstanceLayersAndExtensions(Array<const char*>& outInst
         }
     }
 
+    bool vkTrace = false;
 #if VULKAN_USE_DEBUG_LAYER
     if (!useDebugLayer)
         ValidationLevel = VulkanValidationLevel::Disabled;
     const bool useVkTrace = false;
 
-    bool vkTrace = false;
     if (useVkTrace && useDebugLayer)
     {
         const char* VkTraceName = "VK_LAYER_LUNARG_vktrace";