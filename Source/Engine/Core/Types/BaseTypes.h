FlaxEngine/FlaxEngine

commit bc361683180bae67b3d469b023dbd87fde3bb681

Optimize Animated Model rendering with hardware instancing
All models are using the same global buffer for skinned bones which allows to share shader binding for instancing.
Refactor draw call for batching skinned mesh draws.
Remove `SkinnedMeshDrawData` and merge it into `AnimatedModel` internals.

Source/Engine/Graphics/Materials/MaterialShader.h
@@ -10,7 +10,7 @@
 /// <summary>
 /// Current materials shader version.
 /// </summary>
-#define MATERIAL_GRAPH_VERSION 183
+#define MATERIAL_GRAPH_VERSION 184
 
 class Material;
 class GPUShader;

Source/Engine/Graphics/Models/Mesh.cpp
@@ -306,7 +306,7 @@ void Mesh::Draw(const RenderContext& renderContext, const DrawInfo& info, float 
     drawCall.Surface.PrevWorld = info.DrawState->PrevWorld;
     drawCall.Surface.Lightmap = (info.Flags & StaticFlags::Lightmap) != StaticFlags::None ? info.Lightmap : nullptr;
     drawCall.Surface.LightmapUVsArea = info.LightmapUVs ? *info.LightmapUVs : Half4::Zero;
-    drawCall.Surface.LODDitherFactor = lodDitherFactor;
+    drawCall.Surface.LODDitherFactor = (byte)(lodDitherFactor * 255);
     drawCall.PerInstanceRandom = info.PerInstanceRandom;
     drawCall.StencilValue = info.StencilValue;
 #if USE_EDITOR
@@ -314,7 +314,7 @@ void Mesh::Draw(const RenderContext& renderContext, const DrawInfo& info, float 
     if (viewMode == ViewMode::LightmapUVsDensity || viewMode == ViewMode::LODPreview)
         GBufferPass::AddIndexBufferToModelLOD(_indexBuffer, &((Model*)_model)->LODs[_lodIndex]);
     if (viewMode == ViewMode::LightmapUVsDensity)
-        drawCall.Surface.LODDitherFactor = info.LightmapScale; // See LightmapUVsDensityMaterialShader
+        drawCall.Surface.SkinningBonesOffset = *(uint32*)&info.LightmapScale; // See LightmapUVsDensityMaterialShader
 #endif
 
     // Push draw call to the render list
@@ -369,7 +369,7 @@ void Mesh::Draw(const RenderContextBatch& renderContextBatch, const DrawInfo& in
     drawCall.Surface.PrevWorld = info.DrawState->PrevWorld;
     drawCall.Surface.Lightmap = (info.Flags & StaticFlags::Lightmap) != StaticFlags::None ? info.Lightmap : nullptr;
     drawCall.Surface.LightmapUVsArea = info.LightmapUVs ? *info.LightmapUVs : Half4::Zero;
-    drawCall.Surface.LODDitherFactor = lodDitherFactor;
+    drawCall.Surface.LODDitherFactor = (byte)(lodDitherFactor * 255);
     drawCall.PerInstanceRandom = info.PerInstanceRandom;
     drawCall.StencilValue = info.StencilValue;
 #if USE_EDITOR
@@ -377,7 +377,7 @@ void Mesh::Draw(const RenderContextBatch& renderContextBatch, const DrawInfo& in
     if (viewMode == ViewMode::LightmapUVsDensity || viewMode == ViewMode::LODPreview)
         GBufferPass::AddIndexBufferToModelLOD(_indexBuffer, &((Model*)_model)->LODs[_lodIndex]);
     if (viewMode == ViewMode::LightmapUVsDensity)
-        drawCall.Surface.LODDitherFactor = info.LightmapScale; // See LightmapUVsDensityMaterialShader
+        drawCall.Surface.SkinningBonesOffset = *(uint32*)&info.LightmapScale; // See LightmapUVsDensityMaterialShader
 #endif
 
     // Push draw call to the render lists

Source/Engine/Graphics/Models/SkinnedMesh.cpp
@@ -334,6 +334,7 @@ void SkinnedMesh::Draw(const RenderContext& renderContext, const DrawInfo& info,
         return;
 
     // Setup draw call
+    ASSERT(info.SkinningBones);
     DrawCall drawCall;
     drawCall.Geometry.IndexBuffer = _indexBuffer;
     drawCall.Geometry.VertexBuffers[0] = _vertexBuffers[0];
@@ -348,8 +349,11 @@ void SkinnedMesh::Draw(const RenderContext& renderContext, const DrawInfo& info,
     drawCall.ObjectRadius = (float)info.Bounds.Radius; // TODO: should it be kept in sync with ObjectPosition?
     drawCall.Surface.GeometrySize = _box.GetSize();
     drawCall.Surface.PrevWorld = info.DrawState->PrevWorld;
-    drawCall.Surface.Skinning = info.Skinning;
-    drawCall.Surface.LODDitherFactor = lodDitherFactor;
+    drawCall.Surface.Skinning = info.WithPrevBones ? DrawCall::SkinningMode::WithPrevBones : DrawCall::SkinningMode::Active;
+    drawCall.Surface.SkinningBones = info.SkinningBones;
+    drawCall.Surface.SkinningBonesOffset = info.SkinningBonesOffset;
+    drawCall.Surface.PrevBonesOffset = info.PrevBonesOffset;
+    drawCall.Surface.LODDitherFactor = (byte)(lodDitherFactor * 255);
     drawCall.PerInstanceRandom = info.PerInstanceRandom;
     drawCall.StencilValue = info.StencilValue;
 
@@ -376,6 +380,7 @@ void SkinnedMesh::Draw(const RenderContextBatch& renderContextBatch, const DrawI
         return;
 
     // Setup draw call
+    ASSERT(info.SkinningBones);
     DrawCall drawCall;
     drawCall.Geometry.IndexBuffer = _indexBuffer;
     drawCall.Geometry.VertexBuffers[0] = _vertexBuffers[0];
@@ -389,8 +394,11 @@ void SkinnedMesh::Draw(const RenderContextBatch& renderContextBatch, const DrawI
     drawCall.ObjectRadius = (float)info.Bounds.Radius; // TODO: should it be kept in sync with ObjectPosition?
     drawCall.Surface.GeometrySize = _box.GetSize();
     drawCall.Surface.PrevWorld = info.DrawState->PrevWorld;
-    drawCall.Surface.Skinning = info.Skinning;
-    drawCall.Surface.LODDitherFactor = lodDitherFactor;
+    drawCall.Surface.Skinning = info.WithPrevBones ? DrawCall::SkinningMode::WithPrevBones : DrawCall::SkinningMode::Active;
+    drawCall.Surface.SkinningBones = info.SkinningBones;
+    drawCall.Surface.SkinningBonesOffset = info.SkinningBonesOffset;
+    drawCall.Surface.PrevBonesOffset = info.PrevBonesOffset;
+    drawCall.Surface.LODDitherFactor = (byte)(lodDitherFactor * 255);
     drawCall.PerInstanceRandom = info.PerInstanceRandom;
     drawCall.StencilValue = info.StencilValue;
 

Source/Engine/Renderer/DrawCall.h
@@ -148,6 +148,13 @@ struct DrawCall
     /// </summary>
     int32 InstanceCount;
 
+    enum class SkinningMode
+    {
+        None = 0,
+        Active,
+        WithPrevBones,
+    };
+
     union
     {
         struct
@@ -190,9 +197,12 @@ struct DrawCall
         {
             const Lightmap* Lightmap;
             Half4 LightmapUVsArea;
-            SkinnedMeshDrawData* Skinning;
+            SkinningMode Skinning;
+            int16 PrevBonesOffset; // In Matrix3x4s, can be negative
+            byte LODDitherFactor; // The model LOD transition dither progress.
+            GPUBuffer* SkinningBones;
             Float3 GeometrySize; // Object geometry size in the world (unscaled).
-            float LODDitherFactor; // The model LOD transition dither progress.
+            uint32 SkinningBonesOffset; // In Matrix3x4s
             Matrix PrevWorld;
         } Surface;
 
@@ -276,7 +286,7 @@ struct DrawCall
     uint8 StencilValue;
 
     /// <summary>
-    /// The world matrix determinant sign (used for geometry that is two sided or has inverse scale - needs to flip normal vectors and change triangles culling).
+    /// The world matrix determinant sign (used for geometry that is two-sided or has inverse scale - needs to flip normal vectors and change triangles culling).
     /// 0 - sign is positive
     /// 1 - sign is negative (flips object surfaces)
     /// </summary>

Source/Engine/Renderer/RenderList.cpp
@@ -47,7 +47,7 @@ namespace
     }
 }
 
-void ShaderObjectData::Store(const Matrix& worldMatrix, const Matrix& prevWorldMatrix, const Half4& lightmapUVsArea, const Float3& geometrySize, float perInstanceRandom, float worldDeterminantSign, float lodDitherFactor)
+void ShaderObjectData::Store(const Matrix& worldMatrix, const Matrix& prevWorldMatrix, const Half4& lightmapUVsArea, const Float3& geometrySize, float perInstanceRandom, float worldDeterminantSign, byte lodDitherFactor, uint32 skinningOffset, int16 skinningPrevOffset)
 {
     Float2 lightmapUVsAreaPackedAliased = *(Float2*)&lightmapUVsArea;
     Raw[0] = Float4(worldMatrix.M11, worldMatrix.M12, worldMatrix.M13, worldMatrix.M41);
@@ -57,11 +57,15 @@ void ShaderObjectData::Store(const Matrix& worldMatrix, const Matrix& prevWorldM
     Raw[4] = Float4(prevWorldMatrix.M21, prevWorldMatrix.M22, prevWorldMatrix.M23, prevWorldMatrix.M42);
     Raw[5] = Float4(prevWorldMatrix.M31, prevWorldMatrix.M32, prevWorldMatrix.M33, prevWorldMatrix.M43);
     Raw[6] = Float4(geometrySize, perInstanceRandom);
-    Raw[7] = Float4(worldDeterminantSign, lodDitherFactor, lightmapUVsAreaPackedAliased.X, lightmapUVsAreaPackedAliased.Y);
-    // TODO: pack WorldDeterminantSign and LODDitherFactor
+    // 0-3 bits: LOD Dither Factor (0-1 range mapped to 0-255)
+    // 4 bit: World Determinant Sign (0 for normal or 1 for inversed)
+    // 5-15 bits: unused
+    // 16-31 bits: Offset in Skinning Bones buffer for previous frame bones (can be negative)
+    uint32 packed7x = (uint32)lodDitherFactor + (worldDeterminantSign < 0 ? 256 : 0) + ((skinningPrevOffset + 32760) << 16);
+    Raw[7] = Float4(*(float*)&packed7x, *(float*)&skinningOffset, lightmapUVsAreaPackedAliased.X, lightmapUVsAreaPackedAliased.Y);
 }
 
-void ShaderObjectData::Load(Matrix& worldMatrix, Matrix& prevWorldMatrix, Half4& lightmapUVsArea, Float3& geometrySize, float& perInstanceRandom, float& worldDeterminantSign, float& lodDitherFactor) const
+void ShaderObjectData::Load(Matrix& worldMatrix, Matrix& prevWorldMatrix, Half4& lightmapUVsArea, Float3& geometrySize, float& perInstanceRandom, float& worldDeterminantSign, byte& lodDitherFactor, uint32& skinningOffset, int16& skinningPrevOffset) const
 {
     worldMatrix.SetRow1(Float4(Float3(Raw[0]), 0.0f));
     worldMatrix.SetRow2(Float4(Float3(Raw[1]), 0.0f));
@@ -73,8 +77,11 @@ void ShaderObjectData::Load(Matrix& worldMatrix, Matrix& prevWorldMatrix, Half4&
     prevWorldMatrix.SetRow4(Float4(Raw[3].W, Raw[4].W, Raw[5].W, 1.0f));
     geometrySize = Float3(Raw[6]);
     perInstanceRandom = Raw[6].W;
-    worldDeterminantSign = Raw[7].X;
-    lodDitherFactor = Raw[7].Y;
+    uint32 packed7x = *(uint32*)&Raw[7].X;
+    lodDitherFactor = packed7x & 255;
+    worldDeterminantSign = (packed7x & 256) == 256 ? -1.0f : 1.0f;
+    skinningOffset = *(uint32*)&Raw[7].Y;
+    skinningPrevOffset = (packed7x >> 16) - 32760;
     Float2 lightmapUVsAreaPackedAliased(Raw[7].Z, Raw[7].W);
     lightmapUVsArea = *(Half4*)&lightmapUVsAreaPackedAliased;
 }
@@ -289,6 +296,11 @@ void RenderList::CleanupCache()
     // Don't call it during rendering (data may be already in use)
     ASSERT(GPUDevice::Instance == nullptr || GPUDevice::Instance->CurrentTask == nullptr);
 
+    // Free extensions
+    for (IExtension* e : GetExtensions())
+        e->Dispose();
+
+    // Free pooled memory
     MemPoolLocker.Lock();
     FreeRenderList.ClearDelete();
     for (auto& e : MemPool)
@@ -935,7 +947,7 @@ void RenderList::SortDrawCalls(const RenderContext& renderContext, bool reverseD
 
 FORCE_INLINE bool CanUseInstancing(DrawPass pass)
 {
-    return pass == DrawPass::GBuffer || pass == DrawPass::Depth;
+    return pass == DrawPass::GBuffer || pass == DrawPass::Depth || pass == DrawPass::MotionVectors;
 }
 
 FORCE_INLINE bool DrawsEqual(const DrawCall* a, const DrawCall* b)
@@ -1217,15 +1229,14 @@ void SurfaceDrawCallHandler::GetHash(const DrawCall& drawCall, uint32& batchKey)
 {
     if (drawCall.Surface.Lightmap)
         CombineHash(batchKey, 1313);
-    if (drawCall.Surface.Skinning)
-        CombineHash(batchKey, 11);
+    CombineHash(batchKey, (byte)drawCall.Surface.Skinning);
 }
 
 bool SurfaceDrawCallHandler::CanBatch(const DrawCall& a, const DrawCall& b, DrawPass pass)
 {
     // TODO: find reason why batching static meshes with lightmap causes problems with sampling in shader (flickering when meshes in batch order gets changes due to async draw calls collection)
     if (a.Surface.Lightmap == nullptr && b.Surface.Lightmap == nullptr &&
-        a.Surface.Skinning == nullptr && b.Surface.Skinning == nullptr)
+        a.Surface.Skinning == b.Surface.Skinning)
     {
         auto& materialInfo = a.Material->GetInfo();
         if (a.Material != b.Material)

Source/Engine/Renderer/RenderList.h
@@ -359,6 +359,8 @@ API_CLASS(Sealed) class FLAXENGINE_API RenderList : public ScriptingObject
         IExtension();
         virtual ~IExtension();
 
+        // Event called when GPU Device is shutting down and the extension is being disposed. Used to clean up GPU resources (before scripting might destroy extension).
+        virtual void Dispose() {}
         // Event called before collecting draw calls. Can be used for initialization.
         virtual void PreDraw(GPUContext* context, RenderContextBatch& renderContextBatch) {}
         // Event called after collecting draw calls. Can be used for cleanup or to perform additional drawing using collected draw calls data such as batched data processing.
@@ -690,18 +692,18 @@ GPU_CB_STRUCT(ShaderObjectData
     {
     Float4 Raw[8];
 
-    void FLAXENGINE_API Store(const Matrix& worldMatrix, const Matrix& prevWorldMatrix, const Half4& lightmapUVsAreaPacked, const Float3& geometrySize, float perInstanceRandom = 0.0f, float worldDeterminantSign = 1.0f, float lodDitherFactor = 0.0f);
-    void FLAXENGINE_API Load(Matrix& worldMatrix, Matrix& prevWorldMatrix, Half4& lightmapUVsArea, Float3& geometrySize, float& perInstanceRandom, float& worldDeterminantSign, float& lodDitherFactor) const;
+    void FLAXENGINE_API Store(const Matrix& worldMatrix, const Matrix& prevWorldMatrix, const Half4& lightmapUVsAreaPacked, const Float3& geometrySize, float perInstanceRandom = 0.0f, float worldDeterminantSign = 1.0f, byte lodDitherFactor = 0, uint32 skinningOffset = 0, int16 skinningPrevOffset = 0);
+    void FLAXENGINE_API Load(Matrix& worldMatrix, Matrix& prevWorldMatrix, Half4& lightmapUVsArea, Float3& geometrySize, float& perInstanceRandom, float& worldDeterminantSign, byte& lodDitherFactor, uint32& skinningOffset, int16& prevBonesOffset) const;
 
     FORCE_INLINE void Store(const DrawCall& drawCall)
     {
-    Store(drawCall.World, drawCall.Surface.PrevWorld, drawCall.Surface.LightmapUVsArea, drawCall.Surface.GeometrySize, drawCall.PerInstanceRandom, drawCall.WorldDeterminant ? -1.0f : 1.0f, drawCall.Surface.LODDitherFactor);
+    Store(drawCall.World, drawCall.Surface.PrevWorld, drawCall.Surface.LightmapUVsArea, drawCall.Surface.GeometrySize, drawCall.PerInstanceRandom, drawCall.WorldDeterminant ? -1.0f : 1.0f, drawCall.Surface.LODDitherFactor, drawCall.Surface.SkinningBonesOffset, drawCall.Surface.PrevBonesOffset);
     }
 
     FORCE_INLINE void Load(DrawCall& drawCall) const
     {
     float worldDeterminantSign;
-    Load(drawCall.World, drawCall.Surface.PrevWorld, drawCall.Surface.LightmapUVsArea, drawCall.Surface.GeometrySize, drawCall.PerInstanceRandom, worldDeterminantSign, drawCall.Surface.LODDitherFactor);
+    Load(drawCall.World, drawCall.Surface.PrevWorld, drawCall.Surface.LightmapUVsArea, drawCall.Surface.GeometrySize, drawCall.PerInstanceRandom, worldDeterminantSign, drawCall.Surface.LODDitherFactor, drawCall.Surface.SkinningBonesOffset, drawCall.Surface.PrevBonesOffset);
     drawCall.ObjectPosition = drawCall.World.GetTranslation();
     drawCall.WorldDeterminant = worldDeterminantSign < 0 ? 1 : 0;
     }

Source/Shaders/MaterialCommon.hlsl
@@ -81,9 +81,11 @@ struct ObjectData
     float4x4 PrevWorldMatrix;
     float3 GeometrySize;
     float WorldDeterminantSign;
+    float4 LightmapArea;
     float LODDitherFactor;
     float PerInstanceRandom;
-    float4 LightmapArea;
+    uint SkinningOffset;
+    int PrevBonesOffset;
 };
 
 float2 UnpackHalf2(uint xy)
@@ -115,8 +117,11 @@ ObjectData LoadObject(Buffer<float4> objectsBuffer, uint objectIndex)
     object.PrevWorldMatrix[3] = float4(vector3.w, vector4.w, vector5.w, 1.0f);
     object.GeometrySize = vector6.xyz;
     object.PerInstanceRandom = vector6.w;
-    object.WorldDeterminantSign = vector7.x;
-    object.LODDitherFactor = vector7.y;
+    uint packed7x = asuint(vector7.x);
+    object.WorldDeterminantSign = (packed7x & 256) == 256 ? -1.0f : 1.0f;
+    object.LODDitherFactor = (packed7x & 255) / 255.0f;
+    object.SkinningOffset = asuint(vector7.y);
+    object.PrevBonesOffset = (int)(packed7x >> 16) - 32760;
     object.LightmapArea.xy = UnpackHalf2(asuint(vector7.z));
     object.LightmapArea.zw = UnpackHalf2(asuint(vector7.w));
     return object;
@@ -247,6 +252,9 @@ struct ModelInput_Skinned
 #endif
     uint4 BlendIndices : BLENDINDICES;
     float4 BlendWeights : BLENDWEIGHTS;
+#if USE_INSTANCING
+    uint ObjectIndex : ATTRIBUTE0;
+#endif
 };
 
 struct Model_VS2PS

commit e8c63066e37ff9e820fcd865a3a7fc1fcb6e3c92

Fix LOD Dither Factor regression from bc361683180bae67b3d469b023dbd87fde3bb681 on negative values

Source/Engine/Core/Types/BaseTypes.h
@@ -8,13 +8,13 @@
 typedef unsigned char byte; // Unsigned 8 bits
 
 // Unsigned base types
-typedef unsigned char uint8; // 8-bit  unsigned
+typedef unsigned char uint8; // 8-bit unsigned
 typedef unsigned short uint16; // 16-bit unsigned
 typedef unsigned int uint32; // 32-bit unsigned
 typedef unsigned long long uint64; // 64-bit unsigned
 
 // Signed base types
-typedef signed char int8; // 8-bit  signed
+typedef signed char int8; // 8-bit signed
 typedef signed short int16; // 16-bit signed
 typedef signed int int32; // 32-bit signed
 typedef long long int64; // 64-bit signed

Source/Engine/Graphics/Materials/MaterialShader.h
@@ -10,7 +10,7 @@
 /// <summary>
 /// Current materials shader version.
 /// </summary>
-#define MATERIAL_GRAPH_VERSION 185
+#define MATERIAL_GRAPH_VERSION 186
 
 class Material;
 class GPUShader;

Source/Engine/Graphics/Models/Mesh.cpp
@@ -305,7 +305,7 @@ void Mesh::Draw(const RenderContext& renderContext, const DrawInfo& info, float 
     drawCall.Surface.PrevWorld = info.DrawState->PrevWorld;
     drawCall.Surface.Lightmap = (info.Flags & StaticFlags::Lightmap) != StaticFlags::None ? info.Lightmap : nullptr;
     drawCall.Surface.LightmapUVsArea = info.LightmapUVs ? *info.LightmapUVs : Half4::Zero;
-    drawCall.Surface.LODDitherFactor = (byte)(lodDitherFactor * 255);
+    drawCall.Surface.LODDitherFactor = (int8)(lodDitherFactor * 127);
     drawCall.PerInstanceRandom = info.PerInstanceRandom;
     drawCall.StencilValue = info.StencilValue;
 #if GPU_ENABLE_DEVELOPMENT
@@ -368,7 +368,7 @@ void Mesh::Draw(const RenderContextBatch& renderContextBatch, const DrawInfo& in
     drawCall.Surface.PrevWorld = info.DrawState->PrevWorld;
     drawCall.Surface.Lightmap = (info.Flags & StaticFlags::Lightmap) != StaticFlags::None ? info.Lightmap : nullptr;
     drawCall.Surface.LightmapUVsArea = info.LightmapUVs ? *info.LightmapUVs : Half4::Zero;
-    drawCall.Surface.LODDitherFactor = (byte)(lodDitherFactor * 255);
+    drawCall.Surface.LODDitherFactor = (int8)(lodDitherFactor * 127);
     drawCall.PerInstanceRandom = info.PerInstanceRandom;
     drawCall.StencilValue = info.StencilValue;
 #if GPU_ENABLE_DEVELOPMENT

Source/Engine/Graphics/Models/SkinnedMesh.cpp
@@ -382,7 +382,7 @@ void SkinnedMesh::Draw(const RenderContext& renderContext, const DrawInfo& info,
     drawCall.Surface.SkinningBones = info.SkinningBones;
     drawCall.Surface.SkinningBonesOffset = info.SkinningBonesOffset;
     drawCall.Surface.PrevBonesOffset = info.PrevBonesOffset;
-    drawCall.Surface.LODDitherFactor = (byte)(lodDitherFactor * 255);
+    drawCall.Surface.LODDitherFactor = (int8)(lodDitherFactor * 127);
     drawCall.PerInstanceRandom = info.PerInstanceRandom;
     drawCall.StencilValue = info.StencilValue;
 
@@ -427,7 +427,7 @@ void SkinnedMesh::Draw(const RenderContextBatch& renderContextBatch, const DrawI
     drawCall.Surface.SkinningBones = info.SkinningBones;
     drawCall.Surface.SkinningBonesOffset = info.SkinningBonesOffset;
     drawCall.Surface.PrevBonesOffset = info.PrevBonesOffset;
-    drawCall.Surface.LODDitherFactor = (byte)(lodDitherFactor * 255);
+    drawCall.Surface.LODDitherFactor = (int8)(lodDitherFactor * 127);
     drawCall.PerInstanceRandom = info.PerInstanceRandom;
     drawCall.StencilValue = info.StencilValue;
 

Source/Engine/Renderer/DrawCall.h
@@ -199,7 +199,7 @@ struct DrawCall
             Half4 LightmapUVsArea;
             SkinningMode Skinning;
             int16 PrevBonesOffset; // In Matrix3x4s, can be negative
-            byte LODDitherFactor; // The model LOD transition dither progress.
+            int8 LODDitherFactor; // The model LOD transition dither progress. [-127; 127] that maps into [-1;1] inside shader
             GPUBuffer* SkinningBones;
             Float3 GeometrySize; // Object geometry size in the world (unscaled).
             uint32 SkinningBonesOffset; // In Matrix3x4s

Source/Engine/Renderer/RenderList.cpp
@@ -47,7 +47,7 @@ namespace
     }
 }
 
-void ShaderObjectData::Store(const Matrix& worldMatrix, const Matrix& prevWorldMatrix, const Half4& lightmapUVsArea, const Float3& geometrySize, float perInstanceRandom, float worldDeterminantSign, byte lodDitherFactor, uint32 skinningOffset, int16 skinningPrevOffset)
+void ShaderObjectData::Store(const Matrix& worldMatrix, const Matrix& prevWorldMatrix, const Half4& lightmapUVsArea, const Float3& geometrySize, float perInstanceRandom, float worldDeterminantSign, int8 lodDitherFactor, uint32 skinningOffset, int16 skinningPrevOffset)
 {
     Float2 lightmapUVsAreaPackedAliased = *(Float2*)&lightmapUVsArea;
     Raw[0] = Float4(worldMatrix.M11, worldMatrix.M12, worldMatrix.M13, worldMatrix.M41);
@@ -57,15 +57,15 @@ void ShaderObjectData::Store(const Matrix& worldMatrix, const Matrix& prevWorldM
     Raw[4] = Float4(prevWorldMatrix.M21, prevWorldMatrix.M22, prevWorldMatrix.M23, prevWorldMatrix.M42);
     Raw[5] = Float4(prevWorldMatrix.M31, prevWorldMatrix.M32, prevWorldMatrix.M33, prevWorldMatrix.M43);
     Raw[6] = Float4(geometrySize, perInstanceRandom);
-    // 0-3 bits: LOD Dither Factor (0-1 range mapped to 0-255)
-    // 4 bit: World Determinant Sign (0 for normal or 1 for inversed)
-    // 5-15 bits: unused
+    // 0-7 bits: LOD Dither Factor (0-1 range mapped to 0-255) with separate sign at 7th bit
+    // 8 bit: World Determinant Sign (0 for normal or 1 for inversed)
+    // 9-15 bits: unused
     // 16-31 bits: Offset in Skinning Bones buffer for previous frame bones (can be negative)
-    uint32 packed7x = (uint32)lodDitherFactor + (worldDeterminantSign < 0 ? 256 : 0) + ((skinningPrevOffset + 32760) << 16);
+    uint32 packed7x = (uint32)Math::Abs(lodDitherFactor) + (lodDitherFactor < 0 ? 128 : 0) + (worldDeterminantSign < 0 ? 256 : 0) + ((skinningPrevOffset + 32760) << 16);
     Raw[7] = Float4(*(float*)&packed7x, *(float*)&skinningOffset, lightmapUVsAreaPackedAliased.X, lightmapUVsAreaPackedAliased.Y);
 }
 
-void ShaderObjectData::Load(Matrix& worldMatrix, Matrix& prevWorldMatrix, Half4& lightmapUVsArea, Float3& geometrySize, float& perInstanceRandom, float& worldDeterminantSign, byte& lodDitherFactor, uint32& skinningOffset, int16& skinningPrevOffset) const
+void ShaderObjectData::Load(Matrix& worldMatrix, Matrix& prevWorldMatrix, Half4& lightmapUVsArea, Float3& geometrySize, float& perInstanceRandom, float& worldDeterminantSign, int8& lodDitherFactor, uint32& skinningOffset, int16& skinningPrevOffset) const
 {
     worldMatrix.SetRow1(Float4(Float3(Raw[0]), 0.0f));
     worldMatrix.SetRow2(Float4(Float3(Raw[1]), 0.0f));

Source/Engine/Renderer/RenderList.h
@@ -693,8 +693,8 @@ GPU_CB_STRUCT(ShaderObjectData
     {
     Float4 Raw[8];
 
-    void FLAXENGINE_API Store(const Matrix& worldMatrix, const Matrix& prevWorldMatrix, const Half4& lightmapUVsAreaPacked, const Float3& geometrySize, float perInstanceRandom = 0.0f, float worldDeterminantSign = 1.0f, byte lodDitherFactor = 0, uint32 skinningOffset = 0, int16 skinningPrevOffset = 0);
-    void FLAXENGINE_API Load(Matrix& worldMatrix, Matrix& prevWorldMatrix, Half4& lightmapUVsArea, Float3& geometrySize, float& perInstanceRandom, float& worldDeterminantSign, byte& lodDitherFactor, uint32& skinningOffset, int16& prevBonesOffset) const;
+    void FLAXENGINE_API Store(const Matrix& worldMatrix, const Matrix& prevWorldMatrix, const Half4& lightmapUVsAreaPacked, const Float3& geometrySize, float perInstanceRandom = 0.0f, float worldDeterminantSign = 1.0f, int8 lodDitherFactor = 0, uint32 skinningOffset = 0, int16 skinningPrevOffset = 0);
+    void FLAXENGINE_API Load(Matrix& worldMatrix, Matrix& prevWorldMatrix, Half4& lightmapUVsArea, Float3& geometrySize, float& perInstanceRandom, float& worldDeterminantSign, int8& lodDitherFactor, uint32& skinningOffset, int16& prevBonesOffset) const;
 
     FORCE_INLINE void Store(const DrawCall& drawCall)
     {

Source/Shaders/MaterialCommon.hlsl
@@ -119,7 +119,7 @@ ObjectData LoadObject(Buffer<float4> objectsBuffer, uint objectIndex)
     object.PerInstanceRandom = vector6.w;
     uint packed7x = asuint(vector7.x);
     object.WorldDeterminantSign = (packed7x & 256) == 256 ? -1.0f : 1.0f;
-    object.LODDitherFactor = (packed7x & 255) / 255.0f;
+    object.LODDitherFactor = (packed7x & 127) / 127.0f * ((packed7x & 128) == 128 ? -1.0f : 1.0f);
     object.SkinningOffset = asuint(vector7.y);
     object.PrevBonesOffset = (int)(packed7x >> 16) - 32760;
     object.LightmapArea.xy = UnpackHalf2(asuint(vector7.z));