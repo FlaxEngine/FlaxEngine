388bd26b61ad205adc5b88a607827fd40805aa56
Phantom <salvini.michael01@gmail.com>
2026-08-09 22:38:39 +0200

    Improve Slider

--- Source/Engine/UI/GUI/Common/Slider.cs
+++ Source/Engine/UI/GUI/Common/Slider.cs
@@ -100,7 +100,7 @@ public class Slider : ContainerControl
     private Rectangle _thumbRect;
     private float _thumbCenter;
     private Float2 _thumbSize = new Float2(16, 16);
-    private bool _isSliding;
+    private bool _isSliding, _isNavSliding;
     private bool _mouseOverThumb;
     private const float _step = 10;
 
@@ -267,7 +267,7 @@ public class Slider : ContainerControl
     /// Gets a value indicating whether user is using a slider.
     /// </summary>
     [HideInEditor]
-    public bool IsSliding => _isSliding;
+    public bool IsSliding => (_isSliding || _isNavSliding);
 
     /// <summary>
     /// Occurs when sliding starts.
@@ -399,7 +399,7 @@ public class Slider : ContainerControl
         }
 
         // Draw thumb
-        var thumbColorV = _isSliding ? ThumbColorSelected : (_mouseOverThumb || IsNavFocused ? ThumbColorHighlighted : ThumbColor);
+        var thumbColorV = (_isSliding || _isNavSliding) ? ThumbColorSelected : (_mouseOverThumb || IsNavFocused ? ThumbColorHighlighted : ThumbColor);
         if (ThumbBrush != null)
             ThumbBrush.Draw(_thumbRect, thumbColorV);
         else
@@ -414,35 +414,54 @@ public class Slider : ContainerControl
             EndSliding();
         }
 
+        if(_isNavSliding)
+            _isNavSliding = false;
+
         base.OnLostFocus();
     }
 
     /// <inheritdoc />
     public override Control OnNavigate(NavDirection direction, Float2 location, Control caller, List<Control> visited)
     {
-        // Auto-focus self when navigation comes in
-        Focus();
+        _isNavSliding = IsFocused && direction != NavDirection.None;
 
-        if (!(IsNavFocused && _isSliding))
-        {
-            NavigationFocus();
-            _isSliding = true;
-        }
-        else
+        if (_isNavSliding)
         {
             // Control slider via navigation actions
-            float thumbLoc = Direction is SliderDirection.HorizontalRight or SliderDirection.HorizontalLeft ? location.X : location.Y;
+            bool isNavDirHorizontal = direction is NavDirection.Right or NavDirection.Left;
+            bool isHorizontal = (Direction is SliderDirection.HorizontalRight or SliderDirection.HorizontalLeft) && isNavDirHorizontal;
+            float thumbLoc = isHorizontal ? location.X : location.Y;
             float numValue = WholeNumbers ? 0.1f : 0.001f;
+
+            if (isHorizontal)
+            {
+                switch (Direction)
+                {
+                    case SliderDirection.HorizontalRight:
+                        Value += (thumbLoc < _thumbCenter ? -numValue : numValue) * _step;
+                        break;
+
+                    case SliderDirection.HorizontalLeft:
+                        Value -= (thumbLoc < _thumbCenter ? -numValue : numValue) * _step;
+                        break;
+
+                    default: break;
+                }
+
+                return base.OnNavigate(direction, location, caller, visited);
+            }
 
             switch (Direction)
             {
-                case SliderDirection.HorizontalRight or SliderDirection.VerticalDown:
+                case SliderDirection.VerticalDown:
                     Value += (thumbLoc < _thumbCenter ? -numValue : numValue) * _step;
                     break;
 
-                case SliderDirection.HorizontalLeft or SliderDirection.VerticalUp:
+                case SliderDirection.VerticalUp:
                     Value -= (thumbLoc < _thumbCenter ? -numValue : numValue) * _step;
                     break;
+
+                default: break;
             }
         }
 
