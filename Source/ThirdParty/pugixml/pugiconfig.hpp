10f8461dfaac5ab4cef4a3ab76590de636f099e9
Ari Vuollet <ari.vuollet@goat.moe>
2026-09-02 23:04:46 +0300

    Enable header only-version of `pugixml`
    Fixes duplicate symbol conflicts when linked with latest `Assimp`

--- Source/ThirdParty/pugixml/pugiconfig.hpp
+++ Source/ThirdParty/pugixml/pugiconfig.hpp
@@ -45,7 +45,7 @@
 // #define PUGIXML_XPATH_DEPTH_LIMIT 1024
 
 // Uncomment this to switch to header-only version
-// #define PUGIXML_HEADER_ONLY
+#define PUGIXML_HEADER_ONLY
 
 // Uncomment this to enable long long support (usually enabled automatically)
 // #define PUGIXML_HAS_LONG_LONG
