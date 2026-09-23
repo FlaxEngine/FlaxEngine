f3dddf6cacc140e6b77505fce421055e4de13842
Wojtek Figat <wojtek@figat.pl>
2026-09-23 12:42:53 +0200

    Add max .NET version to 11

--- Source/Tools/Flax.Build/Build/DotNet/DotNetSdk.cs
+++ Source/Tools/Flax.Build/Build/DotNet/DotNetSdk.cs
@@ -136,7 +136,7 @@ namespace Flax.Build
         /// <summary>
         /// The maximum SDK version.
         /// </summary>
-        public static Version MaximumVersion => new Version(10, 0);
+        public static Version MaximumVersion => new Version(11, 0);
 
         /// <inheritdoc />
         public override TargetPlatform[] Platforms
